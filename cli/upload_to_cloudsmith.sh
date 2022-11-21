0b91fd69d69a838e1ceccfb6860adff13ede5256
Maidul Islam <maidul98@gmail.com>
2022-11-21 08:49:14 -0500

    add --republish to cloudsmith script

--- cli/upload_to_cloudsmith.sh
+++ cli/upload_to_cloudsmith.sh
@@ -1,15 +1,15 @@
 cd dist
 for i in *.apk; do
     [ -f "$i" ] || break
-    cloudsmith push alpine infisical/infisical-cli/alpine/any-version $i
+    cloudsmith push alpine --republish infisical/infisical-cli/alpine/any-version $i
 done
 
 for i in *.deb; do
     [ -f "$i" ] || break
-    cloudsmith push deb --no-republish infisical/infisical-cli/any-distro/any-version $i
+    cloudsmith push deb --republish infisical/infisical-cli/any-distro/any-version $i
 done
 
 for i in *.rpm; do
     [ -f "$i" ] || break
-    cloudsmith push rpm --no-republish infisical/infisical-cli/any-distro/any-version $i
+    cloudsmith push rpm --republish infisical/infisical-cli/any-distro/any-version $i
 done
