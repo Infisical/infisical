14bffebc55351dda7d539fdca735bf460d9e0f80
이강준 <me@gangjun.dev>
2022-11-27 14:58:55 +0900

    feat(frontend): update i18n default loader

--- frontend/i18n.js
+++ frontend/i18n.js
@@ -1,9 +1,14 @@
+const workaround = require("next-translate/lib/cjs/plugin/utils.js");
+
+workaround.defaultLoader =
+	"(l, n) => import(`@next-translate-root/locales/${l}.json`).then(m => m?.default[n] ?? {} )";
+
 module.exports = {
 	locales: ["en-US", "ko-KR"],
 	defaultLocale: "en-US",
 	pages: {
 		"*": ["common", "nav"],
-		"reg:^/(login|signup)": ["auth"],
+		"rgx:^/(login|signup)": ["auth"],
 		"/dashboard/[id]": ["dashboard"],
 	},
 };
