4d0969fbc32a759e469745e621652cd85f0eda1d
Tuan Dang <tuandang@mac-mini.mynetworksettings.com>
2022-11-27 18:32:00 -0500

    Remove swagger in favor of Mintlify

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -11,53 +11,6 @@ router.post(
 	authController.getNewToken
 );
 
-/**
- * @swagger
- * /auth/login1:
- *   post:
- *     summary: Return server's public key and user's salt for login via SRP.
- *     description: As part of the 1st step of secure remote password (SRP), return server's public key and user's salt.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               email:
- *                 type: string
- *                 description: Email of user
- *                 example: 'johndoe@gmail.com'
- *               clientPublicKey:
- *                 type: string
- *                 description: Client-side SRP-generated public key
- *                 example: 'b13dc83cb...'
- *     responses:
- *       200:
- *         description: Object containing the server's public key and user's salt.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 serverPublicKey:
- *                   type: string
- *                   description: Server-side SRP-generated public key
- *                   example: 'b13dc83cb...'
- *                 salt:
- *                   type: string
- *                   description: User's salt
- *                   example: 'dc1ac694f...'
- *       400:
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: 'Failed to start authentication process'
- */
 router.post(
 	'/login1',
 	loginLimiter,
