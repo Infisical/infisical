3578552e7f25fab60fbf57c13d5819bf633dc13b
Maidul Islam <maidul98@gmail.com>
2022-11-27 20:35:53 -0500

    test lint runner

--- cli/main.go
+++ cli/main.go
@@ -8,3 +8,14 @@ import "github.com/Infisical/infisical-merge/packages/cmd"
 func main() {
 	cmd.Execute()
 }
+
+func testLint() int {
+	if true {
+		return 1
+	}
+
+	testVar := 1
+
+	return testVar
+
+}
