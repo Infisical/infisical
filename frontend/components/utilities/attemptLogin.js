74467320cb74bc1876c51deaff1fd6c7d7c819de
Vladyslav Matsiiako <matsiiako@gmail.com>
2022-12-04 08:26:38 -0500

    Updated onboarding example secrets

--- frontend/components/utilities/attemptLogin.js
+++ frontend/components/utilities/attemptLogin.js
@@ -137,7 +137,15 @@ const attemptLogin = async (
             await pushKeys(
               {
                 DATABASE_URL: [
-                  "mongodb+srv://this_is:an_example@mongodb.net",
+                  "mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@mongodb.net",
+                  "personal",
+                ],
+                DB_USERNAME: [
+                  "user1234",
+                  "personal",
+                ],
+                DB_PASSWORD: [
+                  "ah8jak3hk8dhiu4dw7whxwe1l",
                   "personal",
                 ],
                 TWILIO_AUTH_TOKEN: [
