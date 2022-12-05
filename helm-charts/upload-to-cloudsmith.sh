4e6a8d6046be6a037cdc5613cfd122c64abeb289
Maidul Islam <maidul98@gmail.com>
2022-12-05 11:53:49 -0500

    script for helm upload to cloudsmith

---
+++ helm-charts/upload-to-cloudsmith.sh
@@ -0,0 +1,11 @@
+export CLOUDSMITH_API_KEY=f5cf077b879c2ab46a49c6f1ec4199e6fba0fc0b
+## Loop through each helm chart directoy and build each into helm package
+for d in */ ; do
+    helm package $d
+done
+
+## Upload each packaged helm chart 
+for i in *.tgz; do
+    [ -f "$i" ] || break
+    cloudsmith push helm --republish infisical/helm-charts $i
+done
