93703475fea30e05d901fd124857108bc10c51bf
Reginald Bondoc <reginald.bondoc@gmail.com>
2022-12-06 22:45:26 +0100

    Limit replacement to JS files only

--- frontend/scripts/replace-variable.sh
+++ frontend/scripts/replace-variable.sh
@@ -4,13 +4,13 @@ ORIGINAL=$1
 REPLACEMENT=$2
 
 if [ "${ORIGINAL}" = "${REPLACEMENT}" ]; then
-    echo "Replacement is the same, skipping.."
+    echo "Environment variable replacement is the same, skipping.."
     exit 0
 fi
 
 echo "Replacing pre-baked value.."
 
-find /app/public /app/.next -type f ! -name "*.png" ! -name "*.svg" ! -name "*.gif" ! -name "*.jpg" ! -name "*.jpeg" ! -name "*.ico" |
+find /app/public /app/.next -type f -name "*.js" |
 while read file; do
     sed -i "s|$ORIGINAL|$REPLACEMENT|g" "$file"
 done
--- frontend/scripts/set-telemetry.sh
+++ frontend/scripts/set-telemetry.sh
@@ -2,7 +2,7 @@
 
 VALUE=$1
 
-find /app/public /app/.next -type f ! -name "*.png" ! -name "*.svg" ! -name "*.gif" ! -name "*.jpg" ! -name "*.jpeg" ! -name "*.ico" |
+find /app/public /app/.next -type f -name "*.js" |
 while read file; do
     sed -i "s|TELEMETRY_CAPTURING_ENABLED|$VALUE|g" "$file"
 done
