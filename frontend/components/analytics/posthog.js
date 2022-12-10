93da106dbc2b1f6bc0d3f867ea909afcfddf0bb4
Maidul Islam <maidul98@gmail.com>
2022-12-10 11:26:53 -0500

    test

--- frontend/components/analytics/posthog.js
+++ frontend/components/analytics/posthog.js
@@ -4,7 +4,6 @@ import { ENV, POSTHOG_API_KEY, POSTHOG_HOST } from '../utilities/config';
 
 export const initPostHog = () => {
   if (typeof window !== 'undefined') {
-    // eslint-disable-next-line
     if (ENV == 'production' && TELEMETRY_CAPTURING_ENABLED) {
       posthog.init(POSTHOG_API_KEY, {
         api_host: POSTHOG_HOST
