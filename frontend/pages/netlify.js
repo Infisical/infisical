a49fcf49f1f9104e6cd0c3474a71c7ccf0788e47
Tuan Dang <dangtony98@gmail.com>
2022-12-14 18:43:21 -0500

    Rotate test OAuth2 token

--- frontend/pages/netlify.js
+++ frontend/pages/netlify.js
@@ -18,9 +18,6 @@ export default function Netlify() {
   useEffect(async () => {
     if (state === localStorage.getItem('latestCSRFToken')) {
       localStorage.removeItem('latestCSRFToken');
-        
-      // http://localhost:8080/netlify?code=qnG0g_krhklWDpdUqfhU-t1sLeZzYI3gF2d6QVnL-Gc&state=3a78cd3154a9a99ddd4eb5d99dbb3289
-      // http://localhost:8080/netlify#access_token=d-78qUAnnSzvlgfG9Y_oUV6_4TQBxLbofImiBbKAjzE&token_type=Bearer&state=5da34fa49e301e9fa1a6e40925694b77
       
       await AuthorizeIntegration({
         workspaceId: localStorage.getItem('projectData.id'),
@@ -28,7 +25,7 @@ export default function Netlify() {
         integration: "netlify"
       });
       
-    //   router.push("/integrations/" + localStorage.getItem("projectData.id"));
+      router.push("/integrations/" + localStorage.getItem("projectData.id"));
     }
 
     try {
