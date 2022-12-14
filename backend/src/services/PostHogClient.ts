f3900213b52532a0ea25950af19726b32e22b340
Tuan Dang <dangtony98@gmail.com>
2022-12-13 21:59:12 -0500

    Add telemetry output at backend container start

--- backend/src/services/PostHogClient.ts
+++ backend/src/services/PostHogClient.ts
@@ -1,15 +1,19 @@
 import { PostHog } from 'posthog-node';
-import { NODE_ENV, POSTHOG_HOST, POSTHOG_PROJECT_API_KEY, TELEMETRY_ENABLED } from '../config';
+import {
+  NODE_ENV,
+  POSTHOG_HOST,
+  POSTHOG_PROJECT_API_KEY,
+  TELEMETRY_ENABLED
+} from '../config';
+
+console.log('TELEMETRY_ENABLED: ', TELEMETRY_ENABLED);
 
 let postHogClient: any;
-if (
-    NODE_ENV === 'production'
-    && TELEMETRY_ENABLED
-) {
-    // case: enable opt-out telemetry in production
-    postHogClient = new PostHog(POSTHOG_PROJECT_API_KEY, {
-        host: POSTHOG_HOST
-    });
+if (NODE_ENV === 'production' && TELEMETRY_ENABLED) {
+  // case: enable opt-out telemetry in production
+  postHogClient = new PostHog(POSTHOG_PROJECT_API_KEY, {
+    host: POSTHOG_HOST
+  });
 }
 
 export default postHogClient;
