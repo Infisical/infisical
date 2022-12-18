648e3e3bbf7c1f0e8fc0bd6b7cdddd28837ab082
Tuan Dang <dangtony98@gmail.com>
2022-12-18 17:19:09 -0500

    Continue developing log schema

--- backend/src/models/log.ts
+++ backend/src/models/log.ts
@@ -2,17 +2,19 @@ import { Schema, model, Types } from 'mongoose';
 
 export interface ILog {
     _id: Types.ObjectId;
-    user: Types.ObjectId;
+    user?: Types.ObjectId;
     workspace: Types.ObjectId;
     event: string;
     source: string;
-    ipAddress: string;
+    ipAddress?: string;
 }
 
 // TODO: need a way to store payload info for each
 // log
 
 // which secret is being ref etc.
+
+// user logged in
 
 const logSchema = new Schema<ILog>(
     {
@@ -24,17 +26,25 @@ const logSchema = new Schema<ILog>(
             type: Schema.Types.ObjectId,
             ref: 'Workspace'
         },
-        event: {
+        event: { // push, pull
             type: String,
             required: true
         },
-        source: { // should this just be a payload attr?
+        payload: { // should this just be a payload attr?
+            numberOfSecrets: {
+                type: Number
+            },
+            environment: {
+                type: String
+            }
+        },
+        channel: { 
             type: String,
+            enum: ['web', 'cli', 'auto'],
             required: true
         },
         ipAddress: { // store in bytes?
-            type: String,
-            required: true
+            type: String
         }
     }, {
         timestamps: true
