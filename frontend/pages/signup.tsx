cc2803acee63356dd6d92c268862e78262321422
Vladyslav Matsiiako <matsiiako@gmail.com>
2022-12-27 18:32:12 -0500

    Fixed the merge conflicts on the signup page

--- frontend/pages/signup.tsx
+++ frontend/pages/signup.tsx
@@ -265,20 +265,6 @@ export default function SignUp() {
 
   // Step 1 of the sign up process (enter the email or choose google authentication)
   const step1 = (
-<<<<<<< HEAD
-    <div className="bg-bunker w-full max-w-md mx-auto h-7/12 py-8 md:px-6 mx-1 mb-48 md:mb-16 rounded-xl drop-shadow-xl">
-      <p className="text-4xl font-semibold flex justify-center text-transparent bg-clip-text bg-gradient-to-br from-sky-400 to-primary">
-        {"Let'"}s get started
-      </p>
-      <div className="flex flex-col items-center justify-center w-full md:pb-2 max-h-24 max-w-md mx-auto pt-2">
-        <Link href="/login">
-          <button
-            type="button"
-            className="w-max pb-3 hover:opacity-90 duration-200"
-          >
-            <u className="font-normal text-md text-sky-500">
-              {t("signup:already-have-account")}
-=======
     <div>
       <div className="bg-bunker w-full max-w-md mx-auto h-7/12 py-8 md:px-6 mx-1 rounded-xl drop-shadow-xl">
         <p className="text-4xl font-semibold flex justify-center text-primary">
@@ -303,8 +289,7 @@ export default function SignUp() {
           </div> */}
         <div className="flex flex-col items-center justify-center w-5/6 md:w-full md:p-2 max-h-28 max-w-xs md:max-w-md mx-auto mt-4 md:mt-4 text-sm text-center md:text-left">
           <p className="text-gray-400 mt-2 md:mx-0.5">
-            By creating an account, you agree to our Terms and have read and
-            acknowledged the Privacy Policy.
+            {t("signup:step1-privacy")}
           </p>
           <div className="text-l mt-6 m-2 md:m-8 px-8 py-1 text-lg">
             <Button text="Get Started" type="submit" onButtonPressed={emailCheck} size="lg" />
@@ -315,40 +300,11 @@ export default function SignUp() {
         <Link href="/login">
           <button type="button" className="w-max pb-3 hover:opacity-90 duration-200">
             <u className="font-normal text-sm text-primary-500">
-              Have an account? Log in
->>>>>>> 876c5f51c2caabf92b47d5a18a9b98e601806bf4
+              {t("signup:already-have-account")}
             </u>
           </button>
         </Link>
       </div>
-<<<<<<< HEAD
-      <div className="flex items-center justify-center w-5/6 md:w-full m-auto md:p-2 rounded-lg max-h-24 mt-4">
-        <InputField
-          label={t("common:email")}
-          onChangeHandler={setEmail}
-          type="email"
-          value={email}
-          placeholder=""
-          isRequired
-          error={emailError}
-          errorText={emailErrorMessage}
-          autoComplete="username"
-        />
-      </div>
-      {/* <div className='flex flex-row justify-left mt-4 max-w-md mx-auto'>
-          <Checkbox className="mr-4"/>
-          <p className='text-sm'>I do not want to receive emails about Infisical and its products.</p>
-        </div> */}
-      <div className="flex flex-col items-center justify-center w-5/6 md:w-full md:p-2 max-h-28 max-w-xs md:max-w-md mx-auto mt-4 md:mt-4 text-sm text-center md:text-left">
-        <p className="text-gray-400 mt-2 md:mx-0.5">
-          {t("signup:step1-privacy")}
-        </p>
-        <div className="text-l mt-6 m-2 md:m-8 px-8 py-1 text-lg">
-          <Button text="Get Started" onButtonPressed={emailCheck} size="lg" />
-        </div>
-      </div>
-=======
->>>>>>> 876c5f51c2caabf92b47d5a18a9b98e601806bf4
     </div>
 
   );
@@ -393,23 +349,11 @@ export default function SignUp() {
         />
       </div>
       <div className="flex flex-col items-center justify-center w-full max-h-24 max-w-md mx-auto pt-2">
-<<<<<<< HEAD
-        <div className="flex flex-row items-baseline gap-1">
-          <span className="text-gray-400">Not seeing an email?</span>
-          <u
-            className={`font-normal text-sm ${
-              isResendingVerificationEmail
-                ? "text-gray-400"
-                : "text-sky-500 hover:opacity-90 duration-200"
-            }`}
-          >
-=======
         <div className="flex flex-row items-baseline gap-1 text-sm">
           <span className="text-gray-400">
             Not seeing an email?
           </span>
           <u className={`font-normal ${isResendingVerificationEmail ? 'text-gray-400' : 'text-primary-500 hover:opacity-90 duration-200'}`}>
->>>>>>> 876c5f51c2caabf92b47d5a18a9b98e601806bf4
             <button disabled={isLoading} onClick={resendVerificationEmail}>
               {isResendingVerificationEmail ? "Resending..." : "Resend"}
             </button>
@@ -579,18 +523,13 @@ export default function SignUp() {
         <FontAwesomeIcon icon={faWarning} className="ml-2 mr-4 text-4xl" />
         {t("signup:step4-description3")}
       </div>
-<<<<<<< HEAD
-      <div className="flex flex-row items-center justify-center w-3/4 md:w-full md:p-2 max-h-28 max-w-max mx-auto mt-6 py-1 md:mt-4 text-lg text-center md:text-left">
-=======
       <div className="flex flex-col items-center justify-center md:px-4 md:py-5 mt-2 px-2 py-3 max-h-24 max-w-max mx-auto text-lg">
->>>>>>> 876c5f51c2caabf92b47d5a18a9b98e601806bf4
         <Button
           text="Download PDF"
           onButtonPressed={async () => {
             await issueBackupKey({
               email,
               password,
-<<<<<<< HEAD
               personalName: firstName + " " + lastName,
               setBackupKeyError,
               setBackupKeyIssued,
@@ -598,13 +537,6 @@ export default function SignUp() {
             const userWorkspaces = await getWorkspaces();
             const userWorkspace = userWorkspaces[0]._id;
             router.push("/home/" + userWorkspace);
-=======
-              personalName: firstName + ' ' + lastName,
-              setBackupKeyError,
-              setBackupKeyIssued
-            });
-            router.push('/dashboard/');
->>>>>>> 876c5f51c2caabf92b47d5a18a9b98e601806bf4
           }}
           size="lg"
         />
