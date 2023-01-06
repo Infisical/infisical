53273df51fff6a6debcee8beb7ae03c9cc40f911
Maidul Islam <maidul98@gmail.com>
2023-01-06 18:47:52 -0500

    Add single secrets v2 operations

--- backend/src/routes/v2/secret.ts
+++ backend/src/routes/v2/secret.ts
@@ -4,7 +4,7 @@ import { body, param, query } from 'express-validator';
 import { ADMIN, MEMBER } from '../../variables';
 import { CreateSecretRequestBody, ModifySecretRequestBody } from '../../types/secret';
 import { secretController } from '../../controllers/v2';
-import { fetchAllSecrets } from '../../controllers/v2/secretController';
+import { fetchAllSecrets, fetchSingleSecret } from '../../controllers/v2/secretController';
 
 const router = express.Router();
 
@@ -27,6 +27,24 @@ router.post(
 );
 
 /**
+ * Create single secret for a given workspace and environmentName
+ */
+router.post(
+  '/workspace/:workspaceId/environment/:environmentName',
+  requireAuth({
+    acceptedAuthModes: ['jwt']
+  }),
+  requireWorkspaceAuth({
+    acceptedRoles: [ADMIN, MEMBER]
+  }),
+  param('workspaceId').exists().isMongoId().trim(),
+  param('environmentName').exists().trim(),
+  body('secret').exists().isObject(),
+  validateRequest,
+  secretController.createSingleSecret
+);
+
+/**
  * Get all secrets for a given environment and workspace id
  */
 router.get(
@@ -41,6 +59,18 @@ router.get(
   }),
   validateRequest,
   fetchAllSecrets
+);
+
+/**
+ * Get single secret by id
+ */
+router.get(
+  '/:secretId',
+  requireAuth({
+    acceptedAuthModes: ['jwt', 'serviceToken']
+  }),
+  validateRequest,
+  fetchSingleSecret
 );
 
 /**
@@ -63,6 +93,19 @@ router.delete(
 );
 
 /**
+ * delete single secret by id
+ */
+router.delete(
+  '/:secretId',
+  requireAuth({
+    acceptedAuthModes: ['jwt']
+  }),
+  param('secretId').isMongoId(),
+  validateRequest,
+  secretController.deleteSingleSecret
+);
+
+/**
  * Apply modifications to many existing secrets in a given workspace and environment
  */
 router.patch(
@@ -78,6 +121,24 @@ router.patch(
   }),
   validateRequest,
   secretController.batchModifySecrets
+);
+
+/**
+ * Apply modifications to single existing secret in a given workspace and environment
+ */
+router.patch(
+  '/workspace/:workspaceId/environment/:environmentName',
+  requireAuth({
+    acceptedAuthModes: ['jwt']
+  }),
+  body('secret').isObject(),
+  param('workspaceId').exists().isMongoId().trim(),
+  param('environmentName').exists().trim(),
+  requireWorkspaceAuth({
+    acceptedRoles: [ADMIN, MEMBER]
+  }),
+  validateRequest,
+  secretController.modifySingleSecrets
 );
 
 export default router;
