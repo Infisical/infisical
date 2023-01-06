3e102fee3d40dec8ea5e8c4df395686261083ba3
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-01-06 11:40:21 -0300

    chore: add language locale

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   debug: process.env.NODE_ENV === "development",
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "ko"],
+    locales: ["en", "ko", "pt-BR"],
   },
   fallbackLng: {
     default: ["en"],
