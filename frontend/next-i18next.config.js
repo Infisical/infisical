933fed5da694c7732e26d787a5b91c3217946d32
Vladyslav Matsiiako <matsiiako@gmail.com>
2023-01-09 13:15:54 -0800

    Got rid of i18n logs

--- frontend/next-i18next.config.js
+++ frontend/next-i18next.config.js
@@ -5,7 +5,7 @@
  */
 module.exports = {
   // https://www.i18next.com/overview/configuration-options#logging
-  debug: process.env.NODE_ENV === "development",
+  debug: false,
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ko"],
