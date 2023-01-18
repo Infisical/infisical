83899bebc832df2158a296b65f04376244f453c0
akhilmhdh <akhilmhdh@gmail.com>
2023-01-18 23:45:54 +0530

    feat(frontend): added storybook with tailwind integration
    chore(frontend): added some required radix components

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,11 @@
+import '../src/styles/globals.css';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  }
+};
