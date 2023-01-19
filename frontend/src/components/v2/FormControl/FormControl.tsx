e7ac74c5a0b73bf63ba6703e4d4f68da4ec549b5
akhilmhdh <akhilmhdh@gmail.com>
2023-01-19 23:16:59 +0530

    feat(ui): implemented form control components

---
+++ frontend/src/components/v2/FormControl/FormControl.tsx
@@ -0,0 +1,72 @@
+import { cloneElement, ReactNode } from 'react';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as Label from '@radix-ui/react-label';
+import { twMerge } from 'tailwind-merge';
+
+export type FormLabelProps = {
+  id?: string;
+  isRequired?: boolean;
+  label?: ReactNode;
+};
+
+export const FormLabel = ({ id, label, isRequired }: FormLabelProps) => (
+  <Label.Root className="text-mineshaft-300 text-sm font-medium block mb-1 ml-0.5" htmlFor={id}>
+    {label}
+    {isRequired && <span className="text-red ml-1">*</span>}
+  </Label.Root>
+);
+
+export type FormHelperTextProps = {
+  isError?: boolean;
+  text?: ReactNode;
+};
+
+export const FormHelperText = ({ isError, text }: FormHelperTextProps) => (
+  <div
+    className={twMerge(
+      'text-xs flex items-center opacity-90 text-mineshaft-300 mt-2',
+      isError && 'text-red-600'
+    )}
+  >
+    {isError && (
+      <span>
+        <FontAwesomeIcon icon={faExclamationTriangle} size="sm" className="mr-1" />
+      </span>
+    )}
+    <span>{text}</span>
+  </div>
+);
+
+export type FormControlProps = {
+  id?: string;
+  isRequired?: boolean;
+  isError?: boolean;
+  label?: ReactNode;
+  helperText?: ReactNode;
+  errorText?: ReactNode;
+  children: JSX.Element;
+};
+
+export const FormControl = ({
+  children,
+  isRequired,
+  label,
+  helperText,
+  errorText,
+  id,
+  isError
+}: FormControlProps): JSX.Element => {
+  return (
+    <div>
+      {typeof label === 'string' ? (
+        <FormLabel label={label} isRequired={isRequired} id={id} />
+      ) : (
+        label
+      )}
+      {cloneElement(children, { isRequired, 'data-required': isRequired, isError })}
+      {!isError && helperText && <FormHelperText isError={isError} text={helperText} />}
+      {isError && errorText && <FormHelperText isError={isError} text={errorText} />}
+    </div>
+  );
+};
--- frontend/src/components/v2/index.tsx
+++ frontend/src/components/v2/index.tsx
@@ -1,1 +1,3 @@
 export * from './Button';
+export * from './FormControl';
+export * from './Input';
