ae0c0241aada2ba6bcecbba54eb335c27b15aa27
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-01-23 08:21:42 +0530

    feat(frontend): allow separate domain for backend
    Fixes Allow configuring `API_URL` for frontend #248.

--- frontend/src/components/utilities/config/index.ts
+++ frontend/src/components/utilities/config/index.ts
@@ -1,11 +1,12 @@
-const ENV = process.env.NEXT_PUBLIC_ENV! || "development"; // investigate
+const ENV = process.env.NEXT_PUBLIC_ENV! || 'development'; // investigate
 const POSTHOG_API_KEY = process.env.NEXT_PUBLIC_POSTHOG_API_KEY!;
-const POSTHOG_HOST =
-  process.env.NEXT_PUBLIC_POSTHOG_HOST! || "https://app.posthog.com";
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST! || 'https://app.posthog.com';
 const STRIPE_PRODUCT_PRO = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_PRO!;
 const STRIPE_PRODUCT_STARTER = process.env.NEXT_PUBLIC_STRIPE_PRODUCT_STARTER!;
+const BACKEND_API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL || '';
 
 export {
+  BACKEND_API_URL,
   ENV,
   POSTHOG_API_KEY,
   POSTHOG_HOST,
