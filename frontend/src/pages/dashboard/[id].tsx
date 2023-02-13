23c740d225345489eb9d59f7e08ed185716cb49b
Fernando Villarreal <fernandovillarreal@Fernandos-MacBook-Pro.local>
2023-02-13 11:07:22 -0600

    setHasUnsavedChanges to false when user selects another env and they agree to not save changes

--- frontend/src/pages/dashboard/[id].tsx
+++ frontend/src/pages/dashboard/[id].tsx
@@ -574,6 +574,8 @@ export default function Dashboard() {
       if (snapshotData) setSelectedSnapshotEnv(selectedWorkspaceEnv);
       else setSelectedEnv(selectedWorkspaceEnv);
     }
+
+    setHasUnsavedChanges(false);
   };
 
   return data ? (
