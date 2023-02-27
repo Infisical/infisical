06ea809d604da34cff92ef27e92458aaa439a645
Maidul Islam <maidul98@gmail.com>
2023-02-26 23:19:28 -0500

    change color of bold welcome text

--- cli/packages/cmd/login.go
+++ cli/packages/cmd/login.go
@@ -234,14 +234,16 @@ var loginCmd = &cobra.Command{
 		// clear backed up secrets from prev account
 		util.DeleteBackupSecrets()
 
-		whilte := color.New(color.FgWhite)
+		whilte := color.New(color.FgGreen)
 		boldWhite := whilte.Add(color.Bold)
 		boldWhite.Printf(">>>> Welcome to Infisical!")
-		color.Green(" You are now logged in as %v <<<<", email)
-		boldWhite.Println("\nQuick links")
+		boldWhite.Printf(" You are now logged in as %v <<<< \n", email)
+
+		plainBold := color.New(color.Bold)
+
+		plainBold.Println("\nQuick links")
 		fmt.Println("- Learn to inject secrets into your application at https://infisical.com/docs/cli/usage")
 		fmt.Println("- Stuck? Join our slack for quick support https://tinyurl.com/infisical-slack")
-
 	},
 }
 
