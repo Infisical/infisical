5535c0479605bfbb98dc93e43793fe23ce53e182
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-04-10 09:21:04 +0545

    model setup

--- backend/src/models/integration.ts
+++ backend/src/models/integration.ts
@@ -1,120 +1,123 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, model, Types } from 'mongoose';
 import {
-  INTEGRATION_AZURE_KEY_VAULT,
-  INTEGRATION_AWS_PARAMETER_STORE,
-  INTEGRATION_AWS_SECRET_MANAGER,
-  INTEGRATION_HEROKU,
-  INTEGRATION_VERCEL,
-  INTEGRATION_NETLIFY,
-  INTEGRATION_GITHUB,
-  INTEGRATION_GITLAB,
-  INTEGRATION_RENDER,
-  INTEGRATION_FLYIO,
-  INTEGRATION_CIRCLECI,
-  INTEGRATION_TRAVISCI,
-} from "../variables";
+    INTEGRATION_AZURE_KEY_VAULT,
+    INTEGRATION_AWS_PARAMETER_STORE,
+    INTEGRATION_AWS_SECRET_MANAGER,
+    INTEGRATION_HEROKU,
+    INTEGRATION_VERCEL,
+    INTEGRATION_NETLIFY,
+    INTEGRATION_GITHUB,
+    INTEGRATION_GITLAB,
+    INTEGRATION_RENDER,
+    INTEGRATION_FLYIO,
+    INTEGRATION_CIRCLECI,
+    INTEGRATION_TRAVISCI,
+    INTEGRATION_SUPABASE
+} from '../variables';
 
 export interface IIntegration {
-  _id: Types.ObjectId;
-  workspace: Types.ObjectId;
-  environment: string;
-  isActive: boolean;
-  app: string;
-  owner: string;
-  targetEnvironment: string;
-  appId: string;
-  path: string;
-  region: string;
-  integration:
-    | 'azure-key-vault' 
-    | 'aws-parameter-store'
-    | 'aws-secret-manager'
-    | 'heroku' 
-    | 'vercel' 
-    | 'netlify' 
-    | 'github'
-    | 'gitlab'
-    | 'render' 
-    | 'flyio'
-    | 'circleci'
-    | 'travisci';
-  integrationAuth: Types.ObjectId;
+    _id: Types.ObjectId;
+    workspace: Types.ObjectId;
+    environment: string;
+    isActive: boolean;
+    app: string;
+    owner: string;
+    targetEnvironment: string;
+    appId: string;
+    path: string;
+    region: string;
+    integration:
+        | 'azure-key-vault'
+        | 'aws-parameter-store'
+        | 'aws-secret-manager'
+        | 'heroku'
+        | 'vercel'
+        | 'netlify'
+        | 'github'
+        | 'gitlab'
+        | 'render'
+        | 'flyio'
+        | 'circleci'
+        | 'travisci'
+        | 'supabase';
+    integrationAuth: Types.ObjectId;
 }
 
 const integrationSchema = new Schema<IIntegration>(
-  {
-    workspace: {
-      type: Schema.Types.ObjectId,
-      ref: "Workspace",
-      required: true,
+    {
+        workspace: {
+            type: Schema.Types.ObjectId,
+            ref: 'Workspace',
+            required: true
+        },
+        environment: {
+            type: String,
+            required: true
+        },
+        isActive: {
+            type: Boolean,
+            required: true
+        },
+        app: {
+            // name of app in provider
+            type: String,
+            default: null
+        },
+        appId: {
+            // id of app in provider
+            type: String,
+            default: null
+        },
+        targetEnvironment: {
+            // target environment
+            type: String,
+            default: null
+        },
+        owner: {
+            // github-specific repo owner-login
+            type: String,
+            default: null
+        },
+        path: {
+            // aws-parameter-store-specific path
+            type: String,
+            default: null
+        },
+        region: {
+            // aws-parameter-store-specific path
+            type: String,
+            default: null
+        },
+        integration: {
+            type: String,
+            enum: [
+                INTEGRATION_AZURE_KEY_VAULT,
+                INTEGRATION_AWS_PARAMETER_STORE,
+                INTEGRATION_AWS_SECRET_MANAGER,
+                INTEGRATION_HEROKU,
+                INTEGRATION_VERCEL,
+                INTEGRATION_NETLIFY,
+                INTEGRATION_GITHUB,
+                INTEGRATION_GITLAB,
+                INTEGRATION_RENDER,
+                INTEGRATION_FLYIO,
+                INTEGRATION_CIRCLECI,
+                INTEGRATION_TRAVISCI,
+                INTEGRATION_SUPABASE
+            ],
+            required: true
+        },
+        integrationAuth: {
+            type: Schema.Types.ObjectId,
+            ref: 'IntegrationAuth',
+            required: true
+        }
     },
-    environment: {
-      type: String,
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      required: true,
-    },
-    app: {
-      // name of app in provider
-      type: String,
-      default: null,
-    },
-    appId: {
-      // id of app in provider
-      type: String,
-      default: null,
-    },
-    targetEnvironment: {
-      // target environment
-      type: String,
-      default: null,
-    },
-    owner: {
-      // github-specific repo owner-login
-      type: String,
-      default: null,
-    },
-    path: {
-      // aws-parameter-store-specific path
-      type: String,
-      default: null
-    },
-    region: {
-      // aws-parameter-store-specific path
-      type: String,
-      default: null
-    },
-    integration: {
-      type: String,
-      enum: [
-        INTEGRATION_AZURE_KEY_VAULT,
-        INTEGRATION_AWS_PARAMETER_STORE,
-        INTEGRATION_AWS_SECRET_MANAGER,
-        INTEGRATION_HEROKU,
-        INTEGRATION_VERCEL,
-        INTEGRATION_NETLIFY,
-        INTEGRATION_GITHUB,
-        INTEGRATION_GITLAB,
-        INTEGRATION_RENDER,
-        INTEGRATION_FLYIO,
-        INTEGRATION_CIRCLECI,
-        INTEGRATION_TRAVISCI,
-      ],
-      required: true,
-    },
-    integrationAuth: {
-      type: Schema.Types.ObjectId,
-      ref: "IntegrationAuth",
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true
+    }
 );
 
-const Integration = model<IIntegration>("Integration", integrationSchema);
+const Integration = model<IIntegration>('Integration', integrationSchema);
 
 export default Integration;
--- backend/src/models/integrationAuth.ts
+++ backend/src/models/integrationAuth.ts
@@ -1,119 +1,134 @@
-import { Schema, model, Types } from "mongoose";
+import { Schema, model, Types } from 'mongoose';
 import {
-  INTEGRATION_AZURE_KEY_VAULT,
-  INTEGRATION_AWS_PARAMETER_STORE,
-  INTEGRATION_AWS_SECRET_MANAGER,
-  INTEGRATION_HEROKU,
-  INTEGRATION_VERCEL,
-  INTEGRATION_NETLIFY,
-  INTEGRATION_GITHUB,
-  INTEGRATION_GITLAB,
-  INTEGRATION_RENDER,
-  INTEGRATION_FLYIO,
-  INTEGRATION_CIRCLECI,
-  INTEGRATION_TRAVISCI,
-} from "../variables";
+    INTEGRATION_AZURE_KEY_VAULT,
+    INTEGRATION_AWS_PARAMETER_STORE,
+    INTEGRATION_AWS_SECRET_MANAGER,
+    INTEGRATION_HEROKU,
+    INTEGRATION_VERCEL,
+    INTEGRATION_NETLIFY,
+    INTEGRATION_GITHUB,
+    INTEGRATION_GITLAB,
+    INTEGRATION_RENDER,
+    INTEGRATION_FLYIO,
+    INTEGRATION_CIRCLECI,
+    INTEGRATION_TRAVISCI,
+    INTEGRATION_SUPABASE
+} from '../variables';
 
 export interface IIntegrationAuth {
-  _id: Types.ObjectId;
-  workspace: Types.ObjectId;
-  integration: 'heroku' | 'vercel' | 'netlify' | 'github' | 'gitlab' | 'render' | 'flyio' | 'azure-key-vault' | 'circleci' | 'travisci' | 'aws-parameter-store' | 'aws-secret-manager';
-  teamId: string;
-  accountId: string;
-  refreshCiphertext?: string;
-  refreshIV?: string;
-  refreshTag?: string;
-  accessIdCiphertext?: string;
-  accessIdIV?: string;
-  accessIdTag?: string;
-  accessCiphertext?: string;
-  accessIV?: string;
-  accessTag?: string;
-  accessExpiresAt?: Date;
+    _id: Types.ObjectId;
+    workspace: Types.ObjectId;
+    integration:
+        | 'heroku'
+        | 'vercel'
+        | 'netlify'
+        | 'github'
+        | 'gitlab'
+        | 'render'
+        | 'flyio'
+        | 'azure-key-vault'
+        | 'circleci'
+        | 'travisci'
+        | 'supabase'
+        | 'aws-parameter-store'
+        | 'aws-secret-manager';
+    teamId: string;
+    accountId: string;
+    refreshCiphertext?: string;
+    refreshIV?: string;
+    refreshTag?: string;
+    accessIdCiphertext?: string;
+    accessIdIV?: string;
+    accessIdTag?: string;
+    accessCiphertext?: string;
+    accessIV?: string;
+    accessTag?: string;
+    accessExpiresAt?: Date;
 }
 
 const integrationAuthSchema = new Schema<IIntegrationAuth>(
-  {
-    workspace: {
-      type: Schema.Types.ObjectId,
-      ref: "Workspace",
-      required: true,
+    {
+        workspace: {
+            type: Schema.Types.ObjectId,
+            ref: 'Workspace',
+            required: true
+        },
+        integration: {
+            type: String,
+            enum: [
+                INTEGRATION_AZURE_KEY_VAULT,
+                INTEGRATION_AWS_PARAMETER_STORE,
+                INTEGRATION_AWS_SECRET_MANAGER,
+                INTEGRATION_HEROKU,
+                INTEGRATION_VERCEL,
+                INTEGRATION_NETLIFY,
+                INTEGRATION_GITHUB,
+                INTEGRATION_GITLAB,
+                INTEGRATION_RENDER,
+                INTEGRATION_FLYIO,
+                INTEGRATION_CIRCLECI,
+                INTEGRATION_TRAVISCI,
+                INTEGRATION_SUPABASE
+            ],
+            required: true
+        },
+        teamId: {
+            // vercel-specific integration param
+            type: String
+        },
+        accountId: {
+            // netlify-specific integration param
+            type: String
+        },
+        refreshCiphertext: {
+            type: String,
+            select: false
+        },
+        refreshIV: {
+            type: String,
+            select: false
+        },
+        refreshTag: {
+            type: String,
+            select: false
+        },
+        accessIdCiphertext: {
+            type: String,
+            select: false
+        },
+        accessIdIV: {
+            type: String,
+            select: false
+        },
+        accessIdTag: {
+            type: String,
+            select: false
+        },
+        accessCiphertext: {
+            type: String,
+            select: false
+        },
+        accessIV: {
+            type: String,
+            select: false
+        },
+        accessTag: {
+            type: String,
+            select: false
+        },
+        accessExpiresAt: {
+            type: Date,
+            select: false
+        }
     },
-    integration: {
-      type: String,
-      enum: [
-        INTEGRATION_AZURE_KEY_VAULT,
-        INTEGRATION_AWS_PARAMETER_STORE,
-        INTEGRATION_AWS_SECRET_MANAGER,
-        INTEGRATION_HEROKU,
-        INTEGRATION_VERCEL,
-        INTEGRATION_NETLIFY,
-        INTEGRATION_GITHUB,
-        INTEGRATION_GITLAB,
-        INTEGRATION_RENDER,
-        INTEGRATION_FLYIO,
-        INTEGRATION_CIRCLECI,
-        INTEGRATION_TRAVISCI,
-      ],
-      required: true,
-    },
-    teamId: {
-      // vercel-specific integration param
-      type: String,
-    },
-    accountId: {
-      // netlify-specific integration param
-      type: String,
-    },
-    refreshCiphertext: {
-      type: String,
-      select: false,
-    },
-    refreshIV: {
-      type: String,
-      select: false,
-    },
-    refreshTag: {
-      type: String,
-      select: false,
-    },
-    accessIdCiphertext: {
-      type: String,
-      select: false
-    },
-    accessIdIV: {
-      type: String,
-      select: false
-    },
-    accessIdTag: {
-      type: String,
-      select: false
-    },
-    accessCiphertext: {
-      type: String,
-      select: false,
-    },
-    accessIV: {
-      type: String,
-      select: false,
-    },
-    accessTag: {
-      type: String,
-      select: false,
-    },
-    accessExpiresAt: {
-      type: Date,
-      select: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true
+    }
 );
 
 const IntegrationAuth = model<IIntegrationAuth>(
-  "IntegrationAuth",
-  integrationAuthSchema
+    'IntegrationAuth',
+    integrationAuthSchema
 );
 
 export default IntegrationAuth;
