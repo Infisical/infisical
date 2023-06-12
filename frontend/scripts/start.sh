c13d5e29f43509cc8f22ec524d01f863660baaa0
Maidul Islam <maidul98@gmail.com>
2023-06-12 16:19:27 -0700

    add intercom env replace during start up

--- frontend/scripts/start.sh
+++ frontend/scripts/start.sh
@@ -2,6 +2,8 @@
 
 scripts/replace-variable.sh "$BAKED_NEXT_PUBLIC_POSTHOG_API_KEY" "$NEXT_PUBLIC_POSTHOG_API_KEY"
 
+scripts/replace-variable.sh "$BAKED_NEXT_PUBLIC_INTERCOM_ID" "$NEXT_PUBLIC_INTERCOM_ID"
+
 if [ "$TELEMETRY_ENABLED" != "false" ]; then
     echo "Telemetry is enabled"
     scripts/set-telemetry.sh true
