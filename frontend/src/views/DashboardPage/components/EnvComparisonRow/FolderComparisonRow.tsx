b1b32a34c9982685039237230cb1a1b544a94aa4
akhilmhdh <akhilmhdh@gmail.com>
2023-06-13 20:16:14 +0530

    feat(folder-sec-overview): made folder cell fully select

--- frontend/src/views/DashboardPage/components/EnvComparisonRow/FolderComparisonRow.tsx
+++ frontend/src/views/DashboardPage/components/EnvComparisonRow/FolderComparisonRow.tsx
@@ -18,13 +18,13 @@ export const FolderComparisonRow = ({
     className="group flex min-w-full cursor-pointer flex-row items-center hover:bg-mineshaft-800"
     onClick={() => onClick(folderName)}
   >
-    <td className="flex h-10 w-10 items-center justify-center border-none px-4">
+    <td className="flex h-10 w-10  items-center justify-center border-none px-4">
       <div className="w-10 text-center text-xs text-bunker-400">
         <FontAwesomeIcon icon={faFolder} className="text-primary-700" />
       </div>
     </td>
     <td className="flex h-full min-w-[200px] flex-row items-center justify-between lg:min-w-[200px] xl:min-w-[250px]">
-      <div className="flex h-8 cursor-default flex-row items-center truncate">{folderName}</div>
+      <div className="flex h-8 flex-row items-center truncate">{folderName}</div>
     </td>
     {userAvailableEnvs?.map(({ slug }) => (
       <td
