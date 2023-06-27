825143f17ce0048e890a182c2b4a260a1dc299b3
Tuan Dang <tony@infisical.com>
2023-06-27 16:12:18 +0700

    Adjust breadcrumb spacing

--- frontend/src/views/Settings/BillingSettingsPage/BillingSettingsPage.tsx
+++ frontend/src/views/Settings/BillingSettingsPage/BillingSettingsPage.tsx
@@ -9,16 +9,18 @@ import {
 export const BillingSettingsPage = () => {
     const { t } = useTranslation();
     return (
-      <div className="h-full p-8">
+      <div className="h-full py-8 px-4">
         <NavHeader pageName={t("billing.title")} />
         
-        <div className="flex text-3xl mt-8 items-start max-w-screen-lg">
+        <div className="ml-4 flex text-3xl mt-8 items-start max-w-screen-lg">
           <div className="flex-1">
             <p className="font-semibold text-gray-200">{t("billing.title")}</p>
           </div>
           <div />
         </div>
-        <BillingTabGroup />
+        <div className="ml-4">
+          <BillingTabGroup />
+        </div>
       </div>
     );
 };
