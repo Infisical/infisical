6c1ccc17b312b8d87eeb23658fbe7600343db1c5
Andrew Atimapre <atimaxandrew@gmail.com>
2023-07-04 19:28:42 +0100

    laravel forge integration

---
+++ frontend/src/pages/integrations/laravel-forge/authorize.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import { Button, Card, CardTitle, FormControl, Input } from "../../../components/v2";
+import saveIntegrationAccessToken from "../../api/integrations/saveIntegrationAccessToken";
+
+export default function LaravelForgeCreateIntegrationPage() {
+  const router = useRouter();
+  const [apiKey, setApiKey] = useState("");
+  const [apiKeyErrorText, setApiKeyErrorText] = useState("");
+  const [serverId, setServerId] = useState("");
+  const [serverIdErrorText, setServerIdErrorText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleButtonClick = async () => {
+    try {
+      setApiKeyErrorText("");
+      setServerIdErrorText("");
+
+      if (apiKey.length === 0 || serverId.length === 0) {
+        if (apiKey.length === 0) setApiKeyErrorText("Access Token cannot be blank");
+        if (serverId.length === 0) setServerIdErrorText("Server Id cannot be blank");
+        return;
+      }
+
+      setIsLoading(true);
+
+      const integrationAuth = await saveIntegrationAccessToken({
+        workspaceId: localStorage.getItem("projectData.id"),
+        integration: "laravel-forge",
+        accessId: serverId,
+        accessToken: apiKey,
+        url: null,
+        namespace: null
+      });
+
+      setIsLoading(false);
+
+      router.push(`/integrations/laravel-forge/create?integrationAuthId=${integrationAuth._id}`);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <Card className="max-w-md rounded-md p-8">
+        <CardTitle className="text-center">Laravel Forge Integration</CardTitle>
+        <FormControl
+          label="Laravel Forge Access Token"
+          errorText={apiKeyErrorText}
+          isError={apiKeyErrorText !== "" ?? false}
+        >
+          <Input placeholder="Access Token" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
+        </FormControl>
+        <FormControl
+          label="Laravel Forge Server Id"
+          errorText={serverIdErrorText}
+          isError={serverIdErrorText !== "" ?? false}
+        >
+          <Input placeholder="Server Id" value={serverId} onChange={(e) => setServerId(e.target.value)} />
+        </FormControl>
+        <Button
+          onClick={handleButtonClick}
+          color="mineshaft"
+          className="mt-4"
+          isLoading={isLoading}
+        >
+          Connect to Laravel Forge
+        </Button>
+      </Card>
+    </div>
+  );
+}
+
+LaravelForgeCreateIntegrationPage.requireAuth = true;
