91e172fd79b764ec1cf6bdadf107f6156f229048
Chukwunonso Frank <franknonso114@gmail.com>
2023-07-09 16:18:58 +0100

    add Northflank specific create.tsx file

--- frontend/src/pages/integrations/northflank/create.tsx
+++ frontend/src/pages/integrations/northflank/create.tsx
@@ -2,7 +2,15 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import queryString from "query-string";
 
-import { Button, Card, CardTitle, FormControl, Select, SelectItem } from "../../../components/v2";
+import {
+  Button,
+  Card,
+  CardTitle,
+  FormControl,
+  Input,
+  Select,
+  SelectItem
+} from "../../../components/v2";
 import {
   useGetIntegrationAuthApps,
   useGetIntegrationAuthById
@@ -22,6 +30,7 @@ export default function NorthflankCreateIntegrationPage() {
   });
 
   const [selectedSourceEnvironment, setSelectedSourceEnvironment] = useState("");
+  const [secretPath, setSecretPath] = useState("/");
   const [targetApp, setTargetApp] = useState("");
 
   const [isLoading, setIsLoading] = useState(false);
@@ -62,7 +71,8 @@ export default function NorthflankCreateIntegrationPage() {
         targetServiceId: null,
         owner: null,
         path: null,
-        region: null
+        region: null,
+        secretPath
       });
 
       setIsLoading(false);
@@ -96,6 +106,13 @@ export default function NorthflankCreateIntegrationPage() {
               </SelectItem>
             ))}
           </Select>
+        </FormControl>
+        <FormControl label="Secrets Path">
+          <Input
+            value={secretPath}
+            onChange={(evt) => setSecretPath(evt.target.value)}
+            placeholder="Provide a path, default is /"
+          />
         </FormControl>
         <FormControl label="Northflank Project" className="mt-4">
           <Select
