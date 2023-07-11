3e3bbe298d2eaf91c9fc0b40503bf64f489f2f29
Tuan Dang <tony@infisical.com>
2023-07-11 14:50:41 +0700

    Add support for Office365 SMTP

--- backend/src/variables/smtp.ts
+++ backend/src/variables/smtp.ts
@@ -3,3 +3,4 @@ export const SMTP_HOST_MAILGUN = "smtp.mailgun.org";
 export const SMTP_HOST_SOCKETLABS = "smtp.socketlabs.com";
 export const SMTP_HOST_ZOHOMAIL = "smtp.zoho.com";
 export const SMTP_HOST_GMAIL = "smtp.gmail.com";
+export const SMTP_HOST_OFFICE365 = "smtp.office365.com";
