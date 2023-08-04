49bcd8839f0baad0791aaaf8aeaea38c254d0c23
Maidul Islam <maidul98@gmail.com>
2023-08-04 18:15:07 -0400

    move github scanning service to ee

--- backend/src/ee/services/index.ts
+++ backend/src/ee/services/index.ts
@@ -1,9 +1,11 @@
 import EELicenseService from "./EELicenseService";
 import EESecretService from "./EESecretService";
 import EELogService from "./EELogService";
+import GithubSecretScanningService from "./GithubSecretScanning/GithubSecretScanningService"
 
 export {
     EELicenseService,
     EESecretService,
     EELogService,
+    GithubSecretScanningService
 }
