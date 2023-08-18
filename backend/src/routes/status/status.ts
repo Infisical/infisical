065e1508476877cee998025743029b2237f0a01b
Maidul Islam <maidul98@gmail.com>
2023-08-18 09:42:33 -0400

    update status api

--- backend/src/routes/status/status.ts
+++ backend/src/routes/status/status.ts
@@ -6,11 +6,19 @@ const router = express.Router();
 router.get(
   "/status",
   async (req: Request, res: Response) => {
+    const gitAppId = await getSecretScanningGitAppId()
+    const gitSecretScanningWebhookSecret = await getSecretScanningWebhookSecret()
+    const gitSecretScanningPrivateKey = await getSecretScanningPrivateKey()
+    let secretScanningConfigured = false
+    if (gitAppId && gitSecretScanningPrivateKey && gitSecretScanningWebhookSecret) {
+      secretScanningConfigured = true
+    }
+
     res.status(200).json({
       date: new Date(),
       message: "Ok",
       emailConfigured: await getSmtpConfigured(),
-      secretScanningConfigured: await getSecretScanningGitAppId() && await getSecretScanningWebhookSecret() && await getSecretScanningPrivateKey(),
+      secretScanningConfigured: secretScanningConfigured,
       inviteOnlySignup: await getInviteOnlySignup()
     })
   }
