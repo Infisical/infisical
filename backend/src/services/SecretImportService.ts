a1f08b064e82427ed1ff439470182081d3ddc378
Maidul Islam <maidul98@gmail.com>
2023-08-24 17:21:14 -0400

    add tags support in secret imports

--- backend/src/services/SecretImportService.ts
+++ backend/src/services/SecretImportService.ts
@@ -55,6 +55,14 @@ export const getAllImportedSecrets = async (
       }
     },
     {
+      $lookup: {
+        from: "tags", // note this is the name of the collection in the database, not the Mongoose model name
+        localField: "tags",
+        foreignField: "_id",
+        as: "tags"
+      }
+    },
+    {
       $group: {
         _id: {
           environment: "$environment",
