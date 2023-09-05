2ad663c02154bed4516606e7cc7cde603b49ea03
Tuan Dang <dangtony98@gmail.com>
2023-09-05 20:10:50 +0100

    Update integration app and appId values

--- frontend/src/pages/integrations/gitlab/create.tsx
+++ frontend/src/pages/integrations/gitlab/create.tsx
@@ -94,10 +94,7 @@ export default function GitLabCreateIntegrationPage() {
       await mutateAsync({
         integrationAuthId: integrationAuth?._id,
         isActive: true,
-        app:
-          integrationAuthApps?.find(
-            (integrationAuthApp) => integrationAuthApp.appId === targetAppId
-          )?.name ?? null,
+        app: integrationAuthApps?.find((integrationAuthApp) => integrationAuthApp.appId === targetAppId)?.name,
         appId: targetAppId,
         sourceEnvironment: selectedSourceEnvironment,
         targetEnvironment: targetEnvironment === "" ? "*" : targetEnvironment,
--- frontend/src/pages/integrations/laravel-forge/create.tsx
+++ frontend/src/pages/integrations/laravel-forge/create.tsx
@@ -64,9 +64,7 @@ export default function LaravelForgeCreateIntegrationPage() {
         integrationAuthId: integrationAuth?._id,
         isActive: true,
         app: targetApp,
-        appId:
-          integrationAuthApps?.find((integrationAuthApp) => integrationAuthApp.name === targetApp)
-            ?.appId ?? null,
+        appId: integrationAuthApps?.find((integrationAuthApp) => integrationAuthApp.name === targetApp)?.appId,
         sourceEnvironment: selectedSourceEnvironment,
         targetEnvironment: null,
         targetEnvironmentId: null,
