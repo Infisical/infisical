a024eecf2c19a8143519f09d955f79b6088c9560
nafees nazik <nafeesnazik21@gmail.com>
2023-09-22 23:10:46 +0530

    chore: remove utils

--- frontend/src/helpers/classNames.ts
+++
@@ -1,7 +0,0 @@
-import type { CxOptions } from "cva";
-import { cx } from "cva";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: CxOptions) {
-  return twMerge(cx(inputs));
-}
