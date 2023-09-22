a2ad9e10b4c59e692a1acb4a6e32f4ca906665ed
nafees nazik <nafeesnazik21@gmail.com>
2023-09-22 22:37:36 +0530

    chore: enable prop types

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -29,7 +29,6 @@ module.exports = {
   },
   plugins: ["react", "prettier", "simple-import-sort", "import"],
   rules: {
-    "react/prop-types": "off",
     quotes: ["error", "double", { avoidEscape: true }],
     "comma-dangle": ["error", "only-multiline"],
     "react/react-in-jsx-scope": "off",
