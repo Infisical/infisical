f5a9afec618244f18c1e8eab3ac64c973e3c7cb4
nafees nazik <nafeesnazik21@gmail.com>
2023-10-15 11:52:55 +0530

    fix: pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,15 @@
 module.exports = {
   apps: [
     {
-      name: 'frontend',
-      script: "./scripts/start.sh",
-      instances: 1,
-      cwd: "./app",
-      interpreter: 'sh',
-      exec_mode: "fork",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '500M',
-    },
-    {
-      name: 'backend',
-      script: 'npm',
-      args: 'run start',
+      name: "backend",
+      script: "npm",
+      args: "run start",
       cwd: "./backend",
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
       watch: false,
-      max_memory_restart: '500M',
+      max_memory_restart: "500M",
     },
     {
       name: "nginx",
