5eb603688f2e10510e7e848582e370764c77e66d
nafees nazik <nafeesnazik21@gmail.com>
2023-10-31 13:43:33 +0530

    chore: use single replica for development

--- scripts/mongo-dev.sh
+++ scripts/mongo-dev.sh
@@ -1,48 +1,32 @@
 #!/bin/bash
 
 
-MONGODB1=mongo
-MONGODB2=mongo2
+mongohost=mongo
 
-echo "**********************************************" ${MONGODB1}
-echo "Waiting for startup.."
-until curl http://${MONGODB1}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+ echo "Waiting for MongoDB to start..."
+until curl http://${mongohost}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   echo '.'
   sleep 1
 done
 
-
-
-echo SETUP.sh time now: `date +"%T" `
-
-
 initiate_replica_set() {
-mongosh --host ${MONGODB1}:27017 <<EOF
+mongosh --host ${mongohost}:27017 <<EOF
 var cfg = {
     "_id": "rs0",
-    "protocolVersion": 1,
-    "version": 1,
     "members": [
         {
             "_id": 0,
-            "host": "${MONGODB1}:27017",
-            "priority": 2
-        },
-        {
-            "_id": 1,
-            "host": "${MONGODB2}:27018",
-            "priority": 0
+            "host": "${mongohost}:27017"
         }
     ]
 };
 rs.initiate(cfg, { force: true });
-quit();
 EOF
 }
 
 
 create_admin_user() {
-mongosh --host ${MONGODB1}:27017 --authenticationDatabase "admin" <<EOF
+mongosh --host ${mongohost}:27017 --authenticationDatabase "admin" <<EOF
 use admin;
 db.createUser(
   {
@@ -59,7 +43,7 @@ echo "Step 1: Initiating the replica set..."
 initiate_replica_set
 
 echo "Step 2: Waiting for the replica set to be initialized..."
-until mongosh --host ${MONGODB1}:27017 --eval "rs.isMaster().ismaster" | grep "true"; do
+until mongosh --host ${mongohost}:27017 --eval "rs.isMaster().ismaster" | grep "true"; do
   sleep 1
 done
 
