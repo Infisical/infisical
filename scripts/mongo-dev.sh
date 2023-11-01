516e71192909284d8f6f36bb3772e8509f742ef0
nafees nazik <nafeesnazik21@gmail.com>
2023-11-01 15:30:45 +0530

    fix: script

--- scripts/mongo-dev.sh
+++ scripts/mongo-dev.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
 
-mongohost=mongo
+ADMIN_USERNAME=${MONGO_USERNAME:-root}
+ADMIN_PASSWORD=${MONGO_PASSWORD:-example}
+
+PRIMARY_HOST=mongo:27017
+SECONDARY_HOST=mongo2:27018
+
 
  echo "Waiting for MongoDB to start..."
-until curl http://${mongohost}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+until curl http://${PRIMARY_HOST}/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   echo '.'
   sleep 1
 done
 
 initiate_replica_set() {
-mongosh --host ${mongohost}:27017 <<EOF
+mongosh --host ${PRIMARY_HOST} <<EOF
 var cfg = {
     "_id": "rs0",
     "members": [
         {
             "_id": 0,
-            "host": "${mongohost}:27017"
+            "host": "${PRIMARY_HOST}",
+            "priority": 2
+        },
+        {
+            "_id": 1,
+            "host": "${SECONDARY_HOST}",
+            "priority": 0
         }
     ]
 };
@@ -26,12 +37,12 @@ EOF
 
 
 create_admin_user() {
-mongosh --host ${mongohost}:27017 --authenticationDatabase "admin" <<EOF
+mongosh --host ${PRIMARY_HOST} --authenticationDatabase "admin" <<EOF
 use admin;
 db.createUser(
   {
-    user: "root",
-    pwd: "example",
+    user: "${ADMIN_USERNAME}",
+    pwd: "${ADMIN_PASSWORD}",
     roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
   }
 );
@@ -43,7 +54,7 @@ echo "Step 1: Initiating the replica set..."
 initiate_replica_set
 
 echo "Step 2: Waiting for the replica set to be initialized..."
-until mongosh --host ${mongohost}:27017 --eval "rs.isMaster().ismaster" | grep "true"; do
+until mongosh --host ${PRIMARY_HOST} --eval "rs.isMaster().ismaster" | grep "true"; do
   sleep 1
 done
 
