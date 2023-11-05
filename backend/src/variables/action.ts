c9ed5f793a6752faee5f0a95031252dfffe8261d
Tuan Dang <dangtony98@gmail.com>
2023-11-05 15:57:04 +0200

    Remove action variables

--- backend/src/variables/action.ts
+++
@@ -1,6 +0,0 @@
-export const ACTION_LOGIN = "login";
-export const ACTION_LOGOUT = "logout";
-export const ACTION_ADD_SECRETS = "addSecrets";
-export const ACTION_DELETE_SECRETS = "deleteSecrets";
-export const ACTION_UPDATE_SECRETS = "updateSecrets";
-export const ACTION_READ_SECRETS = "readSecrets";
