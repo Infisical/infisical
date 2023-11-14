a0dfa5eedf1a24f6e90768ec9a421965d885718b
Maidul Islam <maidul98@gmail.com>
2023-11-14 15:25:17 -0500

    put svt v3 as feature flag

--- frontend/src/views/Settings/ProjectSettingsPage/components/ProjectServiceTokensTab/ProjectServiceTokensTab.tsx
+++ frontend/src/views/Settings/ProjectSettingsPage/components/ProjectServiceTokensTab/ProjectServiceTokensTab.tsx
@@ -1,10 +1,10 @@
 import { ServiceTokenSection } from "../ServiceTokenSection";
-// import { ServiceTokenV3Section } from "../ServiceTokenV3Section";
+import { ServiceTokenV3Section } from "../ServiceTokenV3Section";
 
 export const ProjectServiceTokensTab = () => {
     return (
         <>
-            {/* <ServiceTokenV3Section /> */}
+            {process.env.NEXT_PUBLIC_AGENT_FEATURE_FLAG === "true" ? <ServiceTokenV3Section /> : null}
             <ServiceTokenSection />
         </>
     );
