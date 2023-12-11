1f97ac5192018f09fe70b04e87570c24ed750e21
Maidul Islam <maidul98@gmail.com>
2023-12-11 14:21:51 -0500

    non-zero-max-ttl

--- backend/src/validation/auth.ts
+++ backend/src/validation/auth.ts
@@ -117,7 +117,9 @@ export const AddUniversalAuthToIdentityV1 = z.object({
       .min(1)
       .default([{ ipAddress: "0.0.0.0/0" }]),
     accessTokenTTL: z.number().int().min(0).default(7200),
-    accessTokenMaxTTL: z.number().int().min(0).default(0),
+    accessTokenMaxTTL: z.number().int().refine(value => value !== 0, {
+      message: "accessTokenMaxTTL must have a non zero number",
+    }).default(2592000), // 30 days
     accessTokenNumUsesLimit: z.number().int().min(0).default(0)
   })
 });
@@ -143,7 +145,9 @@ export const UpdateUniversalAuthToIdentityV1 = z.object({
       .optional(),
     accessTokenTTL: z.number().int().min(0).optional(),
     accessTokenNumUsesLimit: z.number().int().min(0).optional(),
-    accessTokenMaxTTL: z.number().int().min(0).default(0),
+    accessTokenMaxTTL: z.number().int().refine(value => value !== 0, {
+      message: "accessTokenMaxTTL must have a non zero number",
+    }).default(2592000),
   }),
 });
 
