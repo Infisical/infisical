d62e1c3703ef92a36ba89ce7087ed27d75f116c8
Tuan Dang <dangtony98@gmail.com>
2024-01-29 16:51:38 +0700

    Bump Cloudflare IP priority

--- backend/src/server/plugins/ip.ts
+++ backend/src/server/plugins/ip.ts
@@ -3,10 +3,10 @@ import fp from "fastify-plugin";
 /*! https://github.com/pbojinov/request-ip/blob/9501cdf6e73059cc70fc6890adb086348d7cca46/src/index.js.
   MIT License. 2022 Petar Bojinov - petarbojinov+github@gmail.com */
 const headersOrder = [
-  "x-client-ip", // Most common
-  "x-forwarded-for", // Mostly used by proxies
   "cf-connecting-ip", // Cloudflare
   "Cf-Pseudo-IPv4", // Cloudflare
+  "x-client-ip", // Most common
+  "x-forwarded-for", // Mostly used by proxies
   "fastly-client-ip",
   "true-client-ip", // Akamai and Cloudflare
   "x-real-ip", // Nginx
