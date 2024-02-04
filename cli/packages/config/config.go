bfcfcd09d2d0b5810701bb385bccea7ebda60d5c
Grraahaam <72856427+Grraahaam@users.noreply.github.com>
2024-02-04 20:19:37 +0100

    feat(cli): info subcommand

--- cli/packages/config/config.go
+++ cli/packages/config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "github.com/spf13/viper"
+
 var INFISICAL_URL string
 var INFISICAL_URL_MANUAL_OVERRIDE string
 var INFISICAL_LOGIN_URL string
@@ -9,4 +11,14 @@ func init() {
 	viper.SetDefault("INFISICAL_DISABLE_TELEMETRY", false)
 	viper.BindEnv("INFISICAL_DISABLE_TELEMETRY")
 	INFISICAL_DISABLE_TELEMETRY = viper.GetBool("INFISICAL_DISABLE_TELEMETRY")
+}
+
+// TODO: automatically generate the below block with a template using go:generate annotation
+func GetConfig() map[string]interface{} {
+	return map[string]interface{}{
+		"INFISICAL_URL":                 INFISICAL_URL,
+		"INFISICAL_URL_MANUAL_OVERRIDE": INFISICAL_URL_MANUAL_OVERRIDE,
+		"INFISICAL_LOGIN_URL":           INFISICAL_LOGIN_URL,
+		"INFISICAL_DISABLE_TELEMETRY":   INFISICAL_DISABLE_TELEMETRY,
+	}
 }
