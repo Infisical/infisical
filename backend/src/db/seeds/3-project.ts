f2c36c58f929ef628dae03c670d074d6e860eb09
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-02-09 21:23:15 +0400

    Fix project seed (seeds old projects that can be upgraded)

--- backend/src/db/seeds/3-project.ts
+++ backend/src/db/seeds/3-project.ts
@@ -2,7 +2,9 @@ import crypto from "node:crypto";
 
 import { Knex } from "knex";
 
-import { encryptSymmetric128BitHexKeyUTF8 } from "@app/lib/crypto";
+import { getConfig, initEnvConfig } from "@app/lib/config/env";
+import { createSecretBlindIndex, encryptAsymmetric, encryptSymmetric128BitHexKeyUTF8 } from "@app/lib/crypto";
+import { initLogger } from "@app/lib/logger";
 
 import { OrgMembershipRole, SecretEncryptionAlgo, SecretKeyEncoding, TableName } from "../schemas";
 import { buildUserProjectKey, getUserPrivateKey, seedData1 } from "../seed-data";
@@ -14,6 +16,8 @@ export const DEFAULT_PROJECT_ENVS = [
 ];
 
 export async function seed(knex: Knex): Promise<void> {
+  initEnvConfig(await initLogger());
+  const appCfg = getConfig();
   // Deletes ALL existing entries
   await knex(TableName.Project).del();
   await knex(TableName.Environment).del();
@@ -24,11 +28,39 @@ export async function seed(knex: Knex): Promise<void> {
       name: seedData1.project.name,
       orgId: seedData1.organization.id,
       slug: "first-project",
-      // eslint-disable-next-line
-      // @ts-ignore
-      id: seedData1.project.id
+      // @ts-expect-error exluded type id needs to be inserted here to keep it testable
+      id: seedData1.project.id,
+      version: "v1"
     })
     .returning("*");
+
+  const blindIndex = createSecretBlindIndex(appCfg.ROOT_ENCRYPTION_KEY, appCfg.ENCRYPTION_KEY);
+
+  await knex(TableName.SecretBlindIndex).insert({
+    projectId: project.id,
+    algorithm: blindIndex.algorithm,
+    keyEncoding: blindIndex.keyEncoding,
+    saltIV: blindIndex.iv,
+    encryptedSaltCipherText: blindIndex.ciphertext,
+    saltTag: blindIndex.tag
+  });
+
+  const randomBytes = crypto.randomBytes(16).toString("hex"); // Project key
+  // const encKeys = await generateUserSrpKeys(seedData1.email, seedData1.password); // User keys
+
+  const { ciphertext: encryptedProjectKey, nonce: encryptedProjectKeyIv } = encryptAsymmetric(
+    randomBytes,
+    seedData1.encryptionKeys.publicKey,
+    seedData1.encryptionKeys.privateKey
+  );
+
+  await knex(TableName.ProjectKeys).insert({
+    projectId: project.id,
+    senderId: seedData1.id,
+    receiverId: seedData1.id,
+    encryptedKey: encryptedProjectKey,
+    nonce: encryptedProjectKeyIv
+  });
 
   await knex(TableName.ProjectMembership).insert({
     projectId: project.id,
