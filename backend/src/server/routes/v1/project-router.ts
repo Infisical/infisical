a3a1a0007d9b88cac1eac5b51605d1bf202b8cf7
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-02-13 18:15:44 +0100

    Add auth methods

--- backend/src/server/routes/v1/project-router.ts
+++ backend/src/server/routes/v1/project-router.ts
@@ -171,7 +171,7 @@ export const registerProjectRouter = async (server: FastifyZodProvider) => {
         })
       }
     },
-    onRequest: verifyAuth([AuthMode.JWT]),
+    onRequest: verifyAuth([AuthMode.JWT, AuthMode.API_KEY, AuthMode.IDENTITY_ACCESS_TOKEN]),
     handler: async (req) => {
       const workspace = await server.services.project.deleteProject({
         actorId: req.permission.id,
