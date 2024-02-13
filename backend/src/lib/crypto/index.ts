592271de3b81f5793ddfa56546d99ac7916221da
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-02-14 00:20:05 +0100

    Fix for commonJS

--- backend/src/lib/crypto/index.ts
+++ backend/src/lib/crypto/index.ts
@@ -1,13 +1,14 @@
 export {
   buildSecretBlindIndexFromName,
   createSecretBlindIndex,
+  decodeBase64,
   decryptAsymmetric,
   decryptSymmetric,
   decryptSymmetric128BitHexKeyUTF8,
+  encodeBase64,
   encryptAsymmetric,
   encryptSymmetric,
   encryptSymmetric128BitHexKeyUTF8,
   generateAsymmetricKeyPair
 } from "./encryption";
 export { generateSrpServerKey, srpCheckClientProof } from "./srp";
-export { decodeBase64, encodeBase64 } from "tweetnacl-util";
