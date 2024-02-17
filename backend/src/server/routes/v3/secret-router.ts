abd28d92693a1d1fda01d313943d279bfa0f0093
Maidul Islam <maidul98@gmail.com>
2024-02-17 12:14:37 -0500

    remove comment

--- backend/src/server/routes/v3/secret-router.ts
+++ backend/src/server/routes/v3/secret-router.ts
@@ -20,8 +20,6 @@ import { PostHogEventTypes } from "@app/services/telemetry/telemetry-types";
 
 import { secretRawSchema } from "../sanitizedSchemas";
 
-// trigger workflow
-
 const getDistinctId = (req: FastifyRequest) => {
   if (req.auth.actor === ActorType.USER) {
     return req.auth.user.email;
