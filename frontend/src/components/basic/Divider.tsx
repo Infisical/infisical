1d2f10178f59267743b5b815f3dd9dcdf041be18
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-03-29 16:24:15 +0100

    Draft

---
+++ frontend/src/components/basic/Divider.tsx
@@ -0,0 +1,15 @@
+import { twMerge } from "tailwind-merge";
+
+interface IProps {
+  className?: string;
+}
+
+const Divider = ({ className }: IProps): JSX.Element => {
+  return (
+    <div className={twMerge("flex items-center px-2 opacity-50", className)}>
+      <div aria-hidden="true" className="h-5 w-full grow border border-t border-mineshaft-200" />
+    </div>
+  );
+};
+
+export default Divider;
