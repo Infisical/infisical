9fddcea3db9928c2ba64c455e8f3bbba112efa2b
Akhil Mohan <akhilmhdh@gmail.com>
2024-04-01 17:15:57 +0530

    fix(ui): sending group users without orgid

--- frontend/src/hooks/api/groups/queries.tsx
+++ frontend/src/hooks/api/groups/queries.tsx
@@ -3,8 +3,8 @@ import { useQuery } from "@tanstack/react-query";
 import { apiRequest } from "@app/config/request";
 
 export const groupKeys = {
-    getGroupUserMembership: (slug: string) => [{ slug }, "group-user-memberships"] as const
-}
+  getGroupUserMembership: (slug: string) => [{ slug }, "group-user-memberships"] as const
+};
 
 type TUser = {
   id: string;
@@ -13,16 +13,15 @@ type TUser = {
   firstName: string;
   lastName: string;
   isPartOfGroup: boolean;
-}
+};
 
 export const useListGroupUsers = (groupSlug: string) => {
   return useQuery({
     queryKey: groupKeys.getGroupUserMembership(groupSlug),
+    enabled: Boolean(groupSlug),
     queryFn: async () => {
-      const {
-        data: users
-      } = await apiRequest.get<TUser[]>(`/api/v1/groups/${groupSlug}/users`);
-      
+      const { data: users } = await apiRequest.get<TUser[]>(`/api/v1/groups/${groupSlug}/users`);
+
       return users;
     }
   });
