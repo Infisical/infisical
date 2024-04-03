91460793175412449a099497aac20ed9d9f8beb4
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-03 16:45:37 -0700

    Feat: Request access

--- frontend/src/hooks/api/accessApproval/mutation.tsx
+++ frontend/src/hooks/api/accessApproval/mutation.tsx
@@ -1,26 +1,34 @@
+import { packRules } from "@casl/ability/extra";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { apiRequest } from "@app/config/request";
 
 import { accessApprovalKeys } from "./queries";
-import { TCreateAccessPolicyDTO, TDeleteSecretPolicyDTO, TUpdateAccessPolicyDTO } from "./types";
+import {
+  TAccessApproval,
+  TCreateAccessPolicyDTO,
+  TCreateAccessRequestDTO,
+  TDeleteSecretPolicyDTO,
+  TUpdateAccessPolicyDTO
+} from "./types";
 
 export const useCreateAccessApprovalPolicy = () => {
   const queryClient = useQueryClient();
 
   return useMutation<{}, {}, TCreateAccessPolicyDTO>({
-    mutationFn: async ({ environment, workspaceId, approvals, approvers, name }) => {
+    mutationFn: async ({ environment, projectSlug, approvals, approvers, name, secretPath }) => {
       const { data } = await apiRequest.post("/api/v1/access-approvals", {
         environment,
-        workspaceId,
+        projectSlug,
         approvals,
         approvers,
+        secretPath,
         name
       });
       return data;
     },
-    onSuccess: (_, { workspaceId }) => {
-      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalPolicies(workspaceId));
+    onSuccess: (_, { projectSlug }) => {
+      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalPolicies(projectSlug));
     }
   });
 };
@@ -29,16 +37,17 @@ export const useUpdateAccessApprovalPolicy = () => {
   const queryClient = useQueryClient();
 
   return useMutation<{}, {}, TUpdateAccessPolicyDTO>({
-    mutationFn: async ({ id, approvers, approvals, name }) => {
+    mutationFn: async ({ id, approvers, approvals, name, secretPath }) => {
       const { data } = await apiRequest.patch(`/api/v1/access-approvals/${id}`, {
         approvals,
         approvers,
+        secretPath,
         name
       });
       return data;
     },
-    onSuccess: (_, { workspaceId }) => {
-      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalPolicies(workspaceId));
+    onSuccess: (_, { projectSlug }) => {
+      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalPolicies(projectSlug));
     }
   });
 };
@@ -51,8 +60,66 @@ export const useDeleteAccessApprovalPolicy = () => {
       const { data } = await apiRequest.delete(`/api/v1/access-approvals/${id}`);
       return data;
     },
-    onSuccess: (_, { workspaceId }) => {
-      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalPolicies(workspaceId));
+    onSuccess: (_, { projectSlug }) => {
+      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalPolicies(projectSlug));
+    }
+  });
+};
+
+export const useCreateAccessRequest = () => {
+  const queryClient = useQueryClient();
+  return useMutation<{}, {}, TCreateAccessRequestDTO>({
+    mutationFn: async ({ envSlug, projectSlug, secretPath, ...privilege }) => {
+      const { data } = await apiRequest.post<TAccessApproval>(
+        "/api/v1/access-approval-requests",
+        {
+          ...privilege,
+          permissions: privilege.permissions ? packRules(privilege.permissions) : undefined
+        },
+        {
+          params: {
+            envSlug,
+            projectSlug,
+            secretPath
+          }
+        }
+      );
+
+      return data;
+    },
+    onSuccess: (_, { projectSlug }) => {
+      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalRequests(projectSlug));
+    }
+  });
+};
+
+export const useReviewAccessRequest = () => {
+  const queryClient = useQueryClient();
+  return useMutation<
+    {},
+    {},
+    {
+      requestId: string;
+      status: "approved" | "rejected";
+      projectSlug: string;
+      envSlug?: string;
+      requestedBy?: string;
+    }
+  >({
+    mutationFn: async ({ requestId, status }) => {
+      const { data } = await apiRequest.post(
+        `/api/v1/access-approval-requests/${requestId}/review`,
+        {
+          status
+        }
+      );
+      return data;
+    },
+    onSuccess: (_, { projectSlug, envSlug, requestedBy }) => {
+      queryClient.invalidateQueries(
+        accessApprovalKeys.getAccessApprovalRequests(projectSlug, envSlug, requestedBy)
+      );
+      queryClient.invalidateQueries(accessApprovalKeys.getAccessApprovalRequestCount(projectSlug));
     }
   });
 };
--- frontend/src/hooks/api/accessApproval/types.ts
+++ frontend/src/hooks/api/accessApproval/types.ts
@@ -1,9 +1,11 @@
+import { TProjectPermission } from "../roles/types";
 import { WorkspaceEnv } from "../workspace/types";
 
 export type TAccessApprovalPolicy = {
   id: string;
   name: string;
   approvals: number;
+  secretPath: string;
   envId: string;
   workspace: string;
   environment: WorkspaceEnv;
@@ -11,8 +13,99 @@ export type TAccessApprovalPolicy = {
   approvers: string[];
 };
 
-export type TGetSecretApprovalPoliciesDTO = {
-  workspaceId: string;
+export type TAccessApprovalRequest = {
+  id: string;
+  policyId: string;
+  privilegeId: string | null;
+  requestedBy: string;
+  createdAt: Date;
+  updatedAt: Date;
+  isTemporary: boolean;
+  temporaryRange: string | null | undefined;
+
+  permissions: TProjectPermission[] | null;
+
+  // Computed
+  environmentName: string;
+  isApproved: boolean;
+
+  privilege: {
+    membershipId: string;
+    isTemporary: boolean;
+    temporaryMode?: string | null;
+    temporaryRange?: string | null;
+    temporaryAccessStartTime?: Date | null;
+    temporaryAccessEndTime?: Date | null;
+    permissions: TProjectPermission[];
+    isApproved: boolean;
+  } | null;
+
+  policy: {
+    id: string;
+    name: string;
+    approvals: number;
+    approvers: string[];
+    secretPath?: string | null;
+    envId: string;
+  };
+
+  reviewers: {
+    member: string;
+    status: string;
+  }[];
+};
+
+export type TAccessApproval = {
+  id: string;
+  policyId: string;
+  privilegeId: string;
+  requestedBy: string;
+};
+
+export type TAccessRequestCount = {
+  pendingCount: number;
+  finalizedCount: number;
+};
+
+export type TProjectUserPrivilege = {
+  projectMembershipId: string;
+  slug: string;
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  permissions?: TProjectPermission[];
+} & (
+  | {
+      isTemporary: true;
+      temporaryMode: string;
+      temporaryRange: string;
+      temporaryAccessStartTime: string;
+      temporaryAccessEndTime?: string;
+    }
+  | {
+      isTemporary: false;
+      temporaryMode?: null;
+      temporaryRange?: null;
+      temporaryAccessStartTime?: null;
+      temporaryAccessEndTime?: null;
+    }
+);
+
+export type TCreateAccessRequestDTO = {
+  envSlug: string;
+  projectSlug: string;
+  secretPath: string;
+} & Omit<TProjectUserPrivilege, "id" | "createdAt" | "updatedAt" | "slug">;
+
+export type TGetAccessApprovalRequestsDTO = {
+  projectSlug: string;
+  envSlug?: string;
+  authorProjectMembershipId?: string;
+};
+
+export type TGetAccessPolicyApprovalCountDTO = {
+  projectSlug: string;
+  envSlug: string;
 };
 
 export type TGetSecretApprovalPolicyOfBoardDTO = {
@@ -22,24 +115,27 @@ export type TGetSecretApprovalPolicyOfBoardDTO = {
 };
 
 export type TCreateAccessPolicyDTO = {
-  workspaceId: string;
+  projectSlug: string;
   name?: string;
   environment: string;
   approvers?: string[];
   approvals?: number;
+  secretPath?: string;
 };
 
 export type TUpdateAccessPolicyDTO = {
   id: string;
   name?: string;
   approvers?: string[];
+  secretPath?: string;
+  environment?: string;
   approvals?: number;
   // for invalidating list
-  workspaceId: string;
+  projectSlug: string;
 };
 
 export type TDeleteSecretPolicyDTO = {
   id: string;
   // for invalidating list
-  workspaceId: string;
+  projectSlug: string;
 };
