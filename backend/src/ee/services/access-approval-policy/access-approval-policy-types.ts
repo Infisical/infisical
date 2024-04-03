d3ea91c54b91298e53cabf4273d1be4723476b48
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-03 16:39:48 -0700

    Fix: Types mismatch

--- backend/src/ee/services/access-approval-policy/access-approval-policy-types.ts
+++ backend/src/ee/services/access-approval-policy/access-approval-policy-types.ts
@@ -1,10 +1,25 @@
+import { TProjectMemberships } from "@app/db/schemas";
 import { TProjectPermission } from "@app/lib/types";
+import { ActorAuthMethod } from "@app/services/auth/auth-type";
+
+import { TPermissionServiceFactory } from "../permission/permission-service";
+
+export type TVerifyApprovers = {
+  approverProjectMemberships: TProjectMemberships[];
+  permissionService: Pick<TPermissionServiceFactory, "getProjectPermission">;
+  envSlug: string;
+  actorAuthMethod: ActorAuthMethod;
+  secretPath: string;
+  projectId: string;
+  orgId: string;
+};
 
 export type TCreateAccessApprovalPolicy = {
   approvals: number;
+  secretPath?: string | null;
   environment: string;
   approvers: string[];
-  projectId: string;
+  projectSlug: string;
   name: string;
 } & Omit<TProjectPermission, "projectId">;
 
@@ -12,6 +27,7 @@ export type TUpdateAccessApprovalPolicy = {
   policyId: string;
   approvals?: number;
   approvers: string[];
+  secretPath?: string;
   name?: string;
 } & Omit<TProjectPermission, "projectId">;
 
@@ -19,7 +35,14 @@ export type TDeleteAccessApprovalPolicy = {
   policyId: string;
 } & Omit<TProjectPermission, "projectId">;
 
-export type TListAccessApprovalPoliciesDTO = TProjectPermission;
+export type TGetAccessPolicyCountByEnvironmentDTO = {
+  envSlug: string;
+  projectSlug: string;
+} & Omit<TProjectPermission, "projectId">;
+
+export type TListAccessApprovalPoliciesDTO = {
+  projectSlug: string;
+} & Omit<TProjectPermission, "projectId">;
 
 export type TGetBoardAccessApprovalPolicy = {
   projectId: string;
