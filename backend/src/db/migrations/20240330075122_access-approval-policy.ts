4cdec497515aa14cd0154a0bade8872322ce796b
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-03 16:35:51 -0700

    Feat: Request Access (migrations)

--- backend/src/db/migrations/20240330075122_access-approval-policy.ts
+++ backend/src/db/migrations/20240330075122_access-approval-policy.ts
@@ -34,7 +34,6 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTableIfExists(TableName.AccessApprovalPolicyApprover);
   await knex.schema.dropTableIfExists(TableName.AccessApprovalPolicy);
-
   await dropOnUpdateTrigger(knex, TableName.AccessApprovalPolicy);
   await dropOnUpdateTrigger(knex, TableName.AccessApprovalPolicyApprover);
 }
