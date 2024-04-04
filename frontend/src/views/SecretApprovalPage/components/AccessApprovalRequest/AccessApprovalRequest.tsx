60a06edd9b9e9ffca6a8a8e326e14d314f3fa4b9
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-04 01:03:44 -0700

    Style: Fix styling

--- frontend/src/views/SecretApprovalPage/components/AccessApprovalRequest/AccessApprovalRequest.tsx
+++ frontend/src/views/SecretApprovalPage/components/AccessApprovalRequest/AccessApprovalRequest.tsx
@@ -26,7 +26,6 @@ import {
 import { Badge } from "@app/components/v2/Badge";
 import {
   ProjectPermissionActions,
-  ProjectPermissionSub,
   useProjectPermission,
   useSubscription,
   useWorkspace
