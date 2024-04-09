f038b28c1c1c21836bcf6ca1304e70609d381d26
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-09 00:20:18 -0700

    Fix: Moved Divider to v2

--- frontend/src/components/v2/Divider/Divider.tsx
+++ frontend/src/components/v2/Divider/Divider.tsx
@@ -4,12 +4,10 @@ interface IProps {
   className?: string;
 }
 
-const Divider = ({ className }: IProps): JSX.Element => {
+export const Divider = ({ className }: IProps): JSX.Element => {
   return (
     <div className={twMerge("flex items-center px-2 opacity-50", className)}>
       <div aria-hidden="true" className="h-5 w-full grow border border-t border-mineshaft-200" />
     </div>
   );
 };
-
-export default Divider;
