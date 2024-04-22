35071af4783c3d75e66520a738d254ddbf017b47
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-04-22 15:27:42 +0200

    Fix: Run cmd tests

--- cli/test/run_test.go
+++ cli/test/run_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 
@@ -12,11 +13,12 @@ func TestServiceToken_RunCmdRecursiveAndImports(t *testing.T) {
 
 	output, err := ExecuteCliCommand(FORMATTED_CLI_NAME, "run", "--token", creds.ServiceToken, "--projectId", creds.ProjectID, "--env", creds.EnvSlug, "--recursive", "--silent", "--", "echo", "hello world")
 
-	fmt.Printf("output: %v\n", output)
-
 	if err != nil {
 		t.Fatalf("error running CLI command: %v", err)
 	}
+
+	output = string(bytes.Split([]byte(output), []byte("INF"))[1])
+	fmt.Printf("output: %v\n", output)
 
 	// Use cupaloy to snapshot test the output
 	err = cupaloy.Snapshot(output)
@@ -29,11 +31,12 @@ func TestServiceToken_RunCmdWithImports(t *testing.T) {
 
 	output, err := ExecuteCliCommand(FORMATTED_CLI_NAME, "run", "--token", creds.ServiceToken, "--projectId", creds.ProjectID, "--env", creds.EnvSlug, "--silent", "--", "echo", "hello world")
 
-	fmt.Printf("output: %v\n", output)
-
 	if err != nil {
 		t.Fatalf("error running CLI command: %v", err)
 	}
+
+	output = string(bytes.Split([]byte(output), []byte("INF"))[1])
+	fmt.Printf("output: %v\n", output)
 
 	// Use cupaloy to snapshot test the output
 	err = cupaloy.Snapshot(output)
@@ -48,11 +51,12 @@ func TestUniversalAuth_RunCmdRecursiveAndImports(t *testing.T) {
 
 	output, err := ExecuteCliCommand(FORMATTED_CLI_NAME, "run", "--token", creds.UAAccessToken, "--projectId", creds.ProjectID, "--env", creds.EnvSlug, "--recursive", "--silent", "--", "echo", "hello world")
 
-	fmt.Printf("output: %v\n", output)
-
 	if err != nil {
 		t.Fatalf("error running CLI command: %v", err)
 	}
+
+	output = string(bytes.Split([]byte(output), []byte("INF"))[1])
+	fmt.Printf("output: %v\n", output)
 
 	// Use cupaloy to snapshot test the output
 	err = cupaloy.Snapshot(output)
@@ -67,11 +71,13 @@ func TestUniversalAuth_RunCmdWithImports(t *testing.T) {
 
 	output, err := ExecuteCliCommand(FORMATTED_CLI_NAME, "run", "--token", creds.UAAccessToken, "--projectId", creds.ProjectID, "--env", creds.EnvSlug, "--silent", "--", "echo", "hello world")
 
-	fmt.Printf("output: %v\n", output)
-
 	if err != nil {
 		t.Fatalf("error running CLI command: %v", err)
 	}
+
+	// remove the first few characters from the output because we don't care about the time, and it will change every time
+	output = string(bytes.Split([]byte(output), []byte("INF"))[1])
+	fmt.Printf("output: %v\n", output)
 
 	// Use cupaloy to snapshot test the output
 	err = cupaloy.Snapshot(output)
@@ -86,11 +92,12 @@ func TestUniversalAuth_RunCmdWithoutImports(t *testing.T) {
 
 	output, err := ExecuteCliCommand(FORMATTED_CLI_NAME, "run", "--token", creds.UAAccessToken, "--projectId", creds.ProjectID, "--env", creds.EnvSlug, "--silent", "--include-imports=false", "--", "echo", "hello world")
 
-	fmt.Printf("output: %v\n", output)
-
 	if err != nil {
 		t.Fatalf("error running CLI command: %v", err)
 	}
+
+	output = string(bytes.Split([]byte(output), []byte("INF"))[1])
+	fmt.Printf("output: %v\n", output)
 
 	// Use cupaloy to snapshot test the output
 	err = cupaloy.Snapshot(output)
@@ -104,11 +111,13 @@ func TestServiceToken_RunCmdWithoutImports(t *testing.T) {
 
 	output, err := ExecuteCliCommand(FORMATTED_CLI_NAME, "run", "--token", creds.ServiceToken, "--projectId", creds.ProjectID, "--env", creds.EnvSlug, "--silent", "--include-imports=false", "--", "echo", "hello world")
 
-	fmt.Printf("output: %v\n", output)
-
 	if err != nil {
 		t.Fatalf("error running CLI command: %v", err)
 	}
+
+	// Remove everything before "INF" because it's not relevant to the test
+	output = string(bytes.Split([]byte(output), []byte("INF"))[1])
+	fmt.Printf("output: %v\n", output)
 
 	// Use cupaloy to snapshot test the output
 	err = cupaloy.Snapshot(output)
