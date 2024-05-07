c5cd5047d71cd1888b445f74e8b62dbeb37ea42a
Tuan Dang <dangtony98@gmail.com>
2024-05-07 07:59:37 -0700

    Update trusted email migration file with backfill

--- backend/src/db/migrations/20240507032811_trusted-saml-ldap-emails.ts
+++ backend/src/db/migrations/20240507032811_trusted-saml-ldap-emails.ts
@@ -5,9 +5,16 @@ import { TableName } from "../schemas";
 export async function up(knex: Knex): Promise<void> {
   const isUsersTablePresent = await knex.schema.hasTable(TableName.Users);
   if (isUsersTablePresent) {
-    await knex.schema.alterTable(TableName.Users, (t) => {
-      t.boolean("isEmailVerified");
-    });
+    const hasIsEmailVerifiedColumn = await knex.schema.hasColumn(TableName.Users, "isEmailVerified");
+
+    if (!hasIsEmailVerifiedColumn) {
+      await knex.schema.alterTable(TableName.Users, (t) => {
+        t.boolean("isEmailVerified").defaultTo(false);
+      });
+    }
+
+    // Backfilling the isEmailVerified to true where isAccepted is true
+    await knex(TableName.Users).update({ isEmailVerified: true }).where("isAccepted", true);
   }
 
   const isUserAliasTablePresent = await knex.schema.hasTable(TableName.UserAliases);
