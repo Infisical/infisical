fa3f957738d91367936b57faadbf0d72919ef144
Maidul Islam <maidul98@gmail.com>
2024-05-20 19:26:03 -0400

    count for null actor

--- backend/src/server/config/rateLimiter.ts
+++ backend/src/server/config/rateLimiter.ts
@@ -6,13 +6,13 @@ import { getConfig } from "@app/lib/config/env";
 import { ActorType } from "@app/services/auth/auth-type";
 
 const getDistinctRequestActorId = (req: FastifyRequest) => {
-  if (req.auth.actor === ActorType.USER) {
+  if (req?.auth?.actor === ActorType.USER) {
     return req.auth.user.username;
   }
-  if (req.auth.actor === ActorType.IDENTITY) {
+  if (req?.auth?.actor === ActorType.IDENTITY) {
     return `identity-${req.auth.identityId}`;
   }
-  if (req.auth.actor === ActorType.SERVICE) {
+  if (req?.auth?.actor === ActorType.SERVICE) {
     return (
       `${req.auth.serviceToken.createdByEmail}-service-token` || `service-token-null-creator-${req.auth.serviceTokenId}`
     ); // when user gets removed from system
