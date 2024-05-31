4d8f94a9dc4909d714af93b8ee5b5e08dd612db3
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2024-05-31 23:25:56 +0800

    feat: added version prune to daily resource queue

--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -795,6 +795,9 @@ export const registerRoutes = async (
   const dailyResourceCleanUp = dailyResourceCleanUpQueueServiceFactory({
     auditLogDAL,
     queueService,
+    secretVersionDAL,
+    secretFolderVersionDAL: folderVersionDAL,
+    snapshotDAL,
     identityAccessTokenDAL,
     secretSharingDAL
   });
