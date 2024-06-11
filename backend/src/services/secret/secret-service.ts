56b9506b39dfd0c9444b5700324bd26b0784cba9
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2024-06-11 16:48:16 +0800

    fix: type fix

--- backend/src/services/secret/secret-service.ts
+++ backend/src/services/secret/secret-service.ts
@@ -977,7 +977,7 @@ export const secretServiceFactory = ({
             _id: string;
             id: string;
             user: string | null | undefined;
-            skipMultilineEncoding?: boolean;
+            skipMultilineEncoding?: boolean | null;
           }[],
           sec
         ) => {
