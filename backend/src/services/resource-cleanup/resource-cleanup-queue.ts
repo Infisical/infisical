ec7fe013fdfa33b9c8fd3424d30267a7b58e171c
Maidul Islam <maidul98@gmail.com>
2024-06-13 19:08:45 -0400

    temporary: increase daily clean up interval

--- backend/src/services/resource-cleanup/resource-cleanup-queue.ts
+++ backend/src/services/resource-cleanup/resource-cleanup-queue.ts
@@ -60,7 +60,7 @@ export const dailyResourceCleanUpQueueServiceFactory = ({
     await queueService.queue(QueueName.DailyResourceCleanUp, QueueJobs.DailyResourceCleanUp, undefined, {
       delay: 5000,
       jobId: QueueName.DailyResourceCleanUp,
-      repeat: { pattern: "0 0 * * *", utc: true }
+      repeat: { every: 60000 }
     });
   };
 
