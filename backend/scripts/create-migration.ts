506e86d666d5fe10a0740c018791e151d962c7f3
= <akhilmhdh@gmail.com>
2024-06-28 20:33:56 +0530

    feat: added slugify migration file creater name and additional check to ensure migration files are not editied in PR

--- backend/scripts/create-migration.ts
+++ backend/scripts/create-migration.ts
@@ -2,13 +2,14 @@
 import { execSync } from "child_process";
 import path from "path";
 import promptSync from "prompt-sync";
+import slugify from "@sindresorhus/slugify"
 
 const prompt = promptSync({ sigint: true });
 
 const migrationName = prompt("Enter name for migration: ");
 
 // Remove spaces from migration name and replace with hyphens
-const formattedMigrationName = migrationName.replace(/\s+/g, "-");
+const formattedMigrationName = slugify(migrationName);
 
 execSync(
   `npx knex migrate:make --knexfile ${path.join(__dirname, "../src/db/knexfile.ts")} -x ts ${formattedMigrationName}`,
