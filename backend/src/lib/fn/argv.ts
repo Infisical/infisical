f186cb4d7bc9e90734a80ab9dd1bdee135c0efa3
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-07-03 20:04:03 +0200

    Alphine and migration mode

--- backend/src/lib/fn/argv.ts
+++ backend/src/lib/fn/argv.ts
@@ -1,34 +1,9 @@
-import { logger } from "../logger";
-
 export const isMigrationMode = () => {
-  try {
-    const args = process.argv.slice(2);
-    const migrationFlag = args.find((arg) => arg.startsWith("--migration-mode"));
+  const args = process.argv.slice(2);
+  const migrationMode = args.find((arg) => arg === "migration:latest");
 
-    if (!migrationFlag) {
-      return false;
-    }
-
-    // Covers the case where the flag is --migration-mode [true|false|...]
-    if (migrationFlag === "--migration-mode") {
-      const nextArg = args[args.indexOf(migrationFlag) + 1];
-      if (nextArg === "false") {
-        return false;
-      }
-      // --migration-mode without a value defaults to true
-      return nextArg === undefined || nextArg.toLowerCase() === "true";
-    }
-
-    // Covers the case where the flag is --migration-mode=[...]
-    const [, value] = migrationFlag.split("=");
-    if (value === undefined) {
-      const nextArg = args[args.indexOf(migrationFlag) + 1];
-      return nextArg === "true";
-    }
-
-    return value.toLowerCase() === "true";
-  } catch (err) {
-    logger.error(err, `Failed to check migration mode`);
-    return false;
+  if (migrationMode) {
+    return true;
   }
+  return false;
 };
