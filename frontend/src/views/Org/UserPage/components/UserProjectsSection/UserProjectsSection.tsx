ebe6b08cab666945538821c7bba00af8109751c3
Tuan Dang <dangtony98@gmail.com>
2024-07-18 20:46:36 +0700

    Public key to not invited state change for project provisioning ui

--- frontend/src/views/Org/UserPage/components/UserProjectsSection/UserProjectsSection.tsx
+++ frontend/src/views/Org/UserPage/components/UserProjectsSection/UserProjectsSection.tsx
@@ -51,7 +51,7 @@ export const UserProjectsSection = ({ membershipId }: Props) => {
     <div className="w-full rounded-lg border border-mineshaft-600 bg-mineshaft-900 p-4">
       <div className="flex items-center justify-between border-b border-mineshaft-400 pb-4">
         <h3 className="text-lg font-semibold text-mineshaft-100">Projects</h3>
-        {userId !== membership.user.id && Boolean(membership.user.publicKey) && (
+        {userId !== membership.user.id && membership.status !== "invited" && (
           <IconButton
             ariaLabel="copy icon"
             variant="plain"
