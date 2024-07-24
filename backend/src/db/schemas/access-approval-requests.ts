e0e21530e27f37a881e1a47d5d9ec69b2a2af2c0
Daniel Hougaard <62331820+DanielHougaard@users.noreply.github.com>
2024-07-24 19:11:51 +0200

    Schemas

--- backend/src/db/schemas/access-approval-requests.ts
+++ backend/src/db/schemas/access-approval-requests.ts
@@ -11,12 +11,12 @@ export const AccessApprovalRequestsSchema = z.object({
   id: z.string().uuid(),
   policyId: z.string().uuid(),
   privilegeId: z.string().uuid().nullable().optional(),
-  requestedBy: z.string().uuid(),
   isTemporary: z.boolean(),
   temporaryRange: z.string().nullable().optional(),
   permissions: z.unknown(),
   createdAt: z.date(),
-  updatedAt: z.date()
+  updatedAt: z.date(),
+  requestedByUserId: z.string().uuid()
 });
 
 export type TAccessApprovalRequests = z.infer<typeof AccessApprovalRequestsSchema>;
