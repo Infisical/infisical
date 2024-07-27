15749a1f52deba7c22c3b0254ac48ad53e086d51
lemmyMwaura <lemmymwauracodes@gmail.com>
2024-07-27 22:18:56 +0300

    feat: update url onvalue change

--- frontend/src/views/Project/MembersPage/MembersPage.tsx
+++ frontend/src/views/Project/MembersPage/MembersPage.tsx
@@ -22,12 +22,19 @@ export const MembersPage = withProjectPermission(
       }
     }, [selectedTab]);
 
+    const updateSelectedTab = (tab: TabSections) => {
+      router.push({
+        pathname: router.pathname,
+        query: { ...router.query, selectedTab: tab },
+      });
+    }
+
     return (
       <div className="container mx-auto flex flex-col justify-between bg-bunker-800 text-white">
         <div className="mx-auto mb-6 w-full max-w-7xl py-6 px-6">
           <div>{selectedTab}</div>
           <p className="mr-4 mb-4 text-3xl font-semibold text-white">Project Access Control</p>
-          <Tabs value={activeTab} onValueChange={setActiveTab}>
+          <Tabs value={activeTab} onValueChange={updateSelectedTab}>
             <TabList>
               <Tab value={TabSections.Member}>Users</Tab>
               <Tab value={TabSections.Identities}>
