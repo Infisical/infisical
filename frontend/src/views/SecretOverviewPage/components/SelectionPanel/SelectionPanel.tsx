de886f8dd07db304b1e8a0e4f726a5c60f01c459
lemmyMwaura <lemmymwauracodes@gmail.com>
2024-07-27 12:27:06 +0300

    feat: make title dynamic when deleting folders and secrets

--- frontend/src/views/SecretOverviewPage/components/SelectionPanel/SelectionPanel.tsx
+++ frontend/src/views/SecretOverviewPage/components/SelectionPanel/SelectionPanel.tsx
@@ -49,8 +49,9 @@ export const SelectionPanel = ({
     "bulkDeleteEntries"
   ] as const);
 
-  const selectedCount =
-    Object.keys(selectedEntries.folder).length + Object.keys(selectedEntries.secret).length;
+  const selectedFolderCount = Object.keys(selectedEntries.folder).length
+  const selectedKeysCount = Object.keys(selectedEntries.secret).length
+  const selectedCount = selectedFolderCount + selectedKeysCount
 
   const { currentWorkspace } = useWorkspace();
   const workspaceId = currentWorkspace?.id || "";
@@ -67,6 +68,16 @@ export const SelectionPanel = ({
       subject(ProjectPermissionSub.Secrets, { environment: env.slug, secretPath })
     )
   );
+
+  const getDeleteModalTitle = () => {
+    if (selectedFolderCount > 0 && selectedKeysCount > 0) {
+      return "Do you want to delete the selected secrets and folders across envs?";
+    } else if (selectedFolderCount > 0 && selectedKeysCount === 0) {
+      return "Do you want to delete the selected folders across envs?";
+    } else {
+      return "Do you want to delete the selected secrets across envs?";
+    }
+  }
 
   const handleBulkDelete = async () => {
     let processedEntries = 0;
@@ -180,7 +191,7 @@ export const SelectionPanel = ({
       <DeleteActionModal
         isOpen={popUp.bulkDeleteEntries.isOpen}
         deleteKey="delete"
-        title="Do you want to delete the selected secrets and folders across envs?"
+        title={getDeleteModalTitle()}
         onChange={(isOpen) => handlePopUpToggle("bulkDeleteEntries", isOpen)}
         onDeleteApproved={handleBulkDelete}
       />
