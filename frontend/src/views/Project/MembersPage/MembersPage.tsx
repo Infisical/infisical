3f3e41282dfc11d608820da5454a66cb7baa3a2d
lemmyMwaura <lemmymwauracodes@gmail.com>
2024-07-28 20:33:17 +0300

    fix: remove unnecessary selectedTab div

--- frontend/src/views/Project/MembersPage/MembersPage.tsx
+++ frontend/src/views/Project/MembersPage/MembersPage.tsx
@@ -33,7 +33,6 @@ export const MembersPage = withProjectPermission(
     return (
       <div className="container mx-auto flex flex-col justify-between bg-bunker-800 text-white">
         <div className="mx-auto mb-6 w-full max-w-7xl py-6 px-6">
-          <div>{selectedTab}</div>
           <p className="mr-4 mb-4 text-3xl font-semibold text-white">Project Access Control</p>
           <Tabs value={activeTab} onValueChange={updateSelectedTab}>
             <TabList>
