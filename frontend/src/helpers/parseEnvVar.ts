1aee50a7518af57aaac354b634e64cbdbdc4be46
Daniel Hougaard <daniel@infisical.com>
2024-08-03 03:29:45 +0200

    Fix: Parser improvements and lint fixes

--- frontend/src/helpers/parseEnvVar.ts
+++ frontend/src/helpers/parseEnvVar.ts
@@ -1,26 +1,14 @@
 /** Extracts the key and value from a passed in env string based on the provided delimiters. */
 export const getKeyValue = (pastedContent: string, delimiters: string[]) => {
-  let splitIndex = -1
-  let key = ""
-  let value = ""
+  const foundDelimiter = delimiters.find((delimiter) => pastedContent.includes(delimiter));
 
-  for (const delimiter of delimiters) {
-    const idx = pastedContent.indexOf(delimiter)
-
-    if (idx !== -1) {
-      splitIndex = idx
-      break;
-    }
+  if (!foundDelimiter) {
+    return { key: pastedContent.trim(), value: "" };
   }
 
-  // if only key is pasted
-  if (splitIndex === -1) {
-    key = pastedContent.trim()
-    return { key, value: "" }
-  }
-
-  key = pastedContent.slice(0, splitIndex).trim()
-  value = pastedContent.slice(splitIndex + 1).trim()
-
-  return { key, value }
+  const [key, value] = pastedContent.split(foundDelimiter);
+  return {
+    key: key.trim(),
+    value: (value ?? "").trim()
+  };
 };
