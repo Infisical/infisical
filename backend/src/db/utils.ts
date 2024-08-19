1e5f0ef07634a8b102f0983c583f5d6592664de3
Ayush Dutt Sharma <duttsharmarahul17@gmail.com>
2024-08-19 22:09:44 +0530

    Removed All console logs

--- backend/src/db/utils.ts
+++ backend/src/db/utils.ts
@@ -20,18 +20,15 @@ export const createJunctionTable = async (
     indexForeignKeys = false
   } = options;
 
-  // Check if the table already exists
   const tableExists = await knex.schema.hasTable(tableName);
   
   if (!tableExists) {
     await knex.schema.createTable(tableName, (table) => {
       table.uuid("id", { primaryKey: true }).defaultTo(knex.fn.uuid());
       
-      // Create foreign key columns
       table.uuid(`${table1Name}Id`).unsigned().notNullable();
       table.uuid(`${table2Name}Id`).unsigned().notNullable();
       
-      // Set up foreign key constraints
       table.foreign(`${table1Name}Id`)
         .references(table1PrimaryKey)
         .inTable(table1Name)
@@ -42,21 +39,13 @@ export const createJunctionTable = async (
         .inTable(table2Name)
         .onDelete("CASCADE");
       
-      // Add indexes on foreign key columns if specified
       if (indexForeignKeys) {
         table.index(`${table1Name}Id`);
         table.index(`${table2Name}Id`);
       }
     });
-    
-    console.log(`Junction table ${tableName} created successfully.`);
-  } else {
-    console.log(`Table ${tableName} already exists. Skipping creation.`);
   }
 };
-
-// one time logic
-// this is a postgres function log to set updateAt to present time whenever row gets updated
 
 export const createUpdateAtTriggerFunction = async (knex: Knex): Promise<void> => {
   await knex.raw(`
@@ -65,14 +54,12 @@ export const createUpdateAtTriggerFunction = async (knex: Knex): Promise<void> =
     END;
     $$ LANGUAGE plpgsql;
   `);
-  console.log(`Update timestamp function 'on_update_timestamp' created or replaced successfully.`);
 };
 
 export const dropUpdatedAtTriggerFunction = async (knex: Knex): Promise<void> => {
   await knex.raw(`
     DROP FUNCTION IF EXISTS on_update_timestamp() CASCADE;
   `);
-  console.log(`Update timestamp function 'on_update_timestamp' dropped successfully.`);
 };
 
 export const createOnUpdateTrigger = async (knex: Knex, tableName: string): Promise<void> => {
@@ -82,10 +69,8 @@ export const createOnUpdateTrigger = async (knex: Knex, tableName: string): Prom
     FOR EACH ROW
     EXECUTE PROCEDURE on_update_timestamp();
   `);
-  console.log(`Update trigger for table ${tableName} created successfully.`);
 };
 
 export const dropOnUpdateTrigger = async (knex: Knex, tableName: string): Promise<void> => {
   await knex.raw(`DROP TRIGGER IF EXISTS "${tableName}_updatedAt" ON ${tableName}`);
-  console.log(`Update trigger for table ${tableName} dropped successfully.`);
 };
