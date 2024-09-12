4de33190a98295f3b858d15de41b86d1456af4ef
Daniel Hougaard <daniel@infisical.com>
2024-09-12 11:12:45 +0400

    Rebase fixes

--- frontend/src/hooks/api/workflowIntegrations/mutation.tsx
+++ frontend/src/hooks/api/workflowIntegrations/mutation.tsx
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { apiRequest } from "@app/config/request";
 
-import { workspaceKeys } from "../workspace/queries";
+import { workspaceKeys } from "../workspace/query-keys";
 import { workflowIntegrationKeys } from "./queries";
 import {
   TDeleteSlackIntegrationDTO,
