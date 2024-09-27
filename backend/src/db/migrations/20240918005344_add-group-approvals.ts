1992a09ac2ea6b8e6a22a7779c35cb863484cbe2
Meet <meetcshah19@gmail.com>
2024-09-28 03:20:02 +0530

    chore: lint fix

--- backend/src/db/migrations/20240918005344_add-group-approvals.ts
+++ backend/src/db/migrations/20240918005344_add-group-approvals.ts
@@ -3,30 +3,36 @@ import { Knex } from "knex";
 import { TableName } from "../schemas";
 
 export async function up(knex: Knex): Promise<void> {
-  const hasAccessApproverGroupId = await knex.schema.hasColumn(TableName.AccessApprovalPolicyApprover, "approverGroupId");
+  const hasAccessApproverGroupId = await knex.schema.hasColumn(
+    TableName.AccessApprovalPolicyApprover,
+    "approverGroupId"
+  );
   const hasAccessApproverUserId = await knex.schema.hasColumn(TableName.AccessApprovalPolicyApprover, "approverUserId");
-  const hasSecretApproverGroupId = await knex.schema.hasColumn(TableName.SecretApprovalPolicyApprover, "approverGroupId");
+  const hasSecretApproverGroupId = await knex.schema.hasColumn(
+    TableName.SecretApprovalPolicyApprover,
+    "approverGroupId"
+  );
   const hasSecretApproverUserId = await knex.schema.hasColumn(TableName.SecretApprovalPolicyApprover, "approverUserId");
   if (await knex.schema.hasTable(TableName.AccessApprovalPolicyApprover)) {
     await knex.schema.alterTable(TableName.AccessApprovalPolicyApprover, (table) => {
       // add column approverGroupId to AccessApprovalPolicyApprover
-      if(!hasAccessApproverGroupId) {
+      if (!hasAccessApproverGroupId) {
         table.uuid("approverGroupId").nullable().references("id").inTable(TableName.Groups).onDelete("CASCADE");
       }
 
       // make approverUserId nullable
-      if(hasAccessApproverUserId){
+      if (hasAccessApproverUserId) {
         table.uuid("approverUserId").nullable().alter();
       }
     });
     await knex.schema.alterTable(TableName.SecretApprovalPolicyApprover, (table) => {
       // add column approverGroupId to SecretApprovalPolicyApprover
-      if(!hasSecretApproverGroupId) {
+      if (!hasSecretApproverGroupId) {
         table.uuid("approverGroupId").nullable().references("id").inTable(TableName.Groups).onDelete("CASCADE");
       }
 
       // make approverUserId nullable
-      if(hasSecretApproverUserId){
+      if (hasSecretApproverUserId) {
         table.uuid("approverUserId").nullable().alter();
       }
     });
@@ -34,29 +40,35 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  const hasAccessApproverGroupId = await knex.schema.hasColumn(TableName.AccessApprovalPolicyApprover, "approverGroupId");
+  const hasAccessApproverGroupId = await knex.schema.hasColumn(
+    TableName.AccessApprovalPolicyApprover,
+    "approverGroupId"
+  );
   const hasAccessApproverUserId = await knex.schema.hasColumn(TableName.AccessApprovalPolicyApprover, "approverUserId");
-  const hasSecretApproverGroupId = await knex.schema.hasColumn(TableName.SecretApprovalPolicyApprover, "approverGroupId");
+  const hasSecretApproverGroupId = await knex.schema.hasColumn(
+    TableName.SecretApprovalPolicyApprover,
+    "approverGroupId"
+  );
   const hasSecretApproverUserId = await knex.schema.hasColumn(TableName.SecretApprovalPolicyApprover, "approverUserId");
 
   if (await knex.schema.hasTable(TableName.AccessApprovalPolicyApprover)) {
     await knex.schema.alterTable(TableName.AccessApprovalPolicyApprover, (table) => {
-      if(hasAccessApproverGroupId){
+      if (hasAccessApproverGroupId) {
         table.dropColumn("approverGroupId");
       }
       // make approverUserId not nullable
-      if(hasAccessApproverUserId){
+      if (hasAccessApproverUserId) {
         table.uuid("approverUserId").notNullable().alter();
       }
     });
 
     // remove
     await knex.schema.alterTable(TableName.SecretApprovalPolicyApprover, (table) => {
-      if(hasSecretApproverGroupId){
+      if (hasSecretApproverGroupId) {
         table.dropColumn("approverGroupId");
       }
       // make approverUserId not nullable
-      if(hasSecretApproverUserId){
+      if (hasSecretApproverUserId) {
         table.uuid("approverUserId").notNullable().alter();
       }
     });
