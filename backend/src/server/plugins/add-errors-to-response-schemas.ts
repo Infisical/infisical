55ca9149d5bd19ae3e2dddccb6471acd76b89812
Daniel Hougaard <daniel@infisical.com>
2024-09-27 19:08:43 +0400

    Re-trigger tests

--- backend/src/server/plugins/add-errors-to-response-schemas.ts
+++ backend/src/server/plugins/add-errors-to-response-schemas.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import fp from "fastify-plugin";
 
 import { DefaultResponseErrorsSchema } from "../routes/sanitizedSchemas";
@@ -5,15 +6,12 @@ import { DefaultResponseErrorsSchema } from "../routes/sanitizedSchemas";
 export const addErrorsToResponseSchemas = fp(async (server) => {
   server.addHook("onRoute", (routeOptions) => {
     if (routeOptions.schema && routeOptions.schema.response) {
-      // eslint-disable-next-line no-param-reassign
       routeOptions.schema.response = {
         ...DefaultResponseErrorsSchema,
         ...routeOptions.schema.response
       };
     } else {
-      // eslint-disable-next-line no-param-reassign
       routeOptions.schema = routeOptions.schema || {};
-      // eslint-disable-next-line no-param-reassign
       routeOptions.schema.response = { ...DefaultResponseErrorsSchema };
     }
   });
