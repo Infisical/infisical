a4091bfcdd9deb0f6b2bb5f54948c6efc1ff739a
= <akhilmhdh@gmail.com>
2024-10-02 00:19:56 +0530

    feat: removed console in test

--- backend/e2e-test/routes/v3/secrets.spec.ts
+++ backend/e2e-test/routes/v3/secrets.spec.ts
@@ -812,8 +812,6 @@ describe.each([{ auth: AuthMode.JWT }, { auth: AuthMode.IDENTITY_ACCESS_TOKEN }]
         },
         body: createSecretReqBody
       });
-      console.log(">>>>>>>>>>");
-      console.log(createSecRes.payload);
       expect(createSecRes.statusCode).toBe(200);
       const createdSecretPayload = JSON.parse(createSecRes.payload);
       expect(createdSecretPayload).toHaveProperty("secret");
