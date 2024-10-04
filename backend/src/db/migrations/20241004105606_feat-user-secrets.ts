9cd15cdf40ac9c9a1b17ea2f279c45314e974f4b
Hitesh Wadhwani <hiteshwadhwani1403@gmail.com>
2024-10-04 18:20:46 +0530

    updated schema

--- backend/src/db/migrations/20241004105606_feat-user-secrets.ts
+++ backend/src/db/migrations/20241004105606_feat-user-secrets.ts
@@ -12,9 +12,9 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable(TableName.UserSecrets))) {
     await knex.schema.createTable(TableName.UserSecrets, (t) => {
       t.uuid("id", { primaryKey: true }).defaultTo(knex.fn.uuid());
-      t.uuid("userId");
+      t.uuid("userId").notNullable();
       t.foreign("userId").references("id").inTable(TableName.Users).onDelete("CASCADE");
-      t.uuid("orgId");
+      t.uuid("orgId").notNullable();
       t.foreign("orgId").references("id").inTable(TableName.Organization).onDelete("CASCADE");
       t.dateTime("createdAt").defaultTo(knex.fn.now());
     });
@@ -23,7 +23,7 @@ export async function up(knex: Knex): Promise<void> {
   if (!(await knex.schema.hasTable(TableName.UserSecretCredentials))) {
     await knex.schema.createTable(TableName.UserSecretCredentials, (t) => {
       t.uuid("id", { primaryKey: true }).defaultTo(knex.fn.uuid());
-      t.uuid("credentialId");
+      t.uuid("credentialId").notNullable();
       t.foreign("credentialId").references("id").inTable(TableName.UserSecrets).onDelete("CASCADE");
       t.enum("credentialType", Object.values(CredentialTypes)).notNullable();
       t.string("name").notNullable();
