771cc7179e75a253fb53457e84ab9fe1f202a62b
Daniel Hougaard <daniel@infisical.com>
2024-10-18 04:28:01 +0400

    Update identities.ts

--- backend/src/db/schemas/identities.ts
+++ backend/src/db/schemas/identities.ts
@@ -10,7 +10,7 @@ import { TImmutableDBKeys } from "./models";
 export const IdentitiesSchema = z.object({
   id: z.string().uuid(),
   name: z.string(),
-  // authMethod: z.string().nullable().optional(),
+  authMethod: z.string().nullable().optional(),
   createdAt: z.date(),
   updatedAt: z.date()
 });
