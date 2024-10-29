cfc0b2fb8ddde1160a82bc6cc7e15317ae215c1d
Daniel Hougaard <daniel@infisical.com>
2024-10-29 22:07:02 +0400

    fix: renamed migration

--- backend/src/db/migrations/20241028134337_kms-root-cfg-hsm.ts
+++ backend/src/db/migrations/20241028134337_kms-root-cfg-hsm.ts
@@ -3,21 +3,21 @@ import { Knex } from "knex";
 import { TableName } from "../schemas";
 
 export async function up(knex: Knex): Promise<void> {
-  const hasIsEncryptedByHsmCol = await knex.schema.hasColumn(TableName.KmsServerRootConfig, "isEncryptedByHsm");
+  const hasEncryptionStrategy = await knex.schema.hasColumn(TableName.KmsServerRootConfig, "encryptionStrategy");
   const hasTimestampsCol = await knex.schema.hasColumn(TableName.KmsServerRootConfig, "createdAt");
 
   await knex.schema.alterTable(TableName.KmsServerRootConfig, (t) => {
-    if (!hasIsEncryptedByHsmCol) t.boolean("isEncryptedByHsm").defaultTo(false).notNullable();
-    if (!hasTimestampsCol) t.timestamps(true, true);
+    if (!hasEncryptionStrategy) t.string("encryptionStrategy").defaultTo("BASIC");
+    if (!hasTimestampsCol) t.timestamps(true, true, true);
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  const hasIsEncryptedByHsmCol = await knex.schema.hasColumn(TableName.KmsServerRootConfig, "isEncryptedByHsm");
+  const hasEncryptionStrategy = await knex.schema.hasColumn(TableName.KmsServerRootConfig, "encryptionStrategy");
   const hasTimestampsCol = await knex.schema.hasColumn(TableName.KmsServerRootConfig, "createdAt");
 
   await knex.schema.alterTable(TableName.KmsServerRootConfig, (t) => {
-    if (hasIsEncryptedByHsmCol) t.dropColumn("isEncryptedByHsm");
+    if (hasEncryptionStrategy) t.dropColumn("encryptionStrategy");
     if (hasTimestampsCol) t.dropTimestamps(true);
   });
 }
