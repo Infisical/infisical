5f14b27f41e065f33c0f29d4aed27fc289c06a76
areifert <areifert@users.noreply.github.com>
2024-10-31 13:29:43 -0600

    Make Azure DevOps variables secret

--- backend/src/services/integration-auth/integration-sync-secret.ts
+++ backend/src/services/integration-auth/integration-sync-secret.ts
@@ -2540,9 +2540,9 @@ const syncSecretsAzureDevops = async ({
 
   const { groupId, groupName } = await getEnvGroupId(integration.app, integration.appId, integration.environment.name);
 
-  const variables: Record<string, { value: string }> = {};
+  const variables: Record<string, { value: string, isSecret: boolean }> = {};
   for (const key of Object.keys(secrets)) {
-    variables[key] = { value: secrets[key].value };
+    variables[key] = { value: secrets[key].value, isSecret: true };
   }
 
   if (!groupId) {
