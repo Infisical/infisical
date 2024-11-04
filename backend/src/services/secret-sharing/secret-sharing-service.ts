de76f30e9dd1dfe71514cd5a2063fe40e606e6c9
Shaharyar Shamshi <shaharyarshamshi@gmail.com>
2024-11-04 12:05:53 +0530

    fix the softdelete issue for the shared secret get by id

--- backend/src/services/secret-sharing/secret-sharing-service.ts
+++ backend/src/services/secret-sharing/secret-sharing-service.ts
@@ -211,9 +211,14 @@ export const secretSharingServiceFactory = ({
 
     // all secrets pass through here, meaning we check if its expired first and then check if it needs verification
     // or can be safely sent to the client.
+
     if (expiresAt !== null && expiresAt < new Date()) {
       // check lifetime expiry
-      await secretSharingDAL.softDeleteById(sharedSecretId);
+      if (isUuidV4(sharedSecretId)) {
+        await secretSharingDAL.softDeleteById(sharedSecretId);
+      } else {
+        await secretSharingDAL.softDeleteByIdentifier(Buffer.from(sharedSecretId, "base64url").toString("hex"));
+      }
       throw new ForbiddenRequestError({
         message: "Access denied: Secret has expired by lifetime"
       });
