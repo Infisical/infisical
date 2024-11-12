3b02eedca69ae217929daeefa512432f899e4bb7
Daniel Hougaard <daniel@infisical.com>
2024-11-12 20:36:09 +0400

    feat: npm CLI

---
+++ npm/README.md
@@ -0,0 +1,45 @@
+<h1 align="center">Infisical</h1>
+<p align="center">
+  <p align="center"><b>The open-source secret management platform</b>: Sync secrets/configs across your team/infrastructure and prevent secret leaks.</p>
+</p>
+
+<h4 align="center">
+  <a href="https://infisical.com/slack">Slack</a> |
+  <a href="https://infisical.com/">Infisical Cloud</a> |
+  <a href="https://infisical.com/docs/self-hosting/overview">Self-Hosting</a> |
+  <a href="https://infisical.com/docs/documentation/getting-started/introduction">Docs</a> |
+  <a href="https://www.infisical.com">Website</a> |
+  <a href="https://infisical.com/careers">Hiring (Remote/SF)</a>
+</h4>
+
+
+<h4 align="center">
+  <a href="https://github.com/Infisical/infisical/blob/main/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="Infisical is released under the MIT license." />
+  </a>
+  <a href="https://github.com/infisical/infisical/blob/main/CONTRIBUTING.md">
+    <img src="https://img.shields.io/badge/PRs-Welcome-brightgreen" alt="PRs welcome!" />
+  </a>
+  <a href="https://github.com/Infisical/infisical/issues">
+    <img src="https://img.shields.io/github/commit-activity/m/infisical/infisical" alt="git commit activity" />
+  </a>
+  <a href="https://cloudsmith.io/~infisical/repos/">
+    <img src="https://img.shields.io/badge/Downloads-6.95M-orange" alt="Cloudsmith downloads" />
+  </a>
+  <a href="https://infisical.com/slack">
+    <img src="https://img.shields.io/badge/chat-on%20Slack-blueviolet" alt="Slack community channel" />
+  </a>
+  <a href="https://twitter.com/infisical">
+    <img src="https://img.shields.io/twitter/follow/infisical?label=Follow" alt="Infisical Twitter" />
+  </a>
+</h4>
+
+## Introduction
+
+**[Infisical](https://infisical.com)** is the open source secret management platform that teams use to centralize their application configuration and secrets like API keys and database credentials as well as manage their internal PKI.
+
+We're on a mission to make security tooling more accessible to everyone, not just security teams, and that means redesigning the entire developer experience from ground up.
+
+## [CLI Documentation](https://infisical.com/docs/cli/usage)
+
+
--- npm/src/index.cjs
+++ npm/src/index.cjs
@@ -1,4 +1,4 @@
-const { execSync } = require("child_process");
+const childProcess = require("child_process");
 const fs = require("fs");
 const stream = require("node:stream");
 const tar = require("tar");
@@ -10,79 +10,76 @@ const supportedPlatforms = ["linux", "darwin", "win32", "freebsd"];
 const outputDir = "bin";
 
 const getPlatform = () => {
-  const platform = process.platform;
-  if (!supportedPlatforms.includes(platform)) {
-    console.error(
-      "Your platform doesn't seem to be of type darwin, linux or windows"
-    );
-    process.exit(1);
-  }
-  return platform;
+	const platform = process.platform;
+	if (!supportedPlatforms.includes(platform)) {
+		console.error("Your platform doesn't seem to be of type darwin, linux or windows");
+		process.exit(1);
+	}
+	return platform;
 };
 
 const getArchitecture = () => {
-  const architecture = execSync("uname -m").toString().trim();
-  let arch = "";
-  if (architecture === "x86_64" || architecture === "amd64") {
-    arch = "amd64";
-  } else if (architecture === "arm64" || architecture === "aarch64") {
-    arch = "arm64";
-  } else if (architecture.startsWith("armv5")) {
-    arch = "armv5";
-  } else if (architecture.startsWith("armv6")) {
-    arch = "armv6";
-  } else if (architecture.startsWith("armv7")) {
-    arch = "armv7";
-  } else if (architecture === "i386" || architecture === "i686") {
-    arch = "i386";
-  } else {
-    console.error(
-      "Your architecture doesn't seem to be supported. Your architecture is",
-      architecture
-    );
-    process.exit(1);
-  }
-  return arch;
+	const architecture = childProcess.execSync("uname -m").toString().trim();
+	let arch = "";
+	if (architecture === "x86_64" || architecture === "amd64") {
+		arch = "amd64";
+	} else if (architecture === "arm64" || architecture === "aarch64") {
+		arch = "arm64";
+	} else if (architecture.startsWith("armv5")) {
+		arch = "armv5";
+	} else if (architecture.startsWith("armv6")) {
+		arch = "armv6";
+	} else if (architecture.startsWith("armv7")) {
+		arch = "armv7";
+	} else if (architecture === "i386" || architecture === "i686") {
+		arch = "i386";
+	} else {
+		console.error("Your architecture doesn't seem to be supported. Your architecture is", architecture);
+		process.exit(1);
+	}
+	return arch;
 };
 
-function main() {
-  const PLATFORM = getPlatform();
-  const ARCH = getArchitecture();
-  const NUMERIC_RELEASE_VERSION = packageJSON.version;
-  const LATEST_RELEASE_VERSION = `v${NUMERIC_RELEASE_VERSION}`;
-  const downloadLink = `https://github.com/Infisical/infisical/releases/download/infisical-cli/${LATEST_RELEASE_VERSION}/infisical_${NUMERIC_RELEASE_VERSION}_${PLATFORM}_${ARCH}.tar.gz`;
+async function main() {
+	const PLATFORM = getPlatform();
+	const ARCH = getArchitecture();
+	const NUMERIC_RELEASE_VERSION = packageJSON.version;
+	const LATEST_RELEASE_VERSION = `v${NUMERIC_RELEASE_VERSION}`;
+	const downloadLink = `https://github.com/Infisical/infisical/releases/download/infisical-cli/${LATEST_RELEASE_VERSION}/infisical_${NUMERIC_RELEASE_VERSION}_${PLATFORM}_${ARCH}.tar.gz`;
 
-  if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
-    fetch(downloadLink, {
-      headers: {
-        Accept: "application/octet-stream",
-      },
-    })
-      .then(async (response) => {
-        if (!response.ok) {
-          throw new Error(
-            `Failed to fetch: ${response.status} - ${response.statusText}`
-          );
-        }
+	// Ensure the output directory exists
+	if (!fs.existsSync(outputDir)) {
+		fs.mkdirSync(outputDir);
+	}
 
-        return new Promise((resolve, reject) => {
-          const outStream = stream.Readable.fromWeb(response.body)
-            .pipe(zlib.createGunzip())
-            .pipe(
-              tar.x({
-                C: path.join(outputDir),
-                filter: (path) => path === "infisical",
-              })
-            );
+	// Download the latest CLI binary
+	try {
+		const response = await fetch(downloadLink, {
+			headers: {
+				Accept: "application/octet-stream"
+			}
+		});
 
-          outStream.on("error", reject);
-          outStream.on("close", resolve);
-        });
-      })
-      .catch((error) => {
-        console.error("Error downloading or extracting the asset:", error);
-      });
-  }
+		if (!response.ok) {
+			throw new Error(`Failed to fetch: ${response.status} - ${response.statusText}`);
+		}
+
+		return await new Promise((resolve, reject) => {
+			const outStream = stream.Readable.fromWeb(response.body)
+				.pipe(zlib.createGunzip())
+				.pipe(
+					tar.x({
+						C: path.join(outputDir),
+						filter: path => path === "infisical"
+					})
+				);
+
+			outStream.on("error", reject);
+			outStream.on("close", resolve);
+		});
+	} catch (error) {
+		console.error("Error downloading or extracting the asset:", error);
+		process.exit(1);
+	}
 }
 main();
