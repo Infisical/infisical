f111a2cb9f2d41e307aaa5ebb54434f2b8431caa
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2024-12-22 21:05:23 -0500

    add to route registration

--- backend/src/server/routes/v1/index.ts
+++ backend/src/server/routes/v1/index.ts
@@ -40,6 +40,7 @@ import { registerSsoRouter } from "./sso-router";
 import { registerUserActionRouter } from "./user-action-router";
 import { registerUserEngagementRouter } from "./user-engagement-router";
 import { registerUserRouter } from "./user-router";
+import { registerUserSecretRouter } from "./user-secret-router";
 import { registerWebhookRouter } from "./webhook-router";
 import { registerWorkflowIntegrationRouter } from "./workflow-integration-router";
 
@@ -107,6 +108,7 @@ export const registerV1Routes = async (server: FastifyZodProvider) => {
   await server.register(registerWebhookRouter, { prefix: "/webhooks" });
   await server.register(registerIdentityRouter, { prefix: "/identities" });
   await server.register(registerSecretSharingRouter, { prefix: "/secret-sharing" });
+  await server.register(registerUserSecretRouter, { prefix: "/user-secret" });
   await server.register(registerUserEngagementRouter, { prefix: "/user-engagement" });
   await server.register(registerDashboardRouter, { prefix: "/dashboard" });
   await server.register(registerCmekRouter, { prefix: "/kms" });
