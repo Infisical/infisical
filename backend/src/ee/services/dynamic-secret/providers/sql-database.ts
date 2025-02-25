c7fc9a3e8c9522c266f1e7ae4693ba107f28aaa7
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2025-02-25 18:42:49 +0900

    misc: add proper ssl verification for private certs

--- backend/src/ee/services/dynamic-secret/providers/sql-database.ts
+++ backend/src/ee/services/dynamic-secret/providers/sql-database.ts
@@ -33,7 +33,7 @@ export const SqlDatabaseProvider = (): TDynamicProviderFns => {
   };
 
   const $getClient = async (providerInputs: z.infer<typeof DynamicSecretSqlDBSchema>) => {
-    const ssl = providerInputs.ca ? { rejectUnauthorized: false, ca: providerInputs.ca } : undefined;
+    const ssl = providerInputs.ca ? { ca: providerInputs.ca } : undefined;
     const isMsSQLClient = providerInputs.client === SqlProviders.MsSQL;
 
     const db = knex({
