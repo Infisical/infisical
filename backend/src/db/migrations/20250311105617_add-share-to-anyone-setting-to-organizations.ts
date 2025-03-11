71b8e3dbce798aaff42e48780101ab04927c7c15
carlosmonastyrski <charly.monastyrski@gmail.com>
2025-03-11 13:44:42 -0300

    Fix migration column name on check variable

--- backend/src/db/migrations/20250311105617_add-share-to-anyone-setting-to-organizations.ts
+++ backend/src/db/migrations/20250311105617_add-share-to-anyone-setting-to-organizations.ts
@@ -4,7 +4,7 @@ import { TableName } from "../schemas";
 
 export async function up(knex: Knex): Promise<void> {
   if (await knex.schema.hasTable(TableName.Organization)) {
-    const hasSecretShareToAnyoneCol = await knex.schema.hasColumn(TableName.Organization, "secretShareToAnyone");
+    const hasSecretShareToAnyoneCol = await knex.schema.hasColumn(TableName.Organization, "secretShareSendToAnyone");
 
     if (!hasSecretShareToAnyoneCol) {
       await knex.schema.alterTable(TableName.Organization, (t) => {
@@ -16,7 +16,7 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
   if (await knex.schema.hasTable(TableName.Organization)) {
-    const hasSecretShareToAnyoneCol = await knex.schema.hasColumn(TableName.Organization, "secretShareToAnyone");
+    const hasSecretShareToAnyoneCol = await knex.schema.hasColumn(TableName.Organization, "secretShareSendToAnyone");
     if (hasSecretShareToAnyoneCol) {
       await knex.schema.alterTable(TableName.Organization, (t) => {
         t.dropColumn("secretShareSendToAnyone");
