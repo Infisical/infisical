0b55ac141cf58722bc5d43559a4f121ff375c79a
Mahyar Mirrashed <mah.mirr@gmail.com>
2025-03-13 11:41:02 -0700

    refactor(projects): rename workspace to project

--- cli/packages/util/projects.go
+++ cli/packages/util/projects.go
@@ -5,12 +5,12 @@ import (
 	"github.com/go-resty/resty/v2"
 )
 
-func GetProjectDetails(accessToken string, workspaceId string) (api.Project, error) {
+func GetProjectDetails(accessToken string, projectId string) (api.Project, error) {
 	httpClient := resty.New()
 	httpClient.SetAuthToken(accessToken).
 		SetHeader("Accept", "application/json")
 
-	res, err := api.CallGetProjectById(httpClient, workspaceId)
+	res, err := api.CallGetProjectById(httpClient, projectId)
 	if err != nil {
 		return api.Project{}, err
 	}
