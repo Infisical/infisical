c95dd69167b288871608996fef7e8ff02e6f64da
Maidul Islam <maidul98@gmail.com>
2025-04-08 16:58:35 -0400

    test upload

--- cli/upload_to_cloudsmith.sh
+++ cli/upload_to_cloudsmith.sh
@@ -1,13 +1,13 @@
 cd dist
-for i in *.apk; do
-    [ -f "$i" ] || break
-    cloudsmith push alpine --republish infisical/infisical-cli/alpine/any-version $i
-done
+# for i in *.apk; do
+#     [ -f "$i" ] || break
+#     cloudsmith push alpine --republish infisical/infisical-cli/alpine/any-version $i
+# done
 
-for i in *.deb; do
-    [ -f "$i" ] || break
-    cloudsmith push deb --republish infisical/infisical-cli/any-distro/any-version $i
-done
+# for i in *.deb; do
+#     [ -f "$i" ] || break
+#     cloudsmith push deb --republish infisical/infisical-cli/any-distro/any-version $i
+# done
 
 for i in *.deb; do
     [ -f "$i" ] || break
@@ -15,7 +15,7 @@ for i in *.deb; do
 done
 
 
-for i in *.rpm; do
-    [ -f "$i" ] || break
-    cloudsmith push rpm --republish infisical/infisical-cli/any-distro/any-version $i
-done
+# for i in *.rpm; do
+#     [ -f "$i" ] || break
+#     cloudsmith push rpm --republish infisical/infisical-cli/any-distro/any-version $i
+# done
