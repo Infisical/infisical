b9d74e0aedbb0e00715b6f3377e74be6f95a0a20
Daniel Hougaard <daniel@infisical.com>
2025-04-14 21:36:16 +0400

    requested changes

--- backend/src/db/migrations/20250402000941_add-type-to-kms-keys.ts
+++ backend/src/db/migrations/20250402000941_add-type-to-kms-keys.ts
@@ -7,17 +7,19 @@ import { TableName } from "../schemas";
 export async function up(knex: Knex): Promise<void> {
   const hasKeyUsageColumn = await knex.schema.hasColumn(TableName.KmsKey, "keyUsage");
 
-  await knex.schema.alterTable(TableName.KmsKey, (t) => {
-    if (!hasKeyUsageColumn) t.string("keyUsage").notNullable().defaultTo(KmsKeyUsage.ENCRYPT_DECRYPT);
-  });
+  if (!hasKeyUsageColumn) {
+    await knex.schema.alterTable(TableName.KmsKey, (t) => {
+      t.string("keyUsage").notNullable().defaultTo(KmsKeyUsage.ENCRYPT_DECRYPT);
+    });
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
   const hasKeyUsageColumn = await knex.schema.hasColumn(TableName.KmsKey, "keyUsage");
 
-  await knex.schema.alterTable(TableName.KmsKey, (t) => {
-    if (hasKeyUsageColumn) {
+  if (hasKeyUsageColumn) {
+    await knex.schema.alterTable(TableName.KmsKey, (t) => {
       t.dropColumn("keyUsage");
-    }
-  });
+    });
+  }
 }
