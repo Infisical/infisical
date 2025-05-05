54927454bf88dc73f55b8f56eb28ccc29ef69d04
Andrey Lyubavin <andrey@MacBook-Pro.local>
2025-05-05 14:37:20 -0400

    ui fetch private key if permission allows it

--- frontend/src/pages/cert-manager/CertificatesPage/components/CertificateCertModal.tsx
+++ frontend/src/pages/cert-manager/CertificatesPage/components/CertificateCertModal.tsx
@@ -3,6 +3,12 @@ import { useGetCertBody } from "@app/hooks/api";
 import { UsePopUpState } from "@app/hooks/usePopUp";
 
 import { CertificateContent } from "./CertificateContent";
+import {
+  ProjectPermissionCertificateActions,
+  ProjectPermissionSub,
+  useProjectPermission
+} from "@app/context";
+import { useGetCertBundle } from "@app/hooks/api/certificates/queries";
 
 type Props = {
   popUp: UsePopUpState<["certificateCert"]>;
@@ -10,9 +16,19 @@ type Props = {
 };
 
 export const CertificateCertModal = ({ popUp, handlePopUpToggle }: Props) => {
-  const { data } = useGetCertBody(
-    (popUp?.certificateCert?.data as { serialNumber: string })?.serialNumber || ""
+  const { permission } = useProjectPermission();
+
+  const serialNumber =
+    (popUp?.certificateCert?.data as { serialNumber: string })?.serialNumber || "";
+
+  const canReadPrivateKey = permission.can(
+    ProjectPermissionCertificateActions.ReadPrivateKey,
+    ProjectPermissionSub.Certificates
   );
+
+  const { data } = canReadPrivateKey
+    ? useGetCertBundle(serialNumber)
+    : useGetCertBody(serialNumber);
 
   return (
     <Modal
@@ -27,6 +43,8 @@ export const CertificateCertModal = ({ popUp, handlePopUpToggle }: Props) => {
             serialNumber={data.serialNumber}
             certificate={data.certificate}
             certificateChain={data.certificateChain}
+            // A hacky fix for typescript error
+            privateKey={(data as { privateKey?: string }).privateKey}
           />
         ) : (
           <div />
