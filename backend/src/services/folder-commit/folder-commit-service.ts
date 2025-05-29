3148c54e18413cf19e286de74f6c8ad90076b953
Maidul Islam <maidul98@gmail.com>
2025-05-28 20:55:46 -0400

    Revert "PIT: fix checkpoint creation to do it in batches to avoid insert fails"

--- backend/src/services/folder-commit/folder-commit-service.ts
+++ backend/src/services/folder-commit/folder-commit-service.ts
@@ -295,19 +295,10 @@ export const folderCommitServiceFactory = ({
       },
       tx
     );
-
-    const batchSize = 500;
-    const chunks = chunkArray(checkpointResources, batchSize);
-
-    await Promise.all(
-      chunks.map(async (chunk) => {
-        await folderCheckpointResourcesDAL.insertMany(
-          chunk.map((resource) => ({ folderCheckpointId: newCheckpoint.id, ...resource })),
-          tx
-        );
-      })
+    await folderCheckpointResourcesDAL.insertMany(
+      checkpointResources.map((resource) => ({ folderCheckpointId: newCheckpoint.id, ...resource })),
+      tx
     );
-
     return latestCommitId;
   };
 
