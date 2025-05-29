d96e8800156149f7107edfd3609c4076f383cb3d
Maidul Islam <maidul98@gmail.com>
2025-05-29 18:26:34 -0400

    updates billing types else where

--- frontend/src/context/OrgPermissionContext/index.tsx
+++ frontend/src/context/OrgPermissionContext/index.tsx
@@ -2,6 +2,7 @@ export { useOrgPermission } from "./OrgPermissionContext";
 export type { TOrgPermission } from "./types";
 export {
   OrgPermissionActions,
+  OrgPermissionBillingActions,
   OrgPermissionGroupActions,
   OrgPermissionIdentityActions,
   OrgPermissionSubjects
