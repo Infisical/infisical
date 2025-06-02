a701635f084ca50542cc47133ca452c21b3678d8
= <akhilmhdh@gmail.com>
2025-06-02 16:23:10 +0530

    feat: remove gateway condition

--- backend/src/ee/services/dynamic-secret/dynamic-secret-fns.ts
+++ backend/src/ee/services/dynamic-secret/dynamic-secret-fns.ts
@@ -60,7 +60,7 @@ export const verifyHostInputValidity = async (host: string, isGateway = false) =
     }
   }
 
-  if (!isGateway && !(appCfg.DYNAMIC_SECRET_ALLOW_INTERNAL_IP || appCfg.ALLOW_INTERNAL_IP_CONNECTIONS)) {
+  if (!(appCfg.DYNAMIC_SECRET_ALLOW_INTERNAL_IP || appCfg.ALLOW_INTERNAL_IP_CONNECTIONS)) {
     const isInternalIp = inputHostIps.some((el) => isPrivateIp(el));
     if (isInternalIp) throw new BadRequestError({ message: "Invalid db host" });
   }
