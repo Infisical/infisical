8fb606368689f5d8a76144a5953e3c2a982807ce
Scott Wilson <scottraywilson@gmail.com>
2025-06-18 16:17:53 -0700

    improvement: better badge color

--- frontend/src/helpers/policies.ts
+++ frontend/src/helpers/policies.ts
@@ -2,11 +2,11 @@ import { PolicyType } from "@app/hooks/api/policies/enums";
 
 export const policyDetails: Record<PolicyType, { name: string; className: string }> = {
   [PolicyType.AccessPolicy]: {
-    className: "bg-yellow-500/40 text-mineshaft-100",
+    className: "bg-green/20 text-green",
     name: "Access Policy"
   },
   [PolicyType.ChangePolicy]: {
-    className: "bg-blue-500/40 text-mineshaft-100",
+    className: "bg-yellow/20 text-yellow",
     name: "Change Policy"
   }
 };
