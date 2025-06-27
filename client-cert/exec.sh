6ac734d6c4b68c48b82f40e9a0cc62787321e3d2
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2025-06-28 01:32:53 +0800

    removed unnecessary changes

--- client-cert/exec.sh
+++
@@ -1,8 +0,0 @@
-curl \
-  --request POST \
-  --insecure \
-  --cert cert.pem \
-  --key key.pem \
-  -d '{"identityId": "a87a7a3b-345c-46b2-a95a-54a608e0538b"}' \
-  -H "Content-Type: application/json" \
-  https://localhost:8443/api/v1/auth/tls-cert-auth/login
