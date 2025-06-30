978a3e5828606fb8e1cd7d1b0cc6711013d1b663
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2025-07-01 01:25:11 +0800

    misc: add indices for referencing columns in identity access token

---
+++ backend/src/db/migrations/20250630170849_add-index-for-access-token.ts
@@ -0,0 +1,25 @@
+import { Knex } from "knex";
+
+import { TableName } from "../schemas";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+        CREATE INDEX IF NOT EXISTS ${TableName.IdentityAccessToken}_identityid_index 
+        ON ${TableName.IdentityAccessToken} ("identityId")
+      `);
+
+  await knex.raw(`
+        CREATE INDEX IF NOT EXISTS ${TableName.IdentityAccessToken}_identityuaclientsecretid_index 
+        ON ${TableName.IdentityAccessToken} ("identityUAClientSecretId")
+      `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+        DROP INDEX IF EXISTS ${TableName.IdentityAccessToken}_identityid_index
+      `);
+
+  await knex.raw(`
+        DROP INDEX IF EXISTS ${TableName.IdentityAccessToken}_identityuaclientsecretid_index
+      `);
+}
