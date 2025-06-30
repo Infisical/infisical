cce393bf424b7282e3fe66fc1e9de9cd9e37ab00
Abimael Martell <abimex@gmail.com>
2025-06-30 16:27:24 -0700

    Allow spaces, and update message

--- frontend/src/components/utilities/checks/validateOrganizationName.ts
+++ frontend/src/components/utilities/checks/validateOrganizationName.ts
@@ -3,8 +3,8 @@ export const validateOrganizationName = (name: string) => {
     return "Please enter your organization name";
   }
 
-  if (!/^[a-zA-Z0-9\s\-_]+$/.test(name)) {
-    return "Organization name can only contain alphanumeric characters, dashes, and spaces";
+  if (!/^[a-zA-Z0-9\s\-_\s]+$/.test(name)) {
+    return "Organization name can only contain alphanumeric characters, underscores, dashes and spaces";
   }
 
   return null
