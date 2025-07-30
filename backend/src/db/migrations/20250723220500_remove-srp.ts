0c98d9187de6246110bef366e04e7f7311b7ba56
Daniel Hougaard <daniel@infisical.com>
2025-07-30 05:03:15 +0400

    Update 20250723220500_remove-srp.ts

--- backend/src/db/migrations/20250723220500_remove-srp.ts
+++ backend/src/db/migrations/20250723220500_remove-srp.ts
@@ -13,6 +13,6 @@ export async function up(knex: Knex): Promise<void> {
   });
 }
 
-export async function down(knex: Knex): Promise<void> {
+export async function down(): Promise<void> {
   // do nothing for now to avoid breaking down migrations
 }
