00e4977cafb11161abf2c5cafadbaaf69b66e27b
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-08-18 12:00:52 +0200

    chore: linted files

--- backend/src/services/app-connection/app-connection-service.ts
+++ backend/src/services/app-connection/app-connection-service.ts
@@ -584,8 +584,9 @@ export const appConnectionServiceFactory = ({
         }).success
       )
         throw new BadRequestError({
-          message: `Invalid credential format for ${APP_CONNECTION_NAME_MAP[app]
-            } Connection with method ${getAppConnectionMethodName(method)}`
+          message: `Invalid credential format for ${
+            APP_CONNECTION_NAME_MAP[app]
+          } Connection with method ${getAppConnectionMethodName(method)}`
         });
 
       updatedCredentials = await validateAppConnectionCredentials(
@@ -763,8 +764,9 @@ export const appConnectionServiceFactory = ({
 
     if (appConnection.app !== app)
       throw new BadRequestError({
-        message: `${APP_CONNECTION_NAME_MAP[appConnection.app as AppConnection]
-          } Connection with ID ${connectionId} cannot be used to connect to ${APP_CONNECTION_NAME_MAP[app]}`
+        message: `${
+          APP_CONNECTION_NAME_MAP[appConnection.app as AppConnection]
+        } Connection with ID ${connectionId} cannot be used to connect to ${APP_CONNECTION_NAME_MAP[app]}`
       });
 
     const connection = await decryptAppConnection(appConnection, kmsService);
