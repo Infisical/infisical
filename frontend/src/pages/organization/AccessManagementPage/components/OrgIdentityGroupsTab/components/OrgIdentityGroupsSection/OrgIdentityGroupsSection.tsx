5a2e62393adc59cd02f83a0359c1cf72d17ae4eb
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-16 22:42:41 +0000

    Rename stuff

--- frontend/src/pages/organization/AccessManagementPage/components/OrgIdentityGroupsTab/components/OrgIdentityGroupsSection/OrgIdentityGroupsSection.tsx
+++ frontend/src/pages/organization/AccessManagementPage/components/OrgIdentityGroupsTab/components/OrgIdentityGroupsSection/OrgIdentityGroupsSection.tsx
@@ -57,7 +57,7 @@ export const OrgIdentityGroupsSection = () => {
   return (
     <div className="mb-6 rounded-lg border border-mineshaft-600 bg-mineshaft-900 p-4">
       <div className="mb-4 flex items-center justify-between">
-        <p className="text-xl font-semibold text-mineshaft-100">Groups</p>
+        <p className="text-xl font-semibold text-mineshaft-100">Identity Groups</p>
         <OrgPermissionCan I={OrgPermissionGroupActions.Create} a={OrgPermissionSubjects.Groups}>
           {(isAllowed) => (
             <Button
@@ -67,7 +67,7 @@ export const OrgIdentityGroupsSection = () => {
               onClick={() => handleAddGroupModal()}
               isDisabled={!isAllowed}
             >
-              Create Group
+              Create Identity Group
             </Button>
           )}
         </OrgPermissionCan>
