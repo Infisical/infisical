475efda132df7e66a87462b7cf97103de01fc564
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-16 19:28:36 +0000

    Add missing table name enum values

--- backend/src/db/schemas/models.ts
+++ backend/src/db/schemas/models.ts
@@ -92,6 +92,9 @@ export enum TableName {
   IdentityProjectMembershipRole = "identity_project_membership_role",
   IdentityProjectAdditionalPrivilege = "identity_project_additional_privilege",
   IdentityAuthTemplate = "identity_auth_templates",
+  IdentityGroups = "identity_groups",
+  IdentityUserGroupMembership = "identity_user_group_membership",
+  IdentityGroupProjectMembership = "identity_group_project_membership",
   // used by both identity and users
   IdentityMetadata = "identity_metadata",
   ResourceMetadata = "resource_metadata",
