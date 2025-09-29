48effcbd3d1c971ac04c5e546372559f49178215
= <akhilmhdh@gmail.com>
2025-09-29 14:39:47 +0530

    feat: completed membership group identity

--- backend/src/services/membership-group/membership-group-service.ts
+++ backend/src/services/membership-group/membership-group-service.ts
@@ -1,66 +1,44 @@
-import { AccessScope, ProjectMembershipRole, TemporaryPermissionMode, TMembershipRolesInsert } from "@app/db/schemas";
-import { TPermissionServiceFactory } from "@app/ee/services/permission/permission-service-types";
+import { ProjectMembershipRole, TemporaryPermissionMode, TMembershipRolesInsert } from "@app/db/schemas";
 import { BadRequestError, NotFoundError } from "@app/lib/errors";
 import { groupBy } from "@app/lib/fn";
 import { ms } from "@app/lib/ms";
 import { SearchResourceOperators } from "@app/lib/search-resource/search";
 
 import { TMembershipRoleDALFactory } from "../membership/membership-role-dal";
-import { TOrgDALFactory } from "../org/org-dal";
 import { TRoleDALFactory } from "../role/role-dal";
-import { TMembershipIdentityDALFactory } from "./membership-identity-dal";
+import { TMembershipGroupDALFactory } from "./membership-group-dal";
 import {
-  TCreateMembershipIdentityDTO,
-  TDeleteMembershipIdentityDTO,
-  TGetMembershipIdentityByIdentityIdDTO,
-  TListMembershipIdentityDTO,
-  TUpdateMembershipIdentityDTO
-} from "./membership-identity-types";
-import { newNamespaceMembershipIdentityFactory } from "./namespace/namespace-membership-identity-factory";
-import { newOrgMembershipIdentityFactory } from "./org/org-membership-identity-factory";
-import { newProjectMembershipIdentityFactory } from "./project/project-membership-identity-factory";
+  TCreateMembershipGroupDTO,
+  TDeleteMembershipGroupDTO,
+  TGetMembershipGroupByGroupIdDTO,
+  TListMembershipGroupDTO,
+  TMembershipGroupScopeFactory,
+  TUpdateMembershipGroupDTO
+} from "./membership-group-types";
 
-type TMembershipIdentityServiceFactoryDep = {
-  membershipIdentityDAL: TMembershipIdentityDALFactory;
+type TMembershipGroupServiceFactoryDep = {
+  membershipGroupDAL: TMembershipGroupDALFactory;
   membershipRoleDAL: Pick<TMembershipRoleDALFactory, "insertMany" | "delete">;
   roleDAL: Pick<TRoleDALFactory, "find">;
-  permissionService: Pick<
-    TPermissionServiceFactory,
-    "getOrgPermission" | "getProjectPermission" | "getProjectPermissionByRoles" | "getOrgPermissionByRoles"
-  >;
-  orgDAL: Pick<TOrgDALFactory, "findById">;
 };
 
-export type TMembershipIdentityServiceFactory = ReturnType<typeof membershipIdentityServiceFactory>;
+export type TMembershipGroupServiceFactory = ReturnType<typeof membershipGroupServiceFactory>;
 
-export const membershipIdentityServiceFactory = ({
-  membershipIdentityDAL,
+export const membershipGroupServiceFactory = ({
+  membershipGroupDAL,
   roleDAL,
-  membershipRoleDAL,
-  permissionService,
-  orgDAL
-}: TMembershipIdentityServiceFactoryDep) => {
-  const scopeFactory = {
-    [AccessScope.Organization]: newOrgMembershipIdentityFactory({
-      orgDAL,
-      permissionService
-    }),
-    [AccessScope.Project]: newProjectMembershipIdentityFactory({
-      membershipIdentityDAL,
-      orgDAL,
-      permissionService
-    }),
-    [AccessScope.Namespace]: newNamespaceMembershipIdentityFactory({})
-  };
+  membershipRoleDAL
+}: TMembershipGroupServiceFactoryDep) => {
+  const scopeFactory: Record<string, TMembershipGroupScopeFactory> = {};
 
-  const createMembership = async (dto: TCreateMembershipIdentityDTO) => {
+  const createMembership = async (dto: TCreateMembershipGroupDTO) => {
     const { scopeData, data } = dto;
     const factory = scopeFactory[scopeData.scope];
 
     const hasOnePermanentRole = data.roles.some((el) => el.isTemporary);
     if (hasOnePermanentRole) {
       throw new BadRequestError({
-        message: "Identity must have atleast one permanent role"
+        message: "Group must have atleast one permanent role"
       });
     }
     const isInvalidTemporaryRole = data.roles.some((el) => {
@@ -78,7 +56,7 @@ export const membershipIdentityServiceFactory = ({
     }
 
     const scopeDatabaseFields = factory.getScopeDatabaseFields(dto.scopeData);
-    await factory.onCreateMembershipIdentityGuard(dto);
+    await factory.onCreateMembershipGroupGuard(dto);
 
     const customInputRoles = data.roles.filter((el) => factory.isCustomRole(el.role));
     const hasCustomRole = customInputRoles.length > 0;
@@ -96,12 +74,12 @@ export const membershipIdentityServiceFactory = ({
 
     const customRolesGroupBySlug = groupBy(customRoles, ({ slug }) => slug);
 
-    const membership = await membershipIdentityDAL.transaction(async (tx) => {
-      const doc = await membershipIdentityDAL.create(
+    const membership = await membershipGroupDAL.transaction(async (tx) => {
+      const doc = await membershipGroupDAL.create(
         {
           scope: scopeData.scope,
           ...scopeDatabaseFields,
-          actorIdentityId: dto.data.identityId
+          actorGroupId: dto.data.groupId
         },
         tx
       );
@@ -142,11 +120,11 @@ export const membershipIdentityServiceFactory = ({
     return { membership };
   };
 
-  const updateMembership = async (dto: TUpdateMembershipIdentityDTO) => {
+  const updateMembership = async (dto: TUpdateMembershipGroupDTO) => {
     const { scopeData, data } = dto;
     const factory = scopeFactory[scopeData.scope];
 
-    await factory.onUpdateMembershipIdentityGuard(dto);
+    await factory.onUpdateMembershipGroupGuard(dto);
 
     const customInputRoles = data.roles.filter((el) => factory.isCustomRole(el.role));
     const hasCustomRole = customInputRoles.length > 0;
@@ -154,7 +132,7 @@ export const membershipIdentityServiceFactory = ({
     const hasOnePermanentRole = data.roles.some((el) => el.isTemporary);
     if (hasOnePermanentRole) {
       throw new BadRequestError({
-        message: "Identity must have atleast one permanent role"
+        message: "Group must have atleast one permanent role"
       });
     }
     const isInvalidTemporaryRole = data.roles.some((el) => {
@@ -172,14 +150,14 @@ export const membershipIdentityServiceFactory = ({
     }
 
     const scopeDatabaseFields = factory.getScopeDatabaseFields(dto.scopeData);
-    const existingMembership = await membershipIdentityDAL.findOne({
+    const existingMembership = await membershipGroupDAL.findOne({
       scope: scopeData.scope,
       ...scopeDatabaseFields,
-      actorIdentityId: dto.selector.identityId
+      actorIdentityId: dto.selector.groupId
     });
     if (!existingMembership)
       throw new BadRequestError({
-        message: "Identity doesn't have membership"
+        message: "Group doesn't have membership"
       });
 
     const scopeField = factory.getScopeField(dto.scopeData);
@@ -195,8 +173,8 @@ export const membershipIdentityServiceFactory = ({
 
     const customRolesGroupBySlug = groupBy(customRoles, ({ slug }) => slug);
 
-    const membershipDoc = await membershipIdentityDAL.transaction(async (tx) => {
-      const doc = await membershipIdentityDAL.updateById(
+    const membershipDoc = await membershipGroupDAL.transaction(async (tx) => {
+      const doc = await membershipGroupDAL.updateById(
         existingMembership.id,
         {
           isActive: data.isActive
@@ -246,21 +224,21 @@ export const membershipIdentityServiceFactory = ({
     return { membership: membershipDoc };
   };
 
-  const deleteMembership = async (dto: TDeleteMembershipIdentityDTO) => {
+  const deleteMembership = async (dto: TDeleteMembershipGroupDTO) => {
     const { scopeData } = dto;
     const factory = scopeFactory[scopeData.scope];
 
-    await factory.onDeleteMembershipIdentityGuard(dto);
+    await factory.onDeleteMembershipGroupGuard(dto);
 
     const scopeDatabaseFields = factory.getScopeDatabaseFields(dto.scopeData);
-    const existingMembership = await membershipIdentityDAL.findOne({
+    const existingMembership = await membershipGroupDAL.findOne({
       scope: scopeData.scope,
       ...scopeDatabaseFields,
-      actorIdentityId: dto.selector.identityId
+      actorIdentityId: dto.selector.groupId
     });
     if (!existingMembership)
       throw new BadRequestError({
-        message: "Identity doesn't have membership"
+        message: "Group doesn't have membership"
       });
 
     if (existingMembership.actorIdentityId === dto.permission.id)
@@ -268,27 +246,27 @@ export const membershipIdentityServiceFactory = ({
         message: "You can't delete you own membership"
       });
 
-    const membershipDoc = await membershipIdentityDAL.transaction(async (tx) => {
+    const membershipDoc = await membershipGroupDAL.transaction(async (tx) => {
       await membershipRoleDAL.delete({ membershipId: existingMembership.id }, tx);
-      const doc = await membershipIdentityDAL.deleteById(existingMembership.id, tx);
+      const doc = await membershipGroupDAL.deleteById(existingMembership.id, tx);
       return doc;
     });
     return { membership: membershipDoc };
   };
 
-  const listMemberships = async (dto: TListMembershipIdentityDTO) => {
+  const listMemberships = async (dto: TListMembershipGroupDTO) => {
     const { scopeData } = dto;
     const factory = scopeFactory[scopeData.scope];
 
-    await factory.onListMembershipIdentityGuard(dto);
-    const memberships = await membershipIdentityDAL.findIdentities({
+    await factory.onListMembershipGroupGuard(dto);
+    const memberships = await membershipGroupDAL.findGroups({
       scopeData,
       filter: {
         limit: dto.data.limit,
         offset: dto.data.offset,
-        name: dto.data.identityName
+        name: dto.data.groupName
           ? {
-              [SearchResourceOperators.$contains]: dto.data.identityName
+              [SearchResourceOperators.$contains]: dto.data.groupName
             }
           : undefined,
         role: dto.data.roles.length
@@ -301,16 +279,16 @@ export const membershipIdentityServiceFactory = ({
     return memberships;
   };
 
-  const getMembershipByIdentityId = async (dto: TGetMembershipIdentityByIdentityIdDTO) => {
+  const getMembershipByGroupId = async (dto: TGetMembershipGroupByGroupIdDTO) => {
     const { scopeData, selector } = dto;
     const factory = scopeFactory[scopeData.scope];
 
-    await factory.onGetMembershipIdentityByIdentityIdGuard(dto);
-    const membership = await membershipIdentityDAL.getIdentityById({
+    await factory.onGetMembershipGroupByGroupIdGuard(dto);
+    const membership = await membershipGroupDAL.getGroupById({
       scopeData,
-      identityId: selector.identityId
+      groupId: selector.groupId
     });
-    if (!membership) throw new NotFoundError({ message: `Identity membership not found` });
+    if (!membership) throw new NotFoundError({ message: `Group membership not found` });
 
     return membership;
   };
@@ -320,6 +298,6 @@ export const membershipIdentityServiceFactory = ({
     updateMembership,
     deleteMembership,
     listMemberships,
-    getMembershipByIdentityId
+    getMembershipByGroupId
   };
 };
---
+++ backend/src/services/membership-group/org/org-membership-group-factory.ts
@@ -0,0 +1,125 @@
+import { ForbiddenError } from "@casl/ability";
+
+import { AccessScope, OrgMembershipRole } from "@app/db/schemas";
+import { OrgPermissionGroupActions, OrgPermissionSubjects } from "@app/ee/services/permission/org-permission";
+import {
+  constructPermissionErrorMessage,
+  validatePrivilegeChangeOperation
+} from "@app/ee/services/permission/permission-fns";
+import { TPermissionServiceFactory } from "@app/ee/services/permission/permission-service-types";
+import { BadRequestError, InternalServerError, PermissionBoundaryError } from "@app/lib/errors";
+import { TOrgDALFactory } from "@app/services/org/org-dal";
+import { isCustomOrgRole } from "@app/services/org/org-role-fns";
+
+import { TMembershipGroupScopeFactory } from "../membership-group-types";
+
+type TOrgMembershipGroupScopeFactoryDep = {
+  permissionService: Pick<TPermissionServiceFactory, "getOrgPermission" | "getOrgPermissionByRoles">;
+  orgDAL: Pick<TOrgDALFactory, "findById">;
+};
+
+export const newOrgMembershipGroupFactory = ({
+  permissionService,
+  orgDAL
+}: TOrgMembershipGroupScopeFactoryDep): TMembershipGroupScopeFactory => {
+  const getScopeField: TMembershipGroupScopeFactory["getScopeField"] = (dto) => {
+    if (dto.scope === AccessScope.Organization) {
+      return { key: "orgId" as const, value: dto.orgId };
+    }
+    throw new InternalServerError({ message: "Invalid scope provided for the org factory" });
+  };
+
+  const getScopeDatabaseFields: TMembershipGroupScopeFactory["getScopeDatabaseFields"] = (dto) => {
+    if (dto.scope === AccessScope.Organization) {
+      return { scopeOrgId: dto.orgId };
+    }
+    throw new InternalServerError({ message: "Invalid scope provided for the org factory" });
+  };
+
+  const isCustomRole: TMembershipGroupScopeFactory["isCustomRole"] = (role: string) => isCustomOrgRole(role);
+
+  const onCreateMembershipGroupGuard: TMembershipGroupScopeFactory["onCreateMembershipGroupGuard"] = async () => {
+    throw new BadRequestError({
+      message: "Organizatin membership cannot be created for groups"
+    });
+  };
+
+  const onUpdateMembershipGroupGuard: TMembershipGroupScopeFactory["onUpdateMembershipGroupGuard"] = async (dto) => {
+    const { permission } = await permissionService.getOrgPermission(
+      dto.permission.type,
+      dto.permission.id,
+      dto.permission.orgId,
+      dto.permission.authMethod,
+      dto.permission.orgId
+    );
+    ForbiddenError.from(permission).throwUnlessCan(OrgPermissionGroupActions.Edit, OrgPermissionSubjects.Groups);
+    const permissionRoles = await permissionService.getOrgPermissionByRoles(
+      dto.data.roles.map((el) => el.role),
+      dto.permission.orgId
+    );
+
+    const { shouldUseNewPrivilegeSystem } = await orgDAL.findById(dto.permission.orgId);
+    for (const permissionRole of permissionRoles) {
+      if (permissionRole?.role?.name !== OrgMembershipRole.NoAccess) {
+        const permissionBoundary = validatePrivilegeChangeOperation(
+          shouldUseNewPrivilegeSystem,
+          OrgPermissionGroupActions.GrantPrivileges,
+          OrgPermissionSubjects.Groups,
+          permission,
+          permissionRole.permission
+        );
+        if (!permissionBoundary.isValid)
+          throw new PermissionBoundaryError({
+            message: constructPermissionErrorMessage(
+              "Failed to create group org membership",
+              shouldUseNewPrivilegeSystem,
+              OrgPermissionGroupActions.GrantPrivileges,
+              OrgPermissionSubjects.Groups
+            ),
+            details: { missingPermissions: permissionBoundary.missingPermissions }
+          });
+      }
+    }
+  };
+
+  const onDeleteMembershipGroupGuard: TMembershipGroupScopeFactory["onDeleteMembershipGroupGuard"] = async () => {
+    throw new BadRequestError({
+      message: "Organizatin membership cannot be created for organization scoped group"
+    });
+  };
+
+  const onListMembershipGroupGuard: TMembershipGroupScopeFactory["onListMembershipGroupGuard"] = async (dto) => {
+    const { permission } = await permissionService.getOrgPermission(
+      dto.permission.type,
+      dto.permission.id,
+      dto.permission.orgId,
+      dto.permission.authMethod,
+      dto.permission.orgId
+    );
+    ForbiddenError.from(permission).throwUnlessCan(OrgPermissionGroupActions.Read, OrgPermissionSubjects.Groups);
+  };
+
+  const onGetMembershipGroupByGroupIdGuard: TMembershipGroupScopeFactory["onGetMembershipGroupByGroupIdGuard"] = async (
+    dto
+  ) => {
+    const { permission } = await permissionService.getOrgPermission(
+      dto.permission.type,
+      dto.permission.id,
+      dto.permission.orgId,
+      dto.permission.authMethod,
+      dto.permission.orgId
+    );
+    ForbiddenError.from(permission).throwUnlessCan(OrgPermissionGroupActions.Read, OrgPermissionSubjects.Groups);
+  };
+
+  return {
+    onCreateMembershipGroupGuard,
+    onUpdateMembershipGroupGuard,
+    onDeleteMembershipGroupGuard,
+    onListMembershipGroupGuard,
+    onGetMembershipGroupByGroupIdGuard,
+    getScopeField,
+    getScopeDatabaseFields,
+    isCustomRole
+  };
+};
