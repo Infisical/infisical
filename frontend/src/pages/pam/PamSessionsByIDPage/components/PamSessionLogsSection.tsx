8bff05b0cf45c9dd58bbd77b0f0c02dc58f45c36
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2025-10-06 01:18:11 +0800

    misc: added message for when session logs aren't uploaded

--- frontend/src/pages/pam/PamSessionsByIDPage/components/PamSessionLogsSection.tsx
+++ frontend/src/pages/pam/PamSessionsByIDPage/components/PamSessionLogsSection.tsx
@@ -69,7 +69,18 @@ export const PamSessionLogsSection = ({ session }: Props) => {
           })
         ) : (
           <div className="flex w-full grow items-center justify-center text-bunker-300">
-            No session logs
+            {session.startedAt && session.endedAt ? (
+              <div className="text-center">
+                <div className="mb-2">Session logs are not available</div>
+                <div className="text-xs text-bunker-400">
+                  Logs failed to upload from the Gateway.
+                  <br />
+                  Please contact your Gateway administrators.
+                </div>
+              </div>
+            ) : (
+              "No session logs"
+            )}
           </div>
         )}
       </div>
