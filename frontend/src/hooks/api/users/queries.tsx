422136b7bed37ae51b13d99bfc880a36e9da61aa
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-10-07 19:01:16 +0530

    fix: clear authentication tokens on user deletion

--- frontend/src/hooks/api/users/queries.tsx
+++ frontend/src/hooks/api/users/queries.tsx
@@ -1,6 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 
+import SecurityClient from "@app/components/utilities/SecurityClient";
 import { apiRequest } from "@app/config/request";
 import { SessionStorageKeys } from "@app/const";
 import { queryClient as qc } from "@app/hooks/api/reactQuery";
@@ -86,6 +87,8 @@ export const useDeleteMe = () => {
       localStorage.removeItem("tag");
       localStorage.removeItem("PRIVATE_KEY");
       localStorage.removeItem("orgData.id");
+      setAuthToken("");
+      SecurityClient.setToken("");
 
       queryClient.clear();
     }
