42b5b676b58ccea113fa116090849dc4965326ea
= <akhilmhdh@gmail.com>
2025-10-08 11:50:08 +0530

    feat: added label component

---
+++ frontend/src/components/v3/generic/Label/Label.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cn } from "../../utils";
+
+function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        "flex select-none items-center gap-2 text-sm font-medium leading-none text-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Label };
