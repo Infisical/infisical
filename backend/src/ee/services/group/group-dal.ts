4333773c0d91203e6f0f6062459af7933807a439
Daniel Hougaard <daniel@infisical.com>
2025-10-09 05:11:06 +0400

    fix: dal error when offset is negative

--- backend/src/ee/services/group/group-dal.ts
+++ backend/src/ee/services/group/group-dal.ts
@@ -20,7 +20,7 @@ export const groupDALFactory = (db: TDbClient) => {
         .select(selectAllTableCols(TableName.Groups));
 
       if (limit) void query.limit(limit);
-      if (offset) void query.limit(offset);
+      if (offset && offset > 0) void query.offset(offset);
       if (sort) {
         void query.orderBy(sort.map(([column, order, nulls]) => ({ column: column as string, order, nulls })));
       }
