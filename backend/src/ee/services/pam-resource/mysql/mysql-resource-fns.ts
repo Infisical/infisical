6dff09a50ef044b1e22394cc7598e6d01e24a4bc
Fang-Pen Lin <fangpen@infisical.com>
2025-10-10 17:55:05 -0700

    Add mysql options

--- backend/src/ee/services/pam-resource/mysql/mysql-resource-fns.ts
+++ backend/src/ee/services/pam-resource/mysql/mysql-resource-fns.ts
@@ -1,6 +1,6 @@
 import { MySQLResourceListItemSchema } from "./mysql-resource-schemas";
 
-export const getPostgresResourceListItem = () => {
+export const getMySQLResourceListItem = () => {
   return {
     name: MySQLResourceListItemSchema.shape.name.value,
     resource: MySQLResourceListItemSchema.shape.resource.value
