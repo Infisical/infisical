42203341095aefe85557a3f6bfb3f70c1fed654c
Fang-Pen Lin <fangpen@infisical.com>
2025-10-10 17:01:34 -0700

    Add missing mysql types

--- backend/src/ee/services/pam-resource/shared/sql/sql-resource-types.ts
+++ backend/src/ee/services/pam-resource/shared/sql/sql-resource-types.ts
@@ -2,6 +2,10 @@ import {
   TPostgresAccountCredentials,
   TPostgresResourceConnectionDetails
 } from "../../postgres/postgres-resource-types";
+import {
+  TMySQLAccountCredentials,
+  TMySQLResourceConnectionDetails
+} from "../../mysql/mysql-resource-types";
 
-export type TSqlResourceConnectionDetails = TPostgresResourceConnectionDetails;
-export type TSqlAccountCredentials = TPostgresAccountCredentials;
+export type TSqlResourceConnectionDetails = TPostgresResourceConnectionDetails | TMySQLResourceConnectionDetails;
+export type TSqlAccountCredentials = TPostgresAccountCredentials | TMySQLAccountCredentials;
