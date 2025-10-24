555f900172858d5e593d11e15a6710967b71453d
= <akhilmhdh@gmail.com>
2025-10-24 21:55:25 +0530

    feat: added transaction

--- backend/src/db/migrations/20251019061215_sub-org-identity-backfill.ts
+++ backend/src/db/migrations/20251019061215_sub-org-identity-backfill.ts
@@ -5,44 +5,46 @@ import { chunkArray } from "@app/lib/fn";
 import { AccessScope, TableName } from "../schemas";
 
 export async function up(knex: Knex): Promise<void> {
-  const hasIdentityOrgCol = await knex.schema.hasColumn(TableName.Identity, "orgId");
-  if (hasIdentityOrgCol) {
-    const identityMemberships = await knex(TableName.Membership)
-      .where({
-        scope: AccessScope.Organization
-      })
-      .whereNotNull("actorIdentityId")
-      .select("actorIdentityId", "scopeOrgId");
+  await knex.transaction(async (tx) => {
+    const hasIdentityOrgCol = await tx.schema.hasColumn(TableName.Identity, "orgId");
+    if (hasIdentityOrgCol) {
+      const identityMemberships = await tx(TableName.Membership)
+        .where({
+          scope: AccessScope.Organization
+        })
+        .whereNotNull("actorIdentityId")
+        .select("actorIdentityId", "scopeOrgId");
 
-    const identityToOrgMapping: Record<string, string> = {};
-    identityMemberships.forEach((el) => {
-      if (el.actorIdentityId) {
-        identityToOrgMapping[el.actorIdentityId] = el.scopeOrgId;
-      }
-    });
+      const identityToOrgMapping: Record<string, string> = {};
+      identityMemberships.forEach((el) => {
+        if (el.actorIdentityId) {
+          identityToOrgMapping[el.actorIdentityId] = el.scopeOrgId;
+        }
+      });
 
-    const batchMemberships = chunkArray(identityMemberships, 500);
-    for await (const membership of batchMemberships) {
-      const identityIds = membership.map((el) => el.actorIdentityId).filter(Boolean) as string[];
-      if (identityIds.length) {
-        const identities = await knex(TableName.Identity).whereIn("id", identityIds).select("*");
-        await knex(TableName.Identity)
-          .insert(
-            identities.map((el) => ({
-              ...el,
-              orgId: identityToOrgMapping[el.id]
-            }))
-          )
-          .onConflict("id")
-          .merge();
+      const batchMemberships = chunkArray(identityMemberships, 500);
+      for await (const membership of batchMemberships) {
+        const identityIds = membership.map((el) => el.actorIdentityId).filter(Boolean) as string[];
+        if (identityIds.length) {
+          const identities = await tx(TableName.Identity).whereIn("id", identityIds).select("*");
+          await tx(TableName.Identity)
+            .insert(
+              identities.map((el) => ({
+                ...el,
+                orgId: identityToOrgMapping[el.id]
+              }))
+            )
+            .onConflict("id")
+            .merge();
+        }
       }
+
+      await tx(TableName.Identity).whereNull("orgId").delete();
+      await tx.schema.alterTable(TableName.Identity, (t) => {
+        t.uuid("orgId").notNullable().alter();
+      });
     }
-
-    await knex(TableName.Identity).whereNull("orgId").delete();
-    await knex.schema.alterTable(TableName.Identity, (t) => {
-      t.uuid("orgId").notNullable().alter();
-    });
-  }
+  });
 }
 
 export async function down(): Promise<void> {}
