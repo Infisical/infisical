0ae7eac8103dd35350e73efaac0629ca92c83785
Piyush Gupta <piyushguptaa2z123@gmail.com>
2025-12-02 00:39:01 +0530

    fix: added constants file

--- backend/src/lib/api-docs/constants.ts
+++ backend/src/lib/api-docs/constants.ts
@@ -595,6 +595,7 @@ export const TOKEN_AUTH = {
     limit: "The number of tokens to return."
   },
   GET_TOKEN: {
+    identityId: "The ID of the machine identity to get the token for.",
     tokenId: "The ID of the token to get metadata for."
   },
   CREATE_TOKEN: {
