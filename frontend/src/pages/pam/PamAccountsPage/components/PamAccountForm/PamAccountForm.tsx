95e56a40553f7b52e1e8960be1f04e12cd48bdda
Fang-Pen Lin <hello@fangpenlin.com>
2025-12-22 15:33:37 -0800

    Add redis pam

--- frontend/src/pages/pam/PamAccountsPage/components/PamAccountForm/PamAccountForm.tsx
+++ frontend/src/pages/pam/PamAccountsPage/components/PamAccountForm/PamAccountForm.tsx
@@ -12,6 +12,7 @@ import { AwsIamAccountForm } from "./AwsIamAccountForm";
 import { KubernetesAccountForm } from "./KubernetesAccountForm";
 import { MySQLAccountForm } from "./MySQLAccountForm";
 import { PostgresAccountForm } from "./PostgresAccountForm";
+import { RedisAccountForm } from "./RedisAccountForm";
 import { SshAccountForm } from "./SshAccountForm";
 
 type FormProps = {
@@ -68,6 +69,10 @@ const CreateForm = ({
       return (
         <MySQLAccountForm onSubmit={onSubmit} resourceId={resourceId} resourceType={resourceType} />
       );
+    case PamResourceType.Redis:
+      return (
+        <RedisAccountForm onSubmit={onSubmit} resourceId={resourceId} resourceType={resourceType} />
+      );
     case PamResourceType.SSH:
       return (
         <SshAccountForm onSubmit={onSubmit} resourceId={resourceId} resourceType={resourceType} />
@@ -116,6 +121,8 @@ const UpdateForm = ({ account, onComplete }: UpdateFormProps) => {
       return <PostgresAccountForm account={account as any} onSubmit={onSubmit} />;
     case PamResourceType.MySQL:
       return <MySQLAccountForm account={account as any} onSubmit={onSubmit} />;
+    case PamResourceType.Redis:
+      return <RedisAccountForm account={account as any} onSubmit={onSubmit} />;
     case PamResourceType.SSH:
       return <SshAccountForm account={account as any} onSubmit={onSubmit} />;
     case PamResourceType.Kubernetes:
