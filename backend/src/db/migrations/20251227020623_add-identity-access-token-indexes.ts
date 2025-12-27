581dfab63209d1fda39eda99a49ab6fca496d69a
Fang-Pen Lin <hello@fangpenlin.com>
2025-12-26 18:20:00 -0800

    Add db migration for new indexes

---
+++ backend/src/db/migrations/20251227020623_add-identity-access-token-indexes.ts
@@ -0,0 +1,60 @@
+import { Knex } from "knex";
+
+import { TableName } from "../schemas";
+
+export async function up(knex: Knex): Promise<void> {
+  if (
+    (await knex.schema.hasTable(TableName.IdentityAccessToken)) &&
+    (await knex.schema.hasColumn(TableName.IdentityAccessToken, "accessTokenTTL")) &&
+    (await knex.schema.hasColumn(TableName.IdentityAccessToken, "accessTokenLastRenewedAt"))
+  ) {
+    await knex.raw(`
+        CREATE INDEX IF NOT EXISTS idx_identity_access_tokens_expiration CONCURRENTLY
+        ON ${TableName.IdentityAccessToken} (
+            (COALESCE("accessTokenLastRenewedAt", "createdAt") AT TIME ZONE 'UTC' + make_interval(secs => LEAST(
+            "identity_access_tokens"."accessTokenTTL",
+            '315360000'
+            )))
+        )
+        WHERE "accessTokenTTL" > 0
+    `);
+  }
+
+  if (
+    (await knex.schema.hasTable(TableName.IdentityAccessToken)) &&
+    (await knex.schema.hasColumn(TableName.IdentityAccessToken, "isAccessTokenRevoked"))
+  ) {
+    await knex.raw(`
+        CREATE INDEX IF NOT EXISTS idx_identity_access_tokens_revoked CONCURRENTLY
+        ON ${TableName.IdentityAccessToken} ("id")
+        WHERE "isAccessTokenRevoked" = true
+    `);
+  }
+
+  if (
+    (await knex.schema.hasTable(TableName.IdentityAccessToken)) &&
+    (await knex.schema.hasColumn(TableName.IdentityAccessToken, "accessTokenNumUses")) &&
+    (await knex.schema.hasColumn(TableName.IdentityAccessToken, "accessTokenNumUsesLimit"))
+  ) {
+    await knex.raw(`
+        CREATE INDEX IF NOT EXISTS idx_identity_access_tokens_num_uses_with_limit CONCURRENTLY
+        ON ${TableName.IdentityAccessToken} ("accessTokenNumUses")
+        WHERE "accessTokenNumUsesLimit" > 0
+          AND "accessTokenNumUses" >= "identity_access_tokens"."accessTokenNumUsesLimit"
+    `);
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+      DROP INDEX IF EXISTS idx_identity_access_tokens_expiration
+    `);
+
+  await knex.raw(`
+      DROP INDEX IF EXISTS idx_identity_access_tokens_revoked
+    `);
+
+  await knex.raw(`
+      DROP INDEX IF EXISTS idx_identity_access_tokens_num_uses_with_limit
+    `);
+}
