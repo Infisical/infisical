9dc8ddaa88c2cffe75724baf3cd54f154f3c4796
Scott Wilson <scottraywilson@gmail.com>
2026-01-09 08:34:13 -0800

    fix: replace migration rollback with drop schema

--- backend/e2e-test/vitest-environment-knex.ts
+++ backend/e2e-test/vitest-environment-knex.ts
@@ -56,14 +56,9 @@ export default {
     await redis.flushdb("SYNC");
 
     try {
-      await db.migrate.rollback(
-        {
-          directory: path.join(__dirname, "../src/db/migrations"),
-          extension: "ts",
-          tableName: "infisical_migrations"
-        },
-        true
-      );
+      // called after all tests with this env have been run
+      await db.raw("DROP SCHEMA IF EXISTS public CASCADE");
+      await db.schema.createSchemaIfNotExists("public");
 
       await db.migrate.latest({
         directory: path.join(__dirname, "../src/db/migrations"),
