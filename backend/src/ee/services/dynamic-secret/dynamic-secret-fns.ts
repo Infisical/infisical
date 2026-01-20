7d8e167451839260d5a314301f6bd274bf8fb943
Daniel Hougaard <daniel@infisical.com>
2026-01-20 01:38:00 +0100

    feat: better host validation errors

--- backend/src/ee/services/dynamic-secret/dynamic-secret-fns.ts
+++ backend/src/ee/services/dynamic-secret/dynamic-secret-fns.ts
@@ -6,9 +6,15 @@ import { BadRequestError } from "@app/lib/errors";
 import { isPrivateIp } from "@app/lib/ip/ipRange";
 import { getDbConnectionHost } from "@app/lib/knex";
 
-const ERROR_MESSAGE = "Invalid host";
-
-export const verifyHostInputValidity = async (host: string, isGateway = false) => {
+export const verifyHostInputValidity = async ({
+  host,
+  isGateway,
+  isDynamicSecret = true
+}: {
+  host: string;
+  isGateway?: boolean;
+  isDynamicSecret?: boolean;
+}) => {
   const appCfg = getConfig();
 
   if (appCfg.isDevelopmentMode || appCfg.isTestMode) return [host];
@@ -47,8 +53,12 @@ export const verifyHostInputValidity = async (host: string, isGateway = false) =
   if (net.isIPv4(host)) {
     inputHostIps.push(host);
   } else {
-    if (normalizedHost === "localhost" || normalizedHost === "host.docker.internal") {
-      throw new BadRequestError({ message: ERROR_MESSAGE });
+    if (!appCfg.DYNAMIC_SECRET_ALLOW_INTERNAL_IP && !appCfg.ALLOW_INTERNAL_IP_CONNECTIONS) {
+      if (normalizedHost === "localhost" || normalizedHost === "host.docker.internal") {
+        throw new BadRequestError({
+          message: `Local host IP addresses (${normalizedHost}) are not allowed. ${!appCfg.isCloud ? `If you are self-hosting, you can allow local host IP addresses by setting the '${isDynamicSecret ? "'DYNAMIC_SECRET_ALLOW_INTERNAL_IP'" : "'ALLOW_INTERNAL_IP_CONNECTIONS'"}' environment variable to 'true' on your instance.` : ""}`
+        });
+      }
     }
     try {
       const resolvedIps = await dns.resolve4(host);
@@ -64,10 +74,16 @@ export const verifyHostInputValidity = async (host: string, isGateway = false) =
 
   if (!(appCfg.DYNAMIC_SECRET_ALLOW_INTERNAL_IP || appCfg.ALLOW_INTERNAL_IP_CONNECTIONS)) {
     const isInternalIp = inputHostIps.some((el) => isPrivateIp(el));
-    if (isInternalIp) throw new BadRequestError({ message: ERROR_MESSAGE });
+    if (isInternalIp)
+      throw new BadRequestError({
+        message: `Private IP addresses (${normalizedHost}) are not allowed. ${!appCfg.isCloud ? `If you are self-hosting, you can allow private IP addresses by setting the '${isDynamicSecret ? "'DYNAMIC_SECRET_ALLOW_INTERNAL_IP'" : "'ALLOW_INTERNAL_IP_CONNECTIONS'"}' environment variable to 'true' on your instance.` : ""}`
+      });
   }
 
   const isAppUsedIps = inputHostIps.some((el) => exclusiveIps.includes(el));
-  if (isAppUsedIps) throw new BadRequestError({ message: ERROR_MESSAGE });
+  if (isAppUsedIps)
+    throw new BadRequestError({
+      message: `The host ${host} belongs to a service that is in-use by Infisical, such as the Infisical database or Redis instance. You cannot use hosts that are in-use by Infisical.`
+    });
   return inputHostIps;
 };
