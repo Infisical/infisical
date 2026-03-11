e400a5805679e4d2b24f4709e552f665df5f0973
Scott Wilson <scottraywilson@gmail.com>
2026-03-11 12:38:19 -0700

    fix: temp remove new batch logic to fix secret value being overwritten

--- backend/src/services/secret-v2-bridge/secret-v2-bridge-fns.ts
+++ backend/src/services/secret-v2-bridge/secret-v2-bridge-fns.ts
@@ -222,10 +222,7 @@ export const fnSecretBulkUpdate = async ({
   );
 
   const allHaveIds = sanitizedInputSecrets.every((s): s is typeof s & { filter: { id: string } } => !!s.filter.id);
-  const newSecrets =
-    allHaveIds && secretDAL.bulkUpdateById
-      ? await secretDAL.bulkUpdateById(sanitizedInputSecrets, tx)
-      : await secretDAL.bulkUpdate(sanitizedInputSecrets, tx);
+  const newSecrets = await secretDAL.bulkUpdate(sanitizedInputSecrets, tx);
   const versionData = newSecrets.map(
     ({ skipMultilineEncoding, type, key, userId, encryptedComment, version, encryptedValue, id: secretId }, index) => ({
       skipMultilineEncoding,
