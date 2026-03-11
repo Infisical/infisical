250ea0740d7f9e1a06bd820092785315acb2fe1f
Sheen Capadngan <sheensantoscapadngan@gmail.com>
2026-03-12 03:29:58 +0800

    chore: improve org auth enforced message

--- backend/src/ee/services/permission/permission-fns.ts
+++ backend/src/ee/services/permission/permission-fns.ts
@@ -219,7 +219,9 @@ function validateOrgSSO(
 
   // case: google sso is enforced, but the actor is not using google sso
   if (isOrgGoogleSsoEnforced && actorAuthMethod !== null && actorAuthMethod !== AuthMethod.GOOGLE) {
-    throw new ForbiddenRequestError({ name: "Org auth enforced. Cannot access org-scoped resource" });
+    throw new ForbiddenRequestError({
+      name: "Org auth enforced. Cannot access org-scoped resource. Login with Google SSO to access this resource."
+    });
   }
 
   // case: SAML SSO is enforced, but the actor is not using SAML SSO
@@ -229,7 +231,9 @@ function validateOrgSSO(
     !isAuthMethodSaml(actorAuthMethod) &&
     actorAuthMethod !== AuthMethod.OIDC
   ) {
-    throw new ForbiddenRequestError({ name: "Org auth enforced. Cannot access org-scoped resource" });
+    throw new ForbiddenRequestError({
+      name: "Org auth enforced. Cannot access org-scoped resource. Login with SAML SSO to access this resource."
+    });
   }
 }
 
