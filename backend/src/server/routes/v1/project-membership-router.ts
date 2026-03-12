f7b9edbad3609d506e2d328dda670272ba580097
= <akhilmhdh@gmail.com>
2026-03-13 00:14:10 +0530

    feat: correct types

--- backend/src/server/routes/v1/project-membership-router.ts
+++ backend/src/server/routes/v1/project-membership-router.ts
@@ -300,11 +300,13 @@ export const registerProjectMembershipRouter = async (server: FastifyZodProvider
         ...req.auditLogInfo,
         event: {
           type: EventType.ADD_BATCH_PROJECT_MEMBER,
-          metadata: memberships.map(({ actorUserId, id }) => ({
-            userId: actorUserId || "",
-            membershipId: id,
-            email: ""
-          }))
+          metadata: {
+            members: memberships.map(({ actorUserId, id }) => ({
+              userId: actorUserId || "",
+              membershipId: id,
+              email: ""
+            }))
+          }
         }
       });
 
