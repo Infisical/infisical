8edce5188b455318cd729a66d6a49090a0d79bd1
Scott Wilson <scottraywilson@gmail.com>
2026-03-18 18:03:12 -0700

    chore: lint

--- frontend/src/components/auth/InitialSignupStep.tsx
+++ frontend/src/components/auth/InitialSignupStep.tsx
@@ -194,7 +194,7 @@ export default function InitialSignupStep({
               href="https://infisical.com/privacy"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-foreground cursor-pointer underline underline-offset-2 duration-200 hover:decoration-project/45"
+              className="cursor-pointer underline underline-offset-2 duration-200 hover:text-foreground hover:decoration-project/45"
             >
               Privacy Policy
             </a>
