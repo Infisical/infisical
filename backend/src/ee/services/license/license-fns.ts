f1fec793b6cf9547ea9f250391272e2a52150335
Scott Wilson <scottraywilson@gmail.com>
2026-03-23 09:52:59 -0700

    improvement: rename to access insights to view access

--- backend/src/ee/services/license/license-fns.ts
+++ backend/src/ee/services/license/license-fns.ts
@@ -73,7 +73,7 @@ export const getDefaultOnPremFeatures = (): TFeatureSet => ({
   customRateLimits: false,
   subOrganization: false,
   customAlerts: false,
-  secretAccessInsights: false,
+  secretAccessInsights: true,
   auditLogs: false,
   auditLogsRetentionDays: 0,
   auditLogStreams: false,
