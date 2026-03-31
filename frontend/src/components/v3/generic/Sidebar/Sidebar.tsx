3478ec101eab8f987f60d6443b5bf4f901f07a48
Scott Wilson <scottraywilson@gmail.com>
2026-03-30 17:19:42 -0700

    fix: correct sidebar persistence

--- frontend/src/components/v3/generic/Sidebar/Sidebar.tsx
+++ frontend/src/components/v3/generic/Sidebar/Sidebar.tsx
@@ -86,7 +86,15 @@ function SidebarProvider({
   // This is the internal state of the sidebar.
   // We use openProp and setOpenProp for control from outside the component.
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  const [_open, _setOpen] = React.useState(defaultOpen);
+  const [_open, _setOpen] = React.useState(() => {
+    const cookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith(`${SIDEBAR_COOKIE_NAME}=`));
+    if (cookie) {
+      return cookie.split("=")[1] === "true";
+    }
+    return defaultOpen;
+  });
   const open = openProp ?? _open;
   const setOpen = React.useCallback(
     (value: boolean | ((value: boolean) => boolean)) => {
