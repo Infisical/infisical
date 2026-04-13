43c741d0bbf9dfbb4211f04f7ec663e009f8562d
x032205 <x032205@gmail.com>
2026-04-13 15:07:15 -0400

    review feedback

--- frontend/src/pages/redirects/oauth-callback-redirect.tsx
+++ frontend/src/pages/redirects/oauth-callback-redirect.tsx
@@ -4,18 +4,10 @@ export const Route = createFileRoute(
   "/_authenticate/_inject-org-details/organization/app-connections/$appConnection/oauth/callback"
 )({
   beforeLoad: ({ context, params, search }) => {
-    const orgId = context.organizationId;
-
-    if (!orgId) {
-      throw redirect({
-        to: "/login/select-organization"
-      });
-    }
-
     throw redirect({
       to: "/organizations/$orgId/app-connections/$appConnection/oauth/callback",
       params: {
-        orgId,
+        orgId: context.organizationId,
         appConnection: params.appConnection
       },
       search
