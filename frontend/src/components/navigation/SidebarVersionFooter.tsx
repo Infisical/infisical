cf8ef19a919e78fe663ee8d3699f71a5e1fc79a5
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2026-04-13 15:06:53 -0300

    include rule for hiding hash versions

--- frontend/src/components/navigation/SidebarVersionFooter.tsx
+++ frontend/src/components/navigation/SidebarVersionFooter.tsx
@@ -4,7 +4,13 @@ import { isInfisicalCloud } from "@app/helpers/platform";
 export const SidebarVersionFooter = () => {
   const version = envConfig.PLATFORM_VERSION;
 
-  const shouldNotDisplay = !version || isInfisicalCloud() || window.location.origin.includes("http://localhost:8080");
+  const shouldNotDisplay =
+    !version ||
+    isInfisicalCloud() ||
+    window.location.origin.includes("http://localhost:8080") ||
+    // Decicated instances don't use semver versions, but instead they use the image hash as the version
+    // For this to not be confusing to users, we hide those hashes and only show semver versions
+    /^[0-9a-f]{7}$/i.test(version);
 
   if (shouldNotDisplay) return null;
 
