8df9a923c7d3f3c37bb1792d63dc5a4a1a0134d0
Saif <saifur.h.rahman@gmail.com>
2026-04-17 20:28:07 +0530

    chore(pki): remove unused AwsAcmKeyAlgorithm enum

--- backend/src/services/certificate-authority/aws-acm-public-ca/aws-acm-public-ca-certificate-authority-enums.ts
+++ backend/src/services/certificate-authority/aws-acm-public-ca/aws-acm-public-ca-certificate-authority-enums.ts
@@ -2,12 +2,6 @@ export enum AwsAcmValidationMethod {
   DNS = "DNS"
 }
 
-export enum AwsAcmKeyAlgorithm {
-  RSA_2048 = "RSA_2048",
-  EC_prime256v1 = "EC_prime256v1",
-  EC_secp384r1 = "EC_secp384r1"
-}
-
 /**
  * ACM public certificates have a fixed validity period (as of 2025).
  * See: https://docs.aws.amazon.com/acm/latest/userguide/managed-renewal.html
