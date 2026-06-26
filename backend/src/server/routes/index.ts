47e91be0fb4a166133f16f28debd5feceb08469d
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2026-06-26 15:43:22 -0300

    refactor

--- backend/src/server/routes/index.ts
+++ backend/src/server/routes/index.ts
@@ -1978,7 +1978,6 @@ export const registerRoutes = async (
     licenseService,
     projectBotService,
     projectGrantDAL,
-    orgDAL,
     projectEnvDAL,
     folderDAL,
     permissionService,
