76e0238a59a9608a55eceba5cdae866f71356881
= <akhilmhdh@gmail.com>
2026-06-30 01:30:37 +0530

    feat: added loading spinners on product confirmation modal

--- frontend/src/pages/organization/BillingV2Page/components/AddProductModal.tsx
+++ frontend/src/pages/organization/BillingV2Page/components/AddProductModal.tsx
@@ -64,7 +64,7 @@ export const AddProductModal = ({ orgId, product, plan, cadence, onClose }: Prop
     <AlertDialog
       open
       onOpenChange={(isOpen) => {
-        if (!isOpen && !addProduct.isPending) {
+        if (!isOpen) {
           onClose();
         }
       }}
