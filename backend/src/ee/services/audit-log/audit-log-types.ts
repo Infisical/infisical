1d31c1a42a937b33801ef81565ac511f2ec077d6
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2026-08-06 13:20:58 -0300

    add local bootstrap

--- backend/src/ee/services/audit-log/audit-log-types.ts
+++ backend/src/ee/services/audit-log/audit-log-types.ts
@@ -929,6 +929,7 @@ interface UserActorMetadata {
   username: string;
   permission?: Record<string, unknown>;
   authMethod?: string;
+  oauthClientId?: string;
 }
 
 interface ServiceActorMetadata {
