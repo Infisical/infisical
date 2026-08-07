459a019db2983ea49bf1cb69e72b9c08a9465ce1
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2026-08-07 06:51:32 -0300

    fix racing condition

--- backend/src/services/oauth-client/oauth-client-dal.ts
+++ backend/src/services/oauth-client/oauth-client-dal.ts
@@ -1,5 +1,6 @@
 import { TDbClient } from "@app/db";
 import { TableName } from "@app/db/schemas";
+import { DatabaseError } from "@app/lib/errors";
 import { ormify } from "@app/lib/knex";
 
 export type TOauthClientDALFactory = ReturnType<typeof oauthClientDALFactory>;
@@ -7,5 +8,13 @@ export type TOauthClientDALFactory = ReturnType<typeof oauthClientDALFactory>;
 export const oauthClientDALFactory = (db: TDbClient) => {
   const oauthClientOrm = ormify(db, TableName.OauthClient);
 
-  return oauthClientOrm;
+  const findByIdOnPrimary = async (id: string) => {
+    try {
+      return await db(TableName.OauthClient).where({ id }).first();
+    } catch (error) {
+      throw new DatabaseError({ error, name: "FindOauthClientByIdOnPrimary" });
+    }
+  };
+
+  return { ...oauthClientOrm, findByIdOnPrimary };
 };
