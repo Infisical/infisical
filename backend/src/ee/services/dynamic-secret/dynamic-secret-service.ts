4e09f339d843aa9e1e420e3974a2bcf8827a1d1a
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2026-08-11 09:25:44 +0100

    don't bypass existing metadata

--- backend/src/ee/services/dynamic-secret/dynamic-secret-service.ts
+++ backend/src/ee/services/dynamic-secret/dynamic-secret-service.ts
@@ -496,7 +496,7 @@ export const dynamicSecretServiceFactory = ({
       subject(ProjectPermissionSub.DynamicSecrets, {
         environment: environmentSlug,
         secretPath: path,
-        metadata: metadata ?? []
+        metadata: metadata ?? dynamicSecretCfg.metadata
       })
     );
 
