4b6f7ab48e346924ae5b6d453f2ea9e088aae60d
Thiago Araujo da Silva <thiagos.araujo09@gmail.com>
2026-08-25 14:48:02 -0300

    fix merge conflict

--- frontend/src/pages/pam/PamAccountsPage/route.tsx
+++ frontend/src/pages/pam/PamAccountsPage/route.tsx
@@ -1,21 +1,19 @@
-import { createFileRoute, stripSearchParams } from '@tanstack/react-router'
-import { zodValidator } from '@tanstack/zod-adapter'
+import { createFileRoute, stripSearchParams } from "@tanstack/react-router";
+import { zodValidator } from "@tanstack/zod-adapter";
 
-import { pamSheetSearchParams } from '@app/hooks/usePamSheetState'
+import { pamSheetSearchParams } from "@app/hooks/usePamSheetState";
 
-import { PamAccountsPage } from './PamAccountsPage'
+import { PamAccountsPage } from "./PamAccountsPage";
 
 export const Route = createFileRoute(
-  '/_authenticate/_inject-org-details/_org-layout/organizations/$orgId/pam/_pam-layout/accounts',
+  "/_authenticate/_inject-org-details/_org-layout/organizations/$orgId/pam/_pam-layout/accounts/"
 )({
   validateSearch: zodValidator(pamSheetSearchParams),
   search: {
-    middlewares: [
-      stripSearchParams({ accountId: undefined, folderId: undefined }),
-    ],
+    middlewares: [stripSearchParams({ accountId: undefined, folderId: undefined })]
   },
   beforeLoad: ({ context }) => ({
-    breadcrumbs: [...context.breadcrumbs, { label: 'Accounts' }],
+    breadcrumbs: [...context.breadcrumbs, { label: "Accounts" }]
   }),
-  component: PamAccountsPage,
-})
+  component: PamAccountsPage
+});
