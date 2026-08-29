275df942e60ffc39d6cd2a7d969fb5f008e19a9a
Andrew Huang <ananhuang1@gmail.com>
2026-08-28 20:29:40 -0700

    Simplify CommandDialog styling

--- frontend/src/components/v3/generic/Command/Command.tsx
+++ frontend/src/components/v3/generic/Command/Command.tsx
@@ -62,7 +62,7 @@ function CommandDialog({
           label={title}
           loop={loop}
           shouldFilter={shouldFilter}
-          className="[&_[cmdk-group-heading]]:text-muted-foreground **:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group]]:px-2 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
+          className="**:data-[slot=command-input-wrapper]:h-12 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12"
         >
           {children}
         </Command>
