b255e7bc4f7857f7721687cc689cf817402eb0ab
Andrey <a@lbvn.net>
2026-09-01 22:21:06 -0400

    address

--- backend/src/services/smtp/emails/EmailMfaTemplate.tsx
+++ backend/src/services/smtp/emails/EmailMfaTemplate.tsx
@@ -11,12 +11,12 @@ interface EmailMfaTemplateProps extends Omit<BaseEmailWrapperProps, "title" | "p
 
 export const EmailMfaTemplate = ({ code, siteUrl, isCloud }: EmailMfaTemplateProps) => {
   return (
-    <BaseEmailWrapper title="MFA Code" preview="Sign-in attempt requires further verification." siteUrl={siteUrl}>
+    <BaseEmailWrapper title="MFA Code" preview="Your request requires further verification." siteUrl={siteUrl}>
       <Heading className="text-black text-[18px] leading-[28px] text-center font-normal p-0 mx-0">
         <strong>MFA required</strong>
       </Heading>
       <Section className="px-[24px] mt-[36px] pt-[8px] text-center pb-[8px] text-[14px] border border-solid border-gray-200 rounded-md bg-gray-50">
-        <Text>Enter the MFA code shown below in the browser where you started sign-in.</Text>
+        <Text>Enter the MFA code shown below in the browser where you started this request.</Text>
         <Text className="text-[24px] mt-[16px]">
           <strong>{code}</strong>
         </Text>
