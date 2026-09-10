af0e5b9c4bb8eae054c5879402bd34e8b6537c97
adilson <adilsonfrankneia@gmail.com>
2026-09-10 19:26:51 -0300

    feat: add a message that points to folder level access

--- frontend/src/pages/project/components/FolderAccessSection/FolderAccessSection.tsx
+++ frontend/src/pages/project/components/FolderAccessSection/FolderAccessSection.tsx
@@ -23,6 +23,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
+  DocumentationLinkBadge,
   Empty,
   EmptyDescription,
   EmptyHeader,
@@ -62,6 +63,9 @@ import { RemoveFolderAccessDialog } from "@app/pages/secret-manager/OverviewPage
 
 import { EditFolderAccessSheet } from "./EditFolderAccessSheet";
 import { TFolderAccessSectionActor } from "./types";
+
+const FOLDER_ACCESS_DOCS_URL =
+  "https://infisical.com/docs/documentation/platform/access-controls/folder-rbac";
 
 type Props = {
   actor: TFolderAccessSectionActor;
@@ -160,7 +164,10 @@ export const FolderAccessSection = ({ actor, hideActions = false }: Props) => {
     <>
       <Card>
         <CardHeader>
-          <CardTitle>Folder Access</CardTitle>
+          <CardTitle>
+            Folder Access
+            <DocumentationLinkBadge href={FOLDER_ACCESS_DOCS_URL} />
+          </CardTitle>
           <CardDescription>
             Folder-level access for this {actorNoun}. Overrides{" "}
             {actor.type === "user" ? "their" : "its"} project roles within each folder.
@@ -303,6 +310,16 @@ export const FolderAccessSection = ({ actor, hideActions = false }: Props) => {
                     Grant folder-level access from the Secrets overview using a folder&apos;s Manage
                     Access action
                   </EmptyDescription>
+                  {!currentProject?.isLegacyAdditionalPrivilegesEnabled && (
+                    <EmptyDescription>
+                      Additional privileges were moved to a more flexible way of defining
+                      permissions, using{" "}
+                      <a href={FOLDER_ACCESS_DOCS_URL} target="_blank" rel="noopener noreferrer">
+                        folder-level access control
+                      </a>
+                      .
+                    </EmptyDescription>
+                  )}
                 </EmptyHeader>
               </Empty>
             ))}
