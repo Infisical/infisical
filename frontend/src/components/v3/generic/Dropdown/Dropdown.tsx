b69483c0519ebefb5d0b31a2b7f39c3ec7285509
andrewhuhh <ananhuang1@gmail.com>
2026-09-20 19:19:01 +0000

    fix(ui): standardize dropdown row spacing

--- frontend/src/components/v3/generic/Dropdown/Dropdown.tsx
+++ frontend/src/components/v3/generic/Dropdown/Dropdown.tsx
@@ -88,7 +88,7 @@ const DropdownMenuItem = React.forwardRef<HTMLDivElement, DropdownMenuProps>(
         className={cn(
           "text-sm",
           "data-[variant=danger]:text-danger data-[variant=danger]:focus:bg-danger/10 data-[variant=danger]:*:[svg]:!text-danger",
-          "relative flex cursor-pointer items-center gap-2 rounded-sm px-3 py-2 outline-0 select-none focus:bg-foreground/5",
+          "relative flex cursor-pointer items-center gap-2 rounded-sm px-2 py-2 outline-0 select-none focus:bg-foreground/5",
           "data-[disabled]:opacity-50 data-[inset]:pl-8",
           isFocusableDisabled
             ? "data-[disabled]:cursor-not-allowed"
@@ -128,7 +128,7 @@ function DropdownMenuCheckboxItem({
     <DropdownMenuPrimitive.CheckboxItem
       data-slot="dropdown-menu-checkbox-item"
       className={cn(
-        "relative flex cursor-pointer items-center gap-2.5 rounded-sm py-2 pr-10 pl-2 text-sm outline-0 select-none",
+        "relative flex cursor-pointer items-center gap-2 rounded-sm py-2 pr-10 pl-2 text-sm outline-0 select-none",
         "focus:bg-foreground/5",
         "data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
         "[&_svg]:pointer-events-none [&_svg]:mb-0.5 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
@@ -162,7 +162,7 @@ function DropdownMenuRadioItem({
     <DropdownMenuPrimitive.RadioItem
       data-slot="dropdown-menu-radio-item"
       className={cn(
-        "relative flex cursor-pointer items-center gap-2 rounded-sm pt-2 pr-8 pb-1.5 pl-3 text-sm outline-0 select-none",
+        "relative flex cursor-pointer items-center gap-2 rounded-sm pt-2 pr-8 pb-1.5 pl-2 text-sm outline-0 select-none",
         "focus:bg-foreground/10",
         "data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
         "[&_svg]:pointer-events-none [&_svg]:mb-0.5 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-3.5",
@@ -239,7 +239,7 @@ function DropdownMenuSubTrigger({
       data-slot="dropdown-menu-sub-trigger"
       data-inset={inset}
       className={cn(
-        "flex cursor-default items-center gap-2 rounded-sm px-3 py-2 text-sm outline-0 select-none focus:bg-foreground/5 data-[inset]:pl-8 data-[state=open]:bg-foreground/5 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "flex cursor-default items-center gap-2 rounded-sm px-2 py-2 text-sm outline-0 select-none focus:bg-foreground/5 data-[inset]:pl-8 data-[state=open]:bg-foreground/5 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className
       )}
       {...props}
